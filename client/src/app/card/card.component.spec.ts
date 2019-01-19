9a02dbc847e004af93bb821dbc8917937f59940d
Michael Sauget <michaelsauget@gmail.com>
2019-01-19 01:45:55 -0500

    MS R: Refactor du component card pour que tout les éléments puissent se changer de façon dynamique.

--- client/src/app/card/card.component.spec.ts
+++ client/src/app/card/card.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HighscoreDisplayComponent } from "../highscore-display/highscore-display.component";
 import { CardComponent } from "./card.component";
 
@@ -14,7 +13,6 @@ describe("CardComponent", () => {
       declarations: [ CardComponent, HighscoreDisplayComponent ],
       imports: [
         TestingImportsModule,
-        BrowserAnimationsModule,
       ],
     })
     .compileComponents()
--- client/src/app/card/card.component.ts
+++ client/src/app/card/card.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from "@angular/core";
 export class CardComponent implements OnInit {
 
   public _isClicked: boolean;
+  public _title: string = "Chien couché";
+  public _subtitle: string = "Animaux";
+  public _avatarImageUrl: string = "https://material.angular.io/assets/img/examples/shiba1.jpg";
+  public _gameImageUrl: string = "http://www.123mobilewallpapers.com/wp-content/uploads/2014/07/the_best_dog.jpg";
+
+  public _textPlay: string = "JOUER";
+  public _textPlaySingle: string = "Jouer en simple";
+  public _textPlayMulti: string = "Jouer en multijoueur";
 
   public constructor() {
     // default constructor
