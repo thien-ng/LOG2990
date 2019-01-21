9531bb868a43787bc0131a181770a814e97bba10
jeremy <jeremyyoyo@hotmail.fr>
2019-01-21 15:57:01 -0500

    JB R: Ajouter une toolbar fixe dans admin au lieu d'etre dans main-side-nav

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material";
 
 import { CreateSimpleGameComponent } from "../create-simple-game/create-simple-game.component";
 
@@ -9,12 +10,27 @@ import { CreateSimpleGameComponent } from "../create-simple-game/create-simple-g
   entryComponents: [CreateSimpleGameComponent],
 })
 export class AdminComponent implements OnInit {
-  public constructor() {
+  public constructor(public dialog: MatDialog) {
     /* default constructor */
   }
 
   public ngOnInit(): void {
     /* default ngOnInit */
+  }
+
+  public openDialog(): void {
+
+    const dialogConfig: MatDialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+
+    const dialogRef: MatDialogRef<CreateSimpleGameComponent> = this.dialog.open(CreateSimpleGameComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      // à faire - envoyer les données au serveur
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
--- client/src/app/main-nav/main-nav.component.ts
+++ client/src/app/main-nav/main-nav.component.ts
@@ -1,11 +1,8 @@
 import { Breakpoints, BreakpointObserver } from "@angular/cdk/layout";
 import { Component } from "@angular/core";
-import { MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material";
-import { Router } from "@angular/router";
 
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { CreateSimpleGameComponent } from "../create-simple-game/create-simple-game.component";
 
 @Component({
   selector: "app-main-nav",
@@ -24,28 +21,5 @@ export class MainNavComponent {
     .observe(Breakpoints.Handset)
     .pipe(map((event) => event.matches));
 
-  public constructor(
-    private breakpointObserver: BreakpointObserver,
-    public dialog: MatDialog,
-    private router: Router,
-  ) {}
-
-  public routeIncludes(r: string): boolean {
-    return this.router.url.includes(r);
-  }
-
-  public openDialog(): void {
-
-    const dialogConfig: MatDialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-
-    const dialogRef: MatDialogRef<CreateSimpleGameComponent> = this.dialog.open(CreateSimpleGameComponent, dialogConfig);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      // à faire - envoyer les données au serveur
-      // console.log(`Dialog result: ${result}`);
-    });
-  }
+  public constructor(private breakpointObserver: BreakpointObserver) {}
 }
