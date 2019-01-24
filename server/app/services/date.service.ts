e8cfc3e9fb6a42c1fc716a9645bfbf131d3b1d47
Michael Sauget <michaelsauget@gmail.com>
2019-01-24 00:32:18 -0500

    MS B: Les problèmes de lint sur le nouveau cadriciel sont réglés. Props à Jerry pour son aide.

--- server/app/services/date.service.ts
+++ server/app/services/date.service.ts
@@ -1,23 +1,18 @@
-// import Axios from "axios";
+import Axios, { AxiosResponse } from "axios";
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
-// import { IDate } from "./IDate";
+import { IDate } from "./IDate";
 
 @injectable()
 export class DateService {
 
-    // Demander au charge de lab comment regler ce criss de lint
     public async currentTime(): Promise<Message> {
-        // const apiResponse: any = await Axios.get<IDate>(`http://worldclockapi.com/api/json/est/now`);
+        const apiResponse: AxiosResponse<IDate> = await Axios.get<IDate>(`http://worldclockapi.com/api/json/est/now`);
 
-        // return {
-        //     title: `Time`,
-        //     body: apiResponse.data.currentDateTime.toString(),
-        // };
         return {
-            title: "fuckthis",
-            body: "fuck that",
+            title: `Time`,
+            body: apiResponse.data.currentDateTime.toString(),
         };
     }
 }
