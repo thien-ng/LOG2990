25b01ab100906967a06e836192b29517219b570a
Thienng97 <duc-thien@sympatico.ca>
2019-01-24 20:06:11 -0500

    DN D: add websocket connection function

--- client/src/app/websocket/websocket.service.ts
+++ client/src/app/websocket/websocket.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from "rxjs/Observable";
+import * as Rx from "rxjs/Rx";
+import { environment } from "../environments/environment";
+import { R3ExpressionFactoryMetadata } from '@angular/compiler/src/render3/r3_factory';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
 
+  private _socket;  //socket that connects to out socket.io server
+
   constructor() { }
+
+  public connect(): Rx.Subject<MessageEvent>{
+    this._socket = io(environment.ws_url);
+
+    let observable = new Observable(observer => {
+      this._socket.on("message", (data) => {
+        console.log("received a message from websocket server");
+        observer.next(data);
+      })
+      return () => {
+        this._socket.disconnect();
+      }
+    })
+
+    let observer = {
+        next: (data: Object) => {
+          this._socket.emit("message", JSON.stringify(data));
+        },
+    };
+
+    return Rx.Subject.create(observer, observable);
+
+  }
 }
