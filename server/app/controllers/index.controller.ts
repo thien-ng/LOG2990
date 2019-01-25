f7da09a5bbf0c662c933237d7de3377e346f9a49
Michael Sauget <michaelsauget@gmail.com>
2019-01-25 03:26:30 -0500

    MS D: Le serveur peut servir les asset par /asset/<nomFichier>.

--- server/app/controllers/index.controller.ts
+++ server/app/controllers/index.controller.ts
@@ -24,6 +24,15 @@ export class IndexController {
                 res.json(this.indexService.about());
             });
 
+        router.get("/asset/:filename", (req: Request, res: Response, next: NextFunction) => {
+                // Send the request to the service and send the response
+                const file: string = req.params.filename;
+                // tslint:disable-next-line:no-any
+                const path: any = require("path");
+                const url: string = path.join(__dirname, "../../../../app/asset/images/" + file);
+                res.sendFile(url);
+            });
+
         return router;
     }
 }
