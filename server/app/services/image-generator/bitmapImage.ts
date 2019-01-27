c6075db49faed5fd5f4dba0c06195d500263e668
Thienng97 <duc-thien@sympatico.ca>
2019-01-27 17:02:45 -0500

    DN D: add new image for testing

---
+++ server/app/services/image-generator/bitmapImage.ts
@@ -0,0 +1,7 @@
+export interface BitmapImage {
+
+    width: number;
+    height: number;
+    data: number[];
+
+}
--- server/app/services/image-generator/services/generatorImageManager.service.ts
+++ server/app/services/image-generator/services/generatorImageManager.service.ts
@@ -1,56 +1,36 @@
 import { injectable } from "inversify";
+import { BitmapImage } from "../bitmapImage";
 
-// var fs = require('fs');
-// var bmp = require("bmp-js");
-var Jimp = require('jimp');
+
+const filePath3: String  = "C:\\Users\\Thien\\Documents\\Projet_2\\Projet_Integrateur_Log2990\\server\\app\\asset\\image\\testBitmap\\corners.bmp";
+// const filePath4: String  = "C:\\Users\\Thien\\Documents\\Projet_2\\Projet_Integrateur_Log2990\\server\\app\\asset\\image\\testBitmap\\blackTest.bmp";
+// const filePath5: String  = "C:\\Users\\Thien\\Documents\\Projet_2\\Projet_Integrateur_Log2990\\server\\app\\asset\\image\\testBitmap\\whiteTest.bmp";
 
 @injectable()
 export class GeneratorImageManager {
 
-    // private _jimp = require("jimp");
-
-    // private _bmp = require("bmp-js");
+    private _jimp = require("Jimp");
+    private _bitmapImage: BitmapImage;
 
     public constructor(){
         //default constructor
-    }
-    
+    }    
 
     public readFile(){
 
-        // const filePath1: String  = "C:\\Users\\Thien\\Documents\\Projet_2\\Projet_Integrateur_Log2990\\server\\app\\asset\\image\\testBitmap\\white.bmp";
-        // const filePath2: String  = "C:\\Users\\Thien\\Documents\\Projet_2\\Projet_Integrateur_Log2990\\server\\app\\asset\\image\\testBitmap\\7dots.bmp";
-        const filePath3: String  = "C:\\Users\\Thien\\Documents\\Projet_2\\Projet_Integrateur_Log2990\\server\\app\\asset\\image\\testBitmap\\corners.bmp";
+        let array: number[];
 
-        // let bmpbuffer1 = fs.readFileSync(filePath1);
-        // let bmpData1 = bmp.decode(bmpbuffer1);
-
-        // let bmpbuffer2 = fs.readFile(filePath2);
-        // let bmpData2 = bmp.decode(bmpbuffer2);
-
-        // // const dv: DataView = new DataView(bmpData.data);
-        // console.log(bmpData1.data);
-        // console.log(bmpData1.width);
-        // console.log(bmpData1.height);
-
-        // console.log(bmpData2.data);
-        // console.log(bmp.encode(bmpData1));
-        // console.log(bmp.encode(bmpData2));
-        // console.log(bmpData.colors);
-        // console.log(dv);
-
-        let array: any[];
-
-        Jimp.read(filePath3).then( (image: any) => {
-            console.log("buffer de image blanc avec dif:");
+        this._jimp.read(filePath3).then( (image: any) => {
+            console.log("corners");
             console.log(image.bitmap.data);
             array = image.bitmap.data;
             array.forEach((element) => {
+                console.log(typeof element);
                 console.log("value: " + element);
             });
+            console.log(image.bitmap.width);
+            console.log(image.bitmap.height);
         });
-
-
     }
 
 
