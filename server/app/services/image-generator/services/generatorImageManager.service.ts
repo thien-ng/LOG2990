498365bc3f591e6a0070b63ba8d06403061e032c
Thienng97 <duc-thien@sympatico.ca>
2019-01-27 01:09:07 -0500

    DN D: add class for generation management

---
+++ server/app/services/image-generator/services/generatorImageManager.service.ts
@@ -0,0 +1,23 @@
+
+export class GeneratorImageManager {
+
+    private _jimp = require("jimp");
+
+    private _bmp = require("bmp-js");
+
+    public constructor(){
+        //default constructor
+    }
+
+    public readFile(){
+
+        const fileReader : FileReader = new FileReader();
+        fileReader.onload = () => {
+
+        }
+
+    }
+
+
+
+}
