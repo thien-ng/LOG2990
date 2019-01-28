19f279ee2192a8df2ac4b0d9f828244d8287a90b
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-01-27 21:01:17 -0500

    GHV D: ajout de tests valides pour login-validator client (mock)

--- client/src/app/login/login-validator.service.spec.ts
+++ client/src/app/login/login-validator.service.spec.ts
@@ -1,38 +1,86 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
+import {FormControl, Validators} from "@angular/forms";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TestingImportsModule } from "../testing-imports/testing-imports.module";
 
-import { LoginValidatorService } from "./login-validator.service";
+import { Constants } from "../constants";
 
-describe("LoginValidatorService", () => {
+class MockLoginValidatorService {
 
-  let component: LoginValidatorService;
-  let fixture: ComponentFixture<LoginValidatorService>;
+  public _usernameFormControl: FormControl = new FormControl("", [
+    Validators.required,
+    Validators.pattern(Constants.REGEX_PATTERN),
+    Validators.minLength(Constants.MIN_LENGTH),
+    Validators.maxLength(Constants.MAX_LENGTH),
+  ]);
+
+  public addUsername(): Boolean {
+    if (this._usernameFormControl.errors == null) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+describe("Tests on LoginValidatorService", () => {
+
+  let service: MockLoginValidatorService;
 
   beforeEach(() => {
 
   TestBed.configureTestingModule({
-    providers: [LoginValidatorService],
     imports: [
       RouterTestingModule,
       TestingImportsModule,
     ],
-    declarations: [LoginValidatorService],
   });
 
-  fixture = TestBed.createComponent(LoginValidatorService);
-
-  component = fixture.componentInstance;
+  service = new MockLoginValidatorService();
   });
 
-  it("should be created", () => {
-    const service: LoginValidatorService = TestBed.get(LoginValidatorService);
-    expect(service).toBeTruthy();
+  it("should be invalid when form empty", () => {
+    service._usernameFormControl.setValue("");
+    expect(service._usernameFormControl.valid).toBeFalsy();
   });
 
-  it("form invalid when empty", () => {
-    component._usernameFormControl.setValue("");
-    expect(component._usernameFormControl.valid).toBeFalsy();
+  it("should be invalid when form has less than 4 chars", () => {
+    service._usernameFormControl.setValue("123");
+    expect(service._usernameFormControl.valid).toBeFalsy();
+  });
+
+  it("should be valid when form has between 4-15 chars (inclusive)", () => {
+    service._usernameFormControl.setValue("12345");
+    expect(service._usernameFormControl.valid).toBeTruthy();
+
+    service._usernameFormControl.setValue("12345678");
+    expect(service._usernameFormControl.valid).toBeTruthy();
+
+    service._usernameFormControl.setValue("12345678901234");
+    expect(service._usernameFormControl.valid).toBeTruthy();
+
+    service._usernameFormControl.setValue("123456789012345");
+    expect(service._usernameFormControl.valid).toBeTruthy();
+  });
+
+  it("should be invalid when form has more than 15 chars", () => {
+    service._usernameFormControl.setValue("1234567890123456");
+    expect(service._usernameFormControl.valid).toBeFalsy();
+  });
+
+  it("should be invalid when form has chars other than alphanumericals", () => {
+    service._usernameFormControl.setValue("test@");
+    expect(service._usernameFormControl.valid).toBeFalsy();
+  });
+
+  it("should be invalid when form has chars other than alphanumericals", () => {
+    service._usernameFormControl.setValue("test with space");
+    expect(service._usernameFormControl.valid).toBeFalsy();
+  });
+
+  it("should be invalid when form has chars other than alphanumericals", () => {
+    service._usernameFormControl.setValue("test.test");
+    expect(service._usernameFormControl.valid).toBeFalsy();
   });
 
 });
