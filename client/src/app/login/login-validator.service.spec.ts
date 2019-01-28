2c560bef971ddd66dced659202d804a2027ead84
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-01-27 19:22:37 -0500

    GHV D: developpement des tests pour login-validator du client

--- client/src/app/login/login-validator.service.spec.ts
+++ client/src/app/login/login-validator.service.spec.ts
@@ -1,21 +1,38 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TestingImportsModule } from "../testing-imports/testing-imports.module";
 
 import { LoginValidatorService } from "./login-validator.service";
 
 describe("LoginValidatorService", () => {
-  beforeEach(() => TestBed.configureTestingModule({
+
+  let component: LoginValidatorService;
+  let fixture: ComponentFixture<LoginValidatorService>;
+
+  beforeEach(() => {
+
+  TestBed.configureTestingModule({
     providers: [LoginValidatorService],
     imports: [
       RouterTestingModule,
       TestingImportsModule,
     ],
-  }));
+    declarations: [LoginValidatorService],
+  });
+
+  fixture = TestBed.createComponent(LoginValidatorService);
+
+  component = fixture.componentInstance;
+  });
 
   it("should be created", () => {
     const service: LoginValidatorService = TestBed.get(LoginValidatorService);
     expect(service).toBeTruthy();
+  });
+
+  it("form invalid when empty", () => {
+    component._usernameFormControl.setValue("");
+    expect(component._usernameFormControl.valid).toBeFalsy();
   });
 
 });
