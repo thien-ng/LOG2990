99bddff1edbe61f321e58c5b2a40d85a2672a153
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-01-30 21:47:21 -0500

    GHV R: removed _

--- server/app/controllers/loginValidator.controller.ts
+++ server/app/controllers/loginValidator.controller.ts
@@ -8,14 +8,14 @@ import Types from "../types";
 @injectable()
 export class LoginValidatorController {
 
-    public constructor(@inject(Types.NameValidatorService) private _nameValidatorService: NameValidatorService) { }
+    public constructor(@inject(Types.NameValidatorService) private nameValidatorService: NameValidatorService) { }
 
     public get router(): Router {
 
         const router: Router = Router();
 
         router.post("/newUsername", (req: Request, res: Response, next: NextFunction) => {
-            const isValidated: Boolean = this._nameValidatorService.validateName(req.body.body);
+            const isValidated: Boolean = this.nameValidatorService.validateName(req.body.body);
             res.json(isValidated);
         });
 
