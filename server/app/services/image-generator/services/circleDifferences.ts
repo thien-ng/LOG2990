23d34ac9bffe6049a2c028502064b1dfdc0bebf0
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-01-30 23:01:27 -0500

    CT R: suppression code inutile pour le cas ou on aurait traite points en 2D

--- server/app/services/image-generator/services/circleDifferences.ts
+++ server/app/services/image-generator/services/circleDifferences.ts
@@ -1,8 +1,3 @@
-// interface IPosition2D {
-//     posX: number;
-//     posY: number;
-// }
-
 export class CircleDifferences {
 
     private circledDifferenceList: number[];
@@ -44,38 +39,6 @@ export class CircleDifferences {
             index++;
         });
     }
-    // // test affichage a delete
-    // private printPositionSquareToProcess(): void {
-    //     let indexArray: number = 0;
-    //     this.differencesArray.forEach((value: number) => {
-    //         if (value === 1) {
-    //             const startPosition: IPosition2D = this.getStartPositionSquare(indexArray);
-    //             console.log("Start Position:\t [ " + startPosition.posX + ", " + startPosition.posY + " ]");
-    //             const actualPosition: IPosition2D = this.getPosition(indexArray);
-    //             console.log("Actual Position: [ " + actualPosition.posX + ", " + actualPosition.posY + " ]");
-    //             const endPosition: IPosition2D = this.getEndPositionSquare(indexArray);
-    //             console.log("End Position:\t [ " + endPosition.posX + ", " + endPosition.posY + " ]");
-    //         }
-    //         indexArray++;
-    //     });
-    // }
-
-    // private getPosition2D(positionInArray: number): IPosition2D {
-    //     let coordX: number = (positionInArray % this.width);
-    //     const coordY: number = Math.floor(positionInArray / this.width);
-
-    //     // Pour gerer modulo negatif
-    //     coordX = coordX >= 0 ? coordX : coordX + this.width;
-
-    //     return {
-    //         posX: coordX,
-    //         posY: coordY,
-    //     };
-    // }
-
-    // private getPosition1D(position2D: IPosition2D): number {
-    //     return position2D.posY * this.width + position2D.posX;
-    // }
 
     private findDistanceBetween(centerPosition: number, periphericPosition: number): number {
 
