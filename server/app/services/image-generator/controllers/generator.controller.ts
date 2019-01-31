67beb04e01245a99c2e673e25b336f86340a27d0
Thienng97 <duc-thien@sympatico.ca>
2019-01-31 15:12:04 -0500

    DN R: refactor  bs stuff

--- server/app/services/image-generator/controllers/generator.controller.ts
+++ server/app/services/image-generator/controllers/generator.controller.ts
@@ -1,8 +1,6 @@
 
-import { NextFunction, Request, Response, Router, RequestHandler } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { injectable } from "inversify";
-import * as multer from "multer";
-import * as Jimp from "jimp";
 
 @injectable()
 export class GeneratorController {
@@ -14,27 +12,11 @@ export class GeneratorController {
     public get router(): Router {
         const router: Router = Router();
 
-        const upload: multer.Instance = multer({
-            dest: "./import",
-        });
-
-       const receivedFile: RequestHandler = upload.fields([{name: "originalImage",  maxCount: 1}, {name: "modifiedImage",  maxCount: 1}]);
-
-       router.post("/receive", receivedFile, (req: Request, res: Response, next: NextFunction) => {
-
-           const originalBuffer: Buffer = req.files["originalImage"][0].buffer;
-           const modifiedBuffer: Buffer = req.files["modifiedImage"][0].buffer;
-
-            Jimp.read(originalBuffer)
-            .then(tpl => (tpl.clone().write("./ducimage.jpg")))
-            .catch(err => {
-                console.error(err);
-            });
-            // image.write("./app/petsauce.jpg");
+        router.post("/receive", (req: Request, res: Response, next: NextFunction) => {
 
            res.json( {
-               "firstImage": originalBuffer,
-               "modifiedImage": modifiedBuffer,
+               "firstImage": "zizi bander",
+               "modifiedImage": "micahel",
            });
        });
 
