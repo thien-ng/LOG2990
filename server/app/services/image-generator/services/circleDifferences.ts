a3079f5bdc9ca980bd50b8a0a7479f912553ba84
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-01-30 23:09:43 -0500

    CT R: cleaning code (lint fix, commentaire, methodes inutiles)

--- server/app/services/image-generator/services/circleDifferences.ts
+++ server/app/services/image-generator/services/circleDifferences.ts
@@ -3,29 +3,8 @@ export class CircleDifferences {
     private circledDifferenceList: number[];
 
     public constructor(public differencesArray: number[], public width: number, public radius: number) {
-        // default constructor
+        // deep copy array
         this.circledDifferenceList = JSON.parse(JSON.stringify(differencesArray));
-    }
-
-    public printToConsole(): void {
-        let index: number = 0;
-        this.differencesArray.forEach((value: number) => {
-            // A ENLEVER
-            process.stdout.write(value.toString());
-            if (++index % this.width === 0) {
-                // A ENLEVER
-                process.stdout.write("\n");
-            }
-        });
-        console.log();
-        this.circledDifferenceList.forEach((value: number) => {
-            // A ENLEVER
-            process.stdout.write(value.toString());
-            if (++index % this.width === 0) {
-                // A ENLEVER
-                process.stdout.write("\n");
-            }
-        });
     }
 
     public circleAllDifferences(): void {
@@ -37,6 +16,29 @@ export class CircleDifferences {
             }
             index++;
         });
+    }
+
+    private drawCircle(positionToCircle: number): void {
+        const squareSize: number = this.radius + this.radius + 1;
+        const startIndex: number = this.getSquareStartIndex(positionToCircle);
+
+        for (let row: number = 0; row < squareSize; row++) {
+            for (let col: number = 0; col < squareSize; col++) {
+
+                const currentPosition: number = row * this.width + col + startIndex;
+                if (currentPosition >= 0 && currentPosition < this.differencesArray.length) {
+                    const currentDistanceToCenter: number = this.findDistanceBetween(positionToCircle, currentPosition);
+
+                    if (this.isInAdjustedRadius(currentDistanceToCenter)) {
+                        this.circledDifferenceList[currentPosition] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private getSquareStartIndex(centerPositionIndex: number): number {
+        return centerPositionIndex - this.width * this.radius - this.radius;
     }
 
     private findDistanceBetween(centerPosition: number, periphericPosition: number): number {
@@ -59,43 +61,4 @@ export class CircleDifferences {
         return this.radius + adjustment;
     }
 
-    private getSquareStartIndex(centerPositionIndex: number): number {
-        return centerPositionIndex - this.width * this.radius - this.radius;
-    }
-
-    private drawCircle(positionToCircle: number): void {
-        const squareSize: number = this.radius + this.radius + 1;
-        const startIndex: number = this.getSquareStartIndex(positionToCircle);
-
-        for (let row: number = 0; row < squareSize; row++) {
-            for (let col: number = 0; col < squareSize; col++) {
-
-                const currentPosition: number = row * this.width + col + startIndex;
-                if (currentPosition >= 0 && currentPosition < this.differencesArray.length) {
-                    const currentDistanceToCenter: number = this.findDistanceBetween(positionToCircle, currentPosition);
-                    console.log("Distance " + currentDistanceToCenter + " for index " + currentPosition);
-                    if (this.isInAdjustedRadius(currentDistanceToCenter)) {
-                        this.circledDifferenceList[currentPosition] = 1;
-                    }
-                }
-            }
-        }
-    }
 }
-
-const RADIUS: number = 3;
-const differencesArray2: number[] = [0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 1,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0 , 0, 0, 0, 0];
-const width2: number = 9;
-const circleDifferences: CircleDifferences = new CircleDifferences(differencesArray2, width2, RADIUS);
-circleDifferences.printToConsole();
-console.log();
-circleDifferences.circleAllDifferences();
-circleDifferences.printToConsole();
