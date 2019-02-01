cdf7b781f43c5a56289bc468690057b819281058
Thienng97 <duc-thien@sympatico.ca>
2019-02-01 10:18:51 -0500

    DN D: find difference in image with bufferarray

--- server/app/services/difference-checker/utilitaries/imagesDifference.ts
+++ server/app/services/difference-checker/utilitaries/imagesDifference.ts
@@ -1,103 +1,51 @@
 import { injectable } from "inversify";
-import * as Jimp from "jimp";
-import { Image } from "./image";
-import { Pixel } from "./pixel";
 
 @injectable()
 export class ImagesDifference {
 
     private readonly VALUE_DIFFERENCE: number = 1;
     private readonly VALUE_EQUAL: number = 0;
-
-    private jimp: Jimp = require("Jimp");
-    private imageOriginal: Image;
-    private imageWithdots: Image;
+    private readonly ERROR_MESSAGE = "size of buffers are not equal";
     private differenceImage: number[];
 
-    public constructor(private requiredHeight: number, private requiredWidth: number) {
+    public constructor() {
         this.differenceImage = [];
     }
 
-    public async searchDifferenceImage(originalBuffer: Buffer, differenceBuffer: Buffer): Promise<number[]> {
+    public searchDifferenceImage(originalBuffer: Buffer, differenceBuffer: Buffer): number[] {
 
-        await this.readFile(originalBuffer, differenceBuffer);
-
-        if (this.hasRequiredDimension(this.imageOriginal) && this.hasRequiredDimension(this.imageWithdots)) {
-
-            this.findDifference();
-
-            return this.differenceImage;
-
-        } else {
-
-            throw new TypeError("Un des images entrees, n'a pas les bonne dimensions");
+        if (this.isBuffersEqualSize(originalBuffer, differenceBuffer)) {
+            throw new TypeError(this.ERROR_MESSAGE);
         }
 
+        this.findDifference(originalBuffer, differenceBuffer);
+        
+        return this.differenceImage;
     }
 
-    private async readFile(originalBuffer: Buffer, differenceBuffer: Buffer): Promise<void> {
-        // We couldn't avoid the code duplication in this method, we're sorry.
-        await this.jimp.read(originalBuffer).then( (image: Jimp) => {
-            this.imageOriginal = this.createImage(
-                                    image.bitmap.height,
-                                    image.bitmap.width,
-                                    image.bitmap.data,
-                                );
-        });
+    private isBuffersEqualSize(originalBuffer: Buffer, modifiedBuffer: Buffer): Boolean {
 
-        await this.jimp.read(differenceBuffer).then( (image: Jimp) => {
-            this.imageWithdots = this.createImage(
-                                    image.bitmap.height,
-                                    image.bitmap.width,
-                                    image.bitmap.data,
-                                );
-        });
+        return originalBuffer.byteLength === modifiedBuffer.byteLength;
     }
 
-    private createImage(height: number, width: number, valueListBuffer: Buffer): Image {
-            const pixelValueList: number[] = Array.prototype.slice.call(valueListBuffer, 0);
+    private findDifference(originalBuffer: Buffer, modifiedBuffer: Buffer): void {
 
-            return new Image(
-                    height,
-                    width,
-                    this.transformToPixel(pixelValueList),
-                );
+        let bufferIndex: number = 0
+        let diffenceListIndex: number = 0;
+        while (bufferIndex < originalBuffer.byteLength) {
+
+            if (originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex++] &&
+                originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex++] &&
+                originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex++] ) {
+
+                this.differenceImage[diffenceListIndex++] = this.VALUE_EQUAL;
+
+            } else {
+
+                this.differenceImage[diffenceListIndex++] = this.VALUE_DIFFERENCE;
+            }
         }
 
-    private transformToPixel(data: number[]): Pixel[] {
-
-        const arrayPixel: Pixel[] = [];
-
-        let pixelCounter: number = 0;
-        while (pixelCounter < data.length) {
-            const redValue: number = data[pixelCounter++];
-            const greenValue: number = data[pixelCounter++];
-            const blueValue: number = data[pixelCounter++];
-
-            // decommenter si jimp redonne des alpha
-            // pixelCounter++
-
-            const pixel: Pixel = new Pixel(redValue, greenValue, blueValue);
-            arrayPixel.push(pixel);
-        }
-
-        return arrayPixel;
-    }
-
-    private findDifference(): void {
-
-        const imageOriginal: Pixel[] = this.imageOriginal.getPixels();
-        const imageDots: Pixel[] = this.imageWithdots.getPixels();
-
-        for (let i: number = 0; i < this.imageOriginal.getPixels().length; i++) {
-            this.differenceImage[i] = imageOriginal[i].isEqual(imageDots[i]) ? this.VALUE_EQUAL : this.VALUE_DIFFERENCE;
-        }
-
-    }
-
-    private hasRequiredDimension(image: Image): Boolean {
-        return image.getHeight() === this.requiredHeight &&
-                image.getWidth() === this.requiredWidth;
     }
 
 }
