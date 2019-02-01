5515bb3da4027b8e4c11902300e73a2085ac06d8
Thienng97 <duc-thien@sympatico.ca>
2019-02-01 08:20:26 -0500

    DN T: manque 2 test non fonctionnel

--- client/src/app/game-list-container/game-mode.service.spec.ts
+++ client/src/app/game-list-container/game-mode.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TestingImportsModule } from ".././testing-imports/testing-imports.module";
-
+import { Router } from "@angular/router";
 import { GameModeService } from "./game-mode.service";
+import { mock } from "ts-mockito";
 
-describe("GameModeService", () => {
+let gameModeService: GameModeService;
+let router: Router;
+
+fdescribe("GameModeService", () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [GameModeService],
     imports: [
@@ -13,8 +17,27 @@ describe("GameModeService", () => {
     ],
   }));
 
+  beforeEach(() => {
+    router = mock(Router);
+    gameModeService = new GameModeService(router);
+  });
+
+
   it("should be created", () => {
     const service: GameModeService = TestBed.get(GameModeService);
     expect(service).toBeTruthy();
   });
+
+  it("should be 0 if getIndex is called", () => {
+    const result = gameModeService.getIndex();
+    expect(result).toBe(0);
+  });
+
+  // it("should be 1 if toggle, then getIndex is called", () => {
+  //   const result = gameModeService.getIndex();
+  //   gameModeService.toggle();
+  //   gameModeService.toggle();
+  //   expect(result).toBe(1);
+  // });
+
 });
