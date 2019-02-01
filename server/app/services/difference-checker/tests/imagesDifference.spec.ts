84a8ed63c7f1ad0ed19e40e09b6f719e8a4e8a30
Thienng97 <duc-thien@sympatico.ca>
2019-02-01 10:50:23 -0500

    DN R: refactor constant variable

---
+++ server/app/services/difference-checker/tests/imagesDifference.spec.ts
@@ -0,0 +1,76 @@
+import { expect } from "chai";
+import { ImagesDifference } from "../utilitaries/imagesDifference";
+
+// tslint:disable:no-magic-numbers
+
+let imagesDifference: ImagesDifference;
+
+describe("Images Difference finder microservice tests", () => {
+
+    beforeEach(() => {
+        imagesDifference = new ImagesDifference();
+    });
+
+    it("test", (done: Function) => {
+
+        const strBuff1: string = "ffffff";
+        const strBuff2: string = "000000";
+        const buffer1: Buffer = Buffer.from(strBuff1, "hex");
+        const buffer2: Buffer = Buffer.from(strBuff2, "hex");
+
+        const result = imagesDifference.searchDifferenceImage(buffer1, buffer2);
+
+        expect(result).to.deep.equal([1]);
+
+        done();
+    });
+
+    it("test", (done: Function) => {
+
+        const strBuff1: string = "ffffff";
+        const strBuff2: string = "ffffff";
+        const buffer1: Buffer = Buffer.from(strBuff1, "hex");
+        const buffer2: Buffer = Buffer.from(strBuff2, "hex");
+
+        const result = imagesDifference.searchDifferenceImage(buffer1, buffer2);
+
+        expect(result).to.deep.equal([0]);
+
+        done();
+    });
+
+    it("test", (done: Function) => {
+
+        const strBuff1: string = "ffffff000000ffffff";
+        const strBuff2: string = "ffffffffffffffffff";
+        const buffer1: Buffer = Buffer.from(strBuff1, "hex");
+        const buffer2: Buffer = Buffer.from(strBuff2, "hex");
+
+        const result = imagesDifference.searchDifferenceImage(buffer1, buffer2);
+
+        expect(result).to.deep.equal([0,1,0]);
+
+        done();
+    });
+
+    it("test", (done: Function) => {
+
+        const strBuff1: string = "ffffff";
+        const strBuff2: string = "ffffffffffff";
+        const buffer1: Buffer = Buffer.from(strBuff1, "hex");
+        const buffer2: Buffer = Buffer.from(strBuff2, "hex");
+
+        try{
+
+            imagesDifference.searchDifferenceImage(buffer1, buffer2);
+
+        }catch(error){
+            if(error instanceof TypeError){
+                expect(error.message).to.deep.equal("size of buffers are not equal");
+            }
+        }
+        done();
+    });
+
+
+});
--- server/app/services/difference-checker/utilitaries/imagesDifference.ts
+++ server/app/services/difference-checker/utilitaries/imagesDifference.ts
@@ -3,6 +3,9 @@ import { injectable } from "inversify";
 @injectable()
 export class ImagesDifference {
 
+    private readonly GREEN_SHIFT = 1;
+    private readonly BLUE_SHIFT = 2;
+    private readonly VALUE_NEXT_PIXEL = 3;
     private readonly VALUE_DIFFERENCE: number = 1;
     private readonly VALUE_EQUAL: number = 0;
     private readonly ERROR_MESSAGE = "size of buffers are not equal";
@@ -14,7 +17,7 @@ export class ImagesDifference {
 
     public searchDifferenceImage(originalBuffer: Buffer, differenceBuffer: Buffer): number[] {
 
-        if (this.isBuffersEqualSize(originalBuffer, differenceBuffer)) {
+        if (this.buffersNotEqualSize(originalBuffer, differenceBuffer)) {
             throw new TypeError(this.ERROR_MESSAGE);
         }
 
@@ -23,27 +26,28 @@ export class ImagesDifference {
         return this.differenceImage;
     }
 
-    private isBuffersEqualSize(originalBuffer: Buffer, modifiedBuffer: Buffer): Boolean {
+    private buffersNotEqualSize(originalBuffer: Buffer, modifiedBuffer: Buffer): Boolean {
 
-        return originalBuffer.byteLength === modifiedBuffer.byteLength;
+        return originalBuffer.byteLength !== modifiedBuffer.byteLength;
     }
 
     private findDifference(originalBuffer: Buffer, modifiedBuffer: Buffer): void {
 
         let bufferIndex: number = 0
-        let diffenceListIndex: number = 0;
+        let differenceListIndex: number = 0;
         while (bufferIndex < originalBuffer.byteLength) {
 
-            if (originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex++] &&
-                originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex++] &&
-                originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex++] ) {
+            if (originalBuffer[bufferIndex] === modifiedBuffer[bufferIndex] &&
+                originalBuffer[bufferIndex + this.GREEN_SHIFT] === modifiedBuffer[bufferIndex + + this.GREEN_SHIFT] &&
+                originalBuffer[bufferIndex + this.BLUE_SHIFT] === modifiedBuffer[bufferIndex + this.BLUE_SHIFT] ) {
 
-                this.differenceImage[diffenceListIndex++] = this.VALUE_EQUAL;
+                this.differenceImage[differenceListIndex++] = this.VALUE_EQUAL;
 
             } else {
 
-                this.differenceImage[diffenceListIndex++] = this.VALUE_DIFFERENCE;
+                this.differenceImage[differenceListIndex++] = this.VALUE_DIFFERENCE;
             }
+            bufferIndex += this.VALUE_NEXT_PIXEL;
         }
 
     }
