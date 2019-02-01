ca3ef212c76ed36527314afddc89bac28be1e457
Michael Sauget <michaelsauget@gmail.com>
2019-02-01 00:02:17 -0500

    MS R: Refactor de l'architecture des dossiers du microservice de génération des différences.

---
+++ server/app/services/image-generator/image-generator.service.ts
@@ -0,0 +1,11 @@
+import { /* inject, */ injectable } from "inversify";
+// import Types from "../../types";
+
+@injectable()
+export class ImageGeneratorService {
+
+    public constructor() {
+    // default constructor
+    }
+
+}
