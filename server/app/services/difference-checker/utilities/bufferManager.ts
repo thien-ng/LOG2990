8b3b9d5d3334a92c08108482d5d13cc44e12f859
Thienng97 <duc-thien@sympatico.ca>
2019-02-02 16:25:01 -0500

    DN R: change format of BufferManager

--- server/app/services/difference-checker/utilities/bufferManager.ts
+++ server/app/services/difference-checker/utilities/bufferManager.ts
@@ -6,25 +6,7 @@ const HEADER_SIZE: number =  54; // size in Bytes
 @injectable()
 export class BufferManager {
 
-    public mergeBuffers(header: Buffer, image: Buffer): Buffer {
-        const b: string =  header.toString(Constants.BUFFER_FORMAT) + image.toString(Constants.BUFFER_FORMAT);
-
-        return Buffer.from(b, Constants.BUFFER_FORMAT);
-    }
-
-    public arrayToBuffer(array: number[]): Buffer {
-        let stringBuffer: string = "";
-        array.forEach((element: number) => {
-            if (element === 0) {
-                stringBuffer += Constants.WHITE_PIXEL;
-            } else {
-                stringBuffer += Constants.BLACK_PIXEL;
-            }
-        });
-
-        return Buffer.from(stringBuffer, Constants.BUFFER_FORMAT);
-    }
-
+    
     public splitHeader(input: Buffer): Buffer[] {
 
         const jsonBuffer: string = JSON.stringify(input);
@@ -41,6 +23,25 @@ export class BufferManager {
 
         return buffers;
 
+    }
+
+    public arrayToBuffer(array: number[]): Buffer {
+        let stringBuffer: string = "";
+        array.forEach((element: number) => {
+            if (element === 0) {
+                stringBuffer += Constants.WHITE_PIXEL;
+            } else {
+                stringBuffer += Constants.BLACK_PIXEL;
+            }
+        });
+
+        return Buffer.from(stringBuffer, Constants.BUFFER_FORMAT);
+    }
+
+    public mergeBuffers(header: Buffer, image: Buffer): Buffer {
+        const b: string =  header.toString(Constants.BUFFER_FORMAT) + image.toString(Constants.BUFFER_FORMAT);
+
+        return Buffer.from(b, Constants.BUFFER_FORMAT);
     }
 
 }
