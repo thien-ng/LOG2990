46afd546ab78f4600a130cfcf9535fe8c0c820df
Thienng97 <duc-thien@sympatico.ca>
2019-02-02 17:47:35 -0500

    DN T: cant read bmp file yet

--- server/app/services/difference-checker/tests/bufferManager.spec.ts
+++ server/app/services/difference-checker/tests/bufferManager.spec.ts
@@ -1,13 +1,47 @@
+import { expect } from "chai";
+import "reflect-metadata";
 import { BufferManager } from "../utilities/bufferManager";
+import * as fs from "fs";
 
 let bufferManager: BufferManager;
 
 describe("BufferManager tests", () => {
 
-    it("test", (done: Function) => {
+    beforeEach(() => {
+        bufferManager = new BufferManager();
+    });
 
+    it("should split buffer into 2 parts", (done: Function) => {
+        fs.readFile("../../../asset/image/testBitmap/imagetestOg.bmp", (err, data) => {
+            console.log(data);
+        });
+        // const result: Buffer[] = bufferManager.splitHeader(testImage);
+
+        // expect(result.length).to.equal(2);
 
         done();
     });
+
+    it("should transform number array of number to buffer", (done: Function) => {
+            const arrayNumber: number[] = [0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0];
+            const result: Buffer = bufferManager.arrayToBuffer(arrayNumber);
+
+            expect(result).instanceOf(Buffer);
+    
+            done();
+    });
+
+    it("should merge 2 buffers", (done: Function) => {
+        const strBuff1: string = "ffffff";
+        const strBuff2: string = "000000";
+        const buffer1: Buffer = Buffer.from(strBuff1, "hex");
+        const buffer2: Buffer = Buffer.from(strBuff2, "hex");
+
+        const result: Buffer = bufferManager.mergeBuffers(buffer1, buffer2);
+
+        expect(result).instanceOf(Buffer);
+
+        done();
+});
 
 });
