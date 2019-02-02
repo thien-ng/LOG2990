514b34eee53b4317f81a74a75a09238d0e15ed33
Syn92 <35808535+Syn92@users.noreply.github.com>
2019-02-02 18:58:20 -0500

    AG T: 100% coverage gameValidatorService + fix PlaceHolder

--- client/src/app/create-simple-game/game-validator.service.spec.ts
+++ client/src/app/create-simple-game/game-validator.service.spec.ts
@@ -2,11 +2,20 @@ import { TestBed } from "@angular/core/testing";
 
 import { FileValidatorService } from "./game-validator.service";
 
-describe("FileValidatorService", () => {
+fdescribe("FileValidatorService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
+  const fileValidatorService: FileValidatorService = new FileValidatorService();
 
   it("should be created", () => {
     const service: FileValidatorService = TestBed.get(FileValidatorService);
     expect(service).toBeTruthy();
+  });
+  it("SHould return false if the type is correct", () => {
+    const blob: Blob = new Blob([], {type: "image/png"});
+    expect(fileValidatorService.validateFile(blob)).toBe(false);
+  });
+  it("SHould return true if the type is correct", () => {
+    const blob: Blob = new Blob([], {type: "image/bmp"});
+    expect(fileValidatorService.validateFile(blob)).toBe(true);
   });
 });
