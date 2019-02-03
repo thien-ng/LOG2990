610616ccba37cd91bec53bebd0081580fc9f29f7
Thienng97 <duc-thien@sympatico.ca>
2019-02-02 20:41:02 -0500

    DN T: change width and height

--- server/app/services/difference-checker/difference-checker.service.spec.ts
+++ server/app/services/difference-checker/difference-checker.service.spec.ts
@@ -3,9 +3,6 @@ import "reflect-metadata";
 import { expect } from "chai";
 import * as fs from "fs";
 import * as path from "path";
-// import { Constants } from "../../../../../client/src/app/constants";
-// import { GameMode, ICard } from "../../../../../common/communication/iCard";
-// import { ICardLists } from "../../../../../common/communication/iCardLists";
 import { Message } from "../../../../common/communication/message";
 import { DifferenceCheckerService } from "./difference-checker.service";
 import { ImageRequirements } from "./utilities/imageRequirements";
@@ -18,12 +15,12 @@ describe("Differece checker service tests", () => {
     const testImageOg: Buffer = fs.readFileSync(path.resolve(__dirname, "../../asset/image/testBitmap/imagetestOg.bmp"));
     const testImageDiff: Buffer = fs.readFileSync(path.resolve(__dirname, "../../asset/image/testBitmap/imagetestDif.bmp"));
 
-    // console.log(testImageOg.length);
-    // console.log(testImageDiff.length);
+    console.log(testImageOg);
+    console.log(testImageDiff);
 
     const requirements: ImageRequirements = {
-        requiredHeight: 640,
-        requiredWidth: 480,
+        requiredHeight: 480,
+        requiredWidth: 640,
         requiredNbDiff: 7,
         originalImage: testImageOg,
         modifiedImage: testImageDiff,
@@ -35,6 +32,6 @@ describe("Differece checker service tests", () => {
 
     it("Should", () => {
         const result: Message | Buffer = differenceCheckerService.generateDifferenceImage(requirements);
-        expect(result).to.be.an("Buffer");
+        expect(result).instanceOf(Buffer);
     });
 });
