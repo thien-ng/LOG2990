c0ed9fc6d9cd50fb1fbf13d04a8e9f3857c0926a
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-02 20:15:56 -0500

    CT T: 100% coverage card-manager.service

--- client/src/app/card-manager.service.spec.ts
+++ client/src/app/card-manager.service.spec.ts
@@ -1,5 +1,9 @@
 import { TestBed } from "@angular/core/testing";
+import "rxjs/add/observable/of";
 
+import { HttpClient } from "@angular/common/http";
+import { mock } from "ts-mockito";
+import { GameMode } from "../../../common/communication/iCard";
 import { CardManagerService } from "./card-manager.service";
 import { TestingImportsModule } from "./testing-imports/testing-imports.module";
 
@@ -11,5 +15,31 @@ describe("CardManager.ServiceService", () => {
   it("should be created", () => {
     const service: CardManagerService = TestBed.get(CardManagerService);
     expect(service).toBeTruthy();
+  });
+});
+
+describe("Card-manager tests", () => {
+  let cardManagerService: CardManagerService;
+  let http: HttpClient;
+  let cardId: number;
+  let gameMode: GameMode;
+
+  beforeEach(() => {
+    http = mock(HttpClient);
+    cardId = 1;
+    gameMode = GameMode.simple;
+    cardManagerService = new CardManagerService(http);
+  });
+
+  it("should return an Observable Object when calling removeCard()", () => {
+    http.get = jasmine.createSpy("get spy");
+    cardManagerService.getCards();
+    expect(http.get).toHaveBeenCalled();
+  });
+
+  it("should return an Observable Object when calling removeCard()", () => {
+    http.delete = jasmine.createSpy("delete spy");
+    cardManagerService.removeCard(cardId, gameMode);
+    expect(http.delete).toHaveBeenCalled();
   });
 });
