794262fe17162dbc136428a322fb0d430c03f02b
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-02 22:43:26 -0500

    CT T: test couverture methode sendMsg

--- client/src/app/socket.service.spec.ts
+++ client/src/app/socket.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from "@angular/core/testing";
-
+// import * as io from "socket.io-client";
 import { SocketService } from "./socket.service";
 
 describe("SocketService", () => {
@@ -8,5 +8,21 @@ describe("SocketService", () => {
   it("should be created", () => {
     const service: SocketService = TestBed.get(SocketService);
     expect(service).toBeTruthy();
+  });
+});
+
+describe("SocketService tests", () => {
+  let socketService: SocketService;
+  // let socket: SocketIOClient.Socket;
+
+  beforeEach(() => {
+    // socket = io("http://url bidon/");
+    socketService = new SocketService();
+  });
+
+  it("should have called socket.emit() when calling sendMsg()", () => {
+    spyOn(socketService["socket"], "emit").and.callThrough();
+    socketService.sendMsg<string>("string", "message bidon qui ne s'enverra nulle part");
+    expect(socketService["socket"].emit).toHaveBeenCalled();
   });
 });
