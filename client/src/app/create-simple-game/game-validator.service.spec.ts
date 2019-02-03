e00e8c9af8f50df5b34064985ab1cd3c3cfaffc8
Syn92 <35808535+Syn92@users.noreply.github.com>
2019-02-02 20:55:55 -0500

    AG R: typo dans la description test

--- client/src/app/create-simple-game/game-validator.service.spec.ts
+++ client/src/app/create-simple-game/game-validator.service.spec.ts
@@ -10,15 +10,15 @@ describe("FileValidatorService", () => {
     const service: FileValidatorService = TestBed.get(FileValidatorService);
     expect(service).toBeTruthy();
   });
-  it("SHould return false if the type is not correct (PNG)", () => {
+  it("Should return false if the type is not correct (PNG)", () => {
     const blob: Blob = new Blob([], {type: "image/png"});
     expect(fileValidatorService.validateFile(blob)).toBe(false);
   });
-  it("SHould return false if the type is not correct (JPG)", () => {
+  it("Should return false if the type is not correct (JPG)", () => {
     const blob: Blob = new Blob([], {type: "image/jpg"});
     expect(fileValidatorService.validateFile(blob)).toBe(false);
   });
-  it("SHould return true if the type is correct (BMP)", () => {
+  it("Should return true if the type is correct (BMP)", () => {
     const blob: Blob = new Blob([], {type: "image/bmp"});
     expect(fileValidatorService.validateFile(blob)).toBe(true);
   });
