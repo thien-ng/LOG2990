c94e629a31ae6e8ecf0caed7d1e4996fa5fc99b7
jeremy <jeremyyoyo@hotmail.fr>
2019-02-02 22:55:03 -0500

    JB R: lint error

--- server/app/services/difference-checker/difference-checker.service.spec.ts
+++ server/app/services/difference-checker/difference-checker.service.spec.ts
@@ -5,7 +5,6 @@ import * as fs from "fs";
 import * as path from "path";
 import { Message } from "../../../../common/communication/message";
 import { DifferenceCheckerService } from "./difference-checker.service";
-import { ImageRequirements } from "./utilities/imageRequirements";
 
 // tslint:disable:no-magic-numbers
 
