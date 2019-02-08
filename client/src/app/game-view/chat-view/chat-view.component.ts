c2f6d7274da2b53da478695e70e7863e8d2a2dc4
Thienng97 <duc-thien@sympatico.ca>
2019-02-08 16:37:55 -0500

    DN D: add display of ngFor directive to show conversations

--- client/src/app/game-view/chat-view/chat-view.component.ts
+++ client/src/app/game-view/chat-view/chat-view.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChatViewService } from "./chat-view.service";
+import { IChat } from "../../../../../common/communication/iChat";
 
 @Component({
   selector: 'app-chat-view',
   templateUrl: './chat-view.component.html',
   styleUrls: ['./chat-view.component.css']
 })
-export class ChatViewComponent implements OnInit {
+export class ChatViewComponent {
 
-  constructor() { }
+  constructor(private chatViewService: ChatViewService) {
+    // default constructor
+   }
 
-  ngOnInit() {
-  }
+   public showConversation(): IChat[] {
+    return this.chatViewService.getConversation();
+   } 
 
 }
--- client/src/app/game-view/chat-view/chat-view.service.ts
+++ client/src/app/game-view/chat-view/chat-view.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { IChat } from "../../../../../common/communication/iChat";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatViewService {
+  
+  private conversation: IChat[] = [
+    {userType: "server", message: "test1"},
+    {userType: "server", message: "test2"},
+    {userType: "server", message: "test3"},
+    {userType: "server", message: "test4"},
+    {userType: "server", message: "test5"},
+    {userType: "server", message: "test5"},
+    {userType: "server", message: "test5"},
+    {userType: "server", message: "test5"},
+  ];
 
   constructor() { }
+
+  public getConversation(): IChat[] {
+    return this.conversation;
+  }
+
 }
