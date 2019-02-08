36df64224396389c60acacea2cde85dbe0236940
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-07 19:47:12 -0500

    GHV R: retrait dencore plus de useless tests

--- client/src/app/login/login-validator.service.spec.ts
+++ client/src/app/login/login-validator.service.spec.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
-import { Observable } from "rxjs";
 import "rxjs/add/observable/of";
 import "rxjs/add/operator/toPromise";
 import { mock } from "ts-mockito";
@@ -35,18 +34,6 @@ describe("Tests on LoginValidatorService", () => {
 
     loginValidatorService.addUsername("validUsername");
     expect(loginValidatorService.addUsername).toHaveBeenCalled();
-  });
-
-  it("should return true on addUsername with VALID username and unique", async () => {
-    spyOn<any>(loginValidatorService, "sendUsernameRequest").and.returnValue(Observable.of("true")).and.callThrough();
-    spyOn<any>(loginValidatorService, "addUsername");
-    expect(loginValidatorService.addUsername("username")).toBe(Observable.of("true"));
-  });
-
-  it("should return false on addUsername with VALID username but NOT unique", async () => {
-    spyOn<any>(loginValidatorService, "sendUsernameRequest").and.returnValue(Observable.of("false")).and.callThrough();
-    spyOn<any>(loginValidatorService, "addUsername");
-    expect(loginValidatorService.addUsername("username")).toBe(Observable.of("false"));
   });
 
 });
