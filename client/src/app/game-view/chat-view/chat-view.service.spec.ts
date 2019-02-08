1d853644085d6d57524195022a528a2f36a8e5ff
Thienng97 <duc-thien@sympatico.ca>
2019-02-08 16:18:21 -0500

    DN D: generate chat-view service

---
+++ client/src/app/game-view/chat-view/chat-view.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ChatViewService } from './chat-view.service';
+
+describe('ChatViewService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ChatViewService = TestBed.get(ChatViewService);
+    expect(service).toBeTruthy();
+  });
+});
