d93801d3f1eda067855096983af0bd6d5b82005e
Thienng97 <duc-thien@sympatico.ca>
2019-02-08 14:21:18 -0500

    DN D: generate chat-view component

---
+++ client/src/app/game-view/chat-view/chat-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-view',
+  templateUrl: './chat-view.component.html',
+  styleUrls: ['./chat-view.component.css']
+})
+export class ChatViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
