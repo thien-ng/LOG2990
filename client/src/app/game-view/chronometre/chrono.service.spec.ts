a2e82b678ae76feaa213d89d518f51d483f98ecf
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-09 14:02:04 -0500

    CT D: fix lint error

--- client/src/app/game-view/chronometre/chrono.service.spec.ts
+++ client/src/app/game-view/chronometre/chrono.service.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { ChronoService } from './chrono.service';
+import { ChronoService } from "./chrono.service";
 
-describe('ChronoService', () => {
+describe("ChronoService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: ChronoService = TestBed.get(ChronoService);
     expect(service).toBeTruthy();
   });
