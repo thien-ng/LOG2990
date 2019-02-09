ad6cff4411e7678a11dcbf2c23bd0b159159ce9d
Thienng97 <duc-thien@sympatico.ca>
2019-02-09 14:58:05 -0500

    DN D: generate game-view service and modify component view

---
+++ client/src/app/game-view/game-view-free/game-view-free.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { GameViewFreeService } from './game-view-free.service';
+
+describe('GameViewFreeService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: GameViewFreeService = TestBed.get(GameViewFreeService);
+    expect(service).toBeTruthy();
+  });
+});
