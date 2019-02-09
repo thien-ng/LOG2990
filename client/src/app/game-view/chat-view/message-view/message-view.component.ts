f3f0bab477122ade69e17daf7418b9a05280eae4
Thienng97 <duc-thien@sympatico.ca>
2019-02-08 21:57:49 -0500

    DN D: generate message view

---
+++ client/src/app/game-view/chat-view/message-view/message-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-view',
+  templateUrl: './message-view.component.html',
+  styleUrls: ['./message-view.component.css']
+})
+export class MessageViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
