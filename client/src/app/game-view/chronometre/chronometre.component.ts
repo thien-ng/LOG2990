1a44d873dd6cc491bbadd1fa498fdf25dc48d72d
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-10 10:25:55 -0500

    CT D: implementation chrono component, chrono fonctionnel

--- client/src/app/game-view/chronometre/chronometre.component.ts
+++ client/src/app/game-view/chronometre/chronometre.component.ts
@@ -5,7 +5,29 @@ import { Component } from "@angular/core";
   styleUrls: ["./chronometre.component.css"],
 })
 export class ChronometreComponent {
+  private readonly INTERVAL: number = 1000;
+  private readonly MINUTE_TO_SECONDS: number = 60;
+  private readonly TWO_DIGITS: number = 10;
+  private readonly START_TIME: string = "00:00";
 
-  public constructor() { /*default*/ }
+  private timeNumber: number = 0;
+  public timeString: string = this.START_TIME;
 
+  public constructor() { this.startTimer(); }
+
+  public startTimer(): void {
+    setInterval(() => {
+                this.timeString = this.convertSeconds(++this.timeNumber);
+    },          this.INTERVAL);
+  }
+
+  private convertSeconds(sec: number): string {
+    const min: number = Math.floor((sec / this.MINUTE_TO_SECONDS));
+    const seconds: number = sec - (min * this.MINUTE_TO_SECONDS);
+
+    let result: string = (min < this.TWO_DIGITS ? "0" + min.toString() : min.toString());
+    result += ":" + (seconds < this.TWO_DIGITS ? "0" + seconds.toString() : seconds.toString());
+
+    return result;
+  }
 }
