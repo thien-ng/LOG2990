f0aa5cc8009a9ac3841495b3042cccd9774bd48b
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-11 23:16:39 -0500

    GHV D: ajout du squelette des fonction a dev

--- server/app/services/scene/scene-builder.ts
+++ server/app/services/scene/scene-builder.ts
@@ -1,1 +1,30 @@
-export class SceneBuilder {}
+export class SceneBuilder {
+
+    private sceneVariables: ISceneVariables;
+
+    public constructor (public sceneOptions: ISceneOptions) { }
+
+    public generateSceneVariables(sceneOptions: ISceneOptions): void {
+        // takes sceneOptions and create the scene variables
+    }
+
+    public setSceneObjectsQuantity(): void {
+
+    }
+
+    public generateSceneObjectsTypes(): void {
+
+    }
+
+    public generatePositions() {
+
+    }
+
+    public generateSceneLights() {
+
+    }
+
+    public generateSceneBackgroundColor() {
+        
+    }
+}
---
+++ server/app/services/scene/sceneConstants.ts
@@ -0,0 +1,7 @@
+export class SceneConstants {
+    // Lights
+
+    // Camera
+
+    // Colors
+}
