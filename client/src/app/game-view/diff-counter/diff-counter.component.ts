b6be7a91aa7a6f24f2a70ec2aeb593a72efeb79c
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-12 10:34:22 -0500

    CT D: ajout attributs, implementation constructeurs de DiffCounterComponent

--- client/src/app/game-view/diff-counter/diff-counter.component.ts
+++ client/src/app/game-view/diff-counter/diff-counter.component.ts
@@ -1,12 +1,29 @@
-import { Component } from "@angular/core";
+import { AfterContentInit, Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-diff-counter",
   templateUrl: "./diff-counter.component.html",
   styleUrls: ["./diff-counter.component.css"],
 })
-export class DiffCounterComponent {
+export class DiffCounterComponent implements AfterContentInit {
+  public readonly DEGREE_CIRCLE: number = 360;
+  public readonly PERCENT: number = 100;
+  public readonly NB_ERROR_MAX: number = 7;
 
-  public constructor() { /*default*/ }
+  @ViewChild("progressCircle", { read: ElementRef })
+  public progressCircle: ElementRef;
+  @ViewChild("progressBarre", { read: ElementRef })
+  public progressBarre: ElementRef;
+
+  private nbErrorMax: number;
+  private convertedErrorToPercent: number;
+
+  public constructor() {
+    this.nbErrorMax = this.NB_ERROR_MAX;
+  }
+
+  public ngAfterContentInit(): void {
+    this.updateSpinner(this.progressCircle.nativeElement, 6);
+  }
 
 }
