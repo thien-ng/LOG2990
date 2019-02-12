61b918bb2fb49b50c260aaf7440ba0aa4a82df89
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-12 12:09:16 -0500

    CT D: export code diffcounter compo vers son service

--- client/src/app/game-view/diff-counter/diff-counter.component.ts
+++ client/src/app/game-view/diff-counter/diff-counter.component.ts
@@ -1,49 +1,25 @@
-import { AfterContentInit, Component, ElementRef, ViewChild } from "@angular/core";
-
+import { AfterContentInit, Component, ElementRef, Inject, ViewChild } from "@angular/core";
+import { DiffCounterService } from "./diff-counter.service";
 @Component({
   selector: "app-diff-counter",
   templateUrl: "./diff-counter.component.html",
   styleUrls: ["./diff-counter.component.css"],
+  providers: [DiffCounterService],
 })
 export class DiffCounterComponent implements AfterContentInit {
-  public readonly DEGREE_CIRCLE: number = 360;
-  public readonly PERCENT: number = 100;
-  public readonly NB_ERROR_MAX: number = 7;
-
   @ViewChild("progressCircle", { read: ElementRef })
   public progressCircle: ElementRef;
   @ViewChild("progressBar", { read: ElementRef })
   public progressBar: ElementRef;
 
-  private convertedErrorToPercent: number;
-
-  public constructor() {
-    // default constructor
+  public constructor(@Inject(DiffCounterService) public diffCounterService: DiffCounterService) {
+    // this.diffCounterService = new DiffCounterService(this.progressCircle.nativeElement, this.progressBar.nativeElement);
   }
 
   public ngAfterContentInit(): void {
-    this.updateSpinner(this.progressCircle.nativeElement, 4);
+    this.diffCounterService = new DiffCounterService(this.progressCircle.nativeElement, this.progressBar.nativeElement);
+
+    this.diffCounterService.updateSpinner(this.progressCircle.nativeElement, 1);
   }
 
-  public updateSpinner(elem: HTMLElement, nbErrorFound: number): void {
-    const angle: number = this.computeAngleSpinner(nbErrorFound);
-    elem.setAttribute("data-value", this.convertedErrorToPercent.toString());
-    elem.setAttribute("id", nbErrorFound.toString());
-
-    if (this.progressBar.nativeElement) {
-      this.progressBar.nativeElement.style.transform = "rotate(" + angle + "deg)";
-    }
-  }
-
-  private convertErrorToPercent(nbErrorFound: number): number {
-    return nbErrorFound * this.PERCENT / this.NB_ERROR_MAX;
-  }
-
-  private computeAngleSpinner(nbErrorFound: number): number {
-    let angle: number;
-    this.convertedErrorToPercent = this.convertErrorToPercent(nbErrorFound);
-    angle = this.convertedErrorToPercent * this.DEGREE_CIRCLE / this.PERCENT;
-
-    return angle;
-  }
 }
--- client/src/app/game-view/diff-counter/diff-counter.service.ts
+++ client/src/app/game-view/diff-counter/diff-counter.service.ts
@@ -1,11 +1,41 @@
-import { Injectable } from "@angular/core";
+import { ElementRef, Injectable } from "@angular/core";
 
 @Injectable({
   providedIn: "root",
 })
 export class DiffCounterService {
+  public readonly DEGREE_CIRCLE: number = 360;
+  public readonly PERCENT: number = 100;
+  public readonly NB_ERROR_MAX: number = 7;
+
+  public progressCircle: ElementRef;
+  public progressBar: ElementRef;
+
+  private convertedErrorToPercent: number;
 
   public constructor() {
     // default constructor
+  }
+
+  public updateSpinner(elem: HTMLElement, nbErrorFound: number): void {
+    const angle: number = this.computeAngleSpinner(nbErrorFound);
+    elem.setAttribute("data-value", this.convertedErrorToPercent.toString());
+    elem.setAttribute("id", nbErrorFound.toString());
+
+    if (this.progressBar.nativeElement) {
+      this.progressBar.nativeElement.style.transform = "rotate(" + angle + "deg)";
+    }
+  }
+
+  private convertErrorToPercent(nbErrorFound: number): number {
+    return nbErrorFound * this.PERCENT / this.NB_ERROR_MAX;
+  }
+
+  private computeAngleSpinner(nbErrorFound: number): number {
+    let angle: number;
+    this.convertedErrorToPercent = this.convertErrorToPercent(nbErrorFound);
+    angle = this.convertedErrorToPercent * this.DEGREE_CIRCLE / this.PERCENT;
+
+    return angle;
   }
 }
