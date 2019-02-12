69690bf6606383f631db5dc1b9b7e4122e1b6791
jeremy <jeremyyoyo@hotmail.fr>
2019-02-11 20:17:17 -0500

    JB T: regler les tests qui ont fail apres le dernier commit

--- server/app/test_services/card-manager.service.spec.ts
+++ server/app/test_services/card-manager.service.spec.ts
@@ -3,15 +3,18 @@ import "reflect-metadata";
 import { expect } from "chai";
 import * as fs from "fs";
 import * as path from "path";
-import { Constants } from "../../../client/src/app/constants";
 import { DefaultCard2D, DefaultCard3D, GameMode, ICard } from "../../../common/communication/iCard";
 import { ICardLists } from "../../../common/communication/iCardLists";
 import { Message } from "../../../common/communication/message";
+import { Constants } from "../constants";
 import { CardManagerService } from "../services/card-manager.service";
 import { HighscoreService } from "../services/highscore.service";
 
-// tslint:disable:no-magic-numbers
+/*tslint:disable no-magic-numbers no-any */
 
+const mockAdapter: any = require("axios-mock-adapter");
+const axios: any = require("axios");
+const mock: any = new mockAdapter(axios);
 const CARD_DELETED: string = "Carte supprimée";
 const CARD_NOT_FOUND: string = "Erreur de suppression, carte pas trouvée";
 const FAKE_PATH: string = Constants.BASIC_SERVICE_BASE_URL + "/image";
@@ -23,7 +26,7 @@ describe("Card-manager tests", () => {
     const testImageOg: Buffer = fs.readFileSync(path.resolve(__dirname, "../asset/image/testBitmap/imagetestOg.bmp"));
 
     const c1: ICard = {
-        gameID: 1,
+        gameID: 4,
         title: "Default 2D",
         subtitle: "default 2D",
         avatarImageUrl: FAKE_PATH + "/elon.jpg",
@@ -42,7 +45,7 @@ describe("Card-manager tests", () => {
 
     const c3: ICard = {
         gameID: 3,
-        title: "Default 3D",
+        title: "Default 3D 2.0",
         subtitle: "default 3D",
         avatarImageUrl: FAKE_PATH + "/poly.jpg",
         gameImageUrl: FAKE_PATH + "/poly.jpg",
@@ -105,16 +108,21 @@ describe("Card-manager tests", () => {
 
     it("corresponding highscore to the gameID should exist", () => {
         cardManagerService.addCard2D(c1);
-        expect(highscoreService.findHighScoreByID(1)).to.be.equal(2);
+        expect(highscoreService.findHighScoreByID(4)).to.be.equal(2);
     });
 
     it("Should return an error message", async () => {
+        mock.onGet("/api/differenceChecker/validate").reply(200, {
+            title: Constants.ON_ERROR_MESSAGE,
+            body: Constants.VALIDATION_FAILED,
+        });
         let messageTitle: string = "";
         await cardManagerService.simpleCardCreationRoutine(testImageOg, testImageOg, "title")
         .then((message: Message) => {
             messageTitle = message.title;
         });
         expect(messageTitle).to.equal("onError");
+        mock.restore();
     });
 
 });
