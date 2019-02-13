05477be575302886b98490fa686a96c0c1f9aba2
Syn92 <35808535+Syn92@users.noreply.github.com>
2019-02-13 17:31:13 -0500

    AG T: 100% coverage activeGameService

--- client/src/app/game-view/active-game.service.spec.ts
+++ client/src/app/game-view/active-game.service.spec.ts
@@ -1,12 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { ActiveGameService } from './active-game.service';
+import { GameMode, ICard } from "../../../../common/communication/iCard";
+import { Constants } from "../constants";
+import { ActiveGameService } from "./active-game.service";
 
-describe('ActiveGameService', () => {
+fdescribe("ActiveGameService Test", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
+  const activeGameService: ActiveGameService = new ActiveGameService();
+  const card: ICard = {
+    gameID: 12,
+    title: "string",
+    subtitle: "string",
+    avatarImageUrl: "string",
+    gameImageUrl: "string",
+    gamemode: GameMode.simple,
+  };
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: ActiveGameService = TestBed.get(ActiveGameService);
     expect(service).toBeTruthy();
+  });
+  it("should return the path of the original image", () => {
+    activeGameService.activeGame = card;
+    const imgPath: string = card.gameImageUrl;
+    expect(activeGameService.originalImage).toEqual(imgPath);
+  });
+  it("should return the path of the modified image", () => {
+    activeGameService.activeGame = card;
+    const imgPath: string = Constants.PATH_TO_IMAGES + "/" + card.gameID + Constants.MODIFIED_FILE;
+    expect(activeGameService.modifiedImage).toEqual(imgPath);
   });
 });
