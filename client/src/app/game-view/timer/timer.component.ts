fb8dafb628791d430ad56ad8ea6d723cdd16d1ad
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-13 12:16:18 -0500

    CT R: deplace convertSecondsToString() dans le service

--- client/src/app/game-view/timer/timer.component.ts
+++ client/src/app/game-view/timer/timer.component.ts
@@ -1,37 +1,29 @@
-import { Component } from "@angular/core";
+import { AfterContentInit, Component, Inject } from "@angular/core";
+import { TimerService } from "./timer.service";
 @Component({
   selector: "app-timer",
   templateUrl: "./timer.component.html",
   styleUrls: ["./timer.component.css"],
 })
-export class TimerComponent {
-  private readonly INTERVAL: number = 1000;
-  private readonly MINUTE_TO_SECONDS: number = 60;
-  private readonly TWO_DIGITS: number = 10;
+export class TimerComponent implements AfterContentInit {
   private readonly START_TIME: string = "00:00";
+  private readonly INTERVAL: number = 1000;
 
-  private timeNumber: number;
   public timeString: string;
+  private timeNumber: number;
 
-  public constructor() {
-    this.timeNumber = 0;
+  public constructor(@Inject(TimerService) public timerService: TimerService) {
     this.timeString = this.START_TIME;
+    this.timeNumber = 0;
+  }
+
+  public ngAfterContentInit(): void {
     this.startTimer();
   }
 
   public startTimer(): void {
     setInterval(() => {
-                this.timeString = this.convertSecondsToString(++this.timeNumber);
+                this.timeString = this.timerService.convertSecondsToString(++this.timeNumber);
     },          this.INTERVAL);
-  }
-
-  private convertSecondsToString(secondsInput: number): string {
-    const min: number = Math.floor((secondsInput / this.MINUTE_TO_SECONDS));
-    const sec: number = secondsInput - (min * this.MINUTE_TO_SECONDS);
-
-    let result: string = (min < this.TWO_DIGITS ? "0" + min.toString() : min.toString());
-    result += ":" + (sec < this.TWO_DIGITS ? "0" + sec.toString() : sec.toString());
-
-    return result;
   }
 }
--- client/src/app/game-view/timer/timer.service.ts
+++ client/src/app/game-view/timer/timer.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class TimerService {
+  private readonly MINUTE_TO_SECONDS: number = 60;
+  private readonly TWO_DIGITS: number = 10;
 
-  public constructor() {}
+  public constructor() {
+    // default constructo
+  }
+
+  public convertSecondsToString(secondsInput: number): string {
+    const min: number = Math.floor((secondsInput / this.MINUTE_TO_SECONDS));
+    const sec: number = secondsInput - (min * this.MINUTE_TO_SECONDS);
+
+    let result: string = (min < this.TWO_DIGITS ? "0" + min.toString() : min.toString());
+    result += ":" + (sec < this.TWO_DIGITS ? "0" + sec.toString() : sec.toString());
+
+    return result;
+  }
 }
