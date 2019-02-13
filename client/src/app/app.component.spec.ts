c0fb6e918c213f82c6a4e8764dd38be591be89ab
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-13 10:55:52 -0500

    CT R: correction path import de DifferenceCounterComponent dans app compo.spec

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -12,7 +12,7 @@ import { CardComponent } from "./card/card.component";
 import { CreateSimpleGameComponent } from "./create-simple-game/create-simple-game.component";
 import { GameListContainerComponent } from "./game-list-container/game-list-container.component";
 import { GameListComponent } from "./game-list/game-list.component";
-import { DifferenceCounterComponent } from "./game-view/diff-counter/difference-counter.component";
+import { DifferenceCounterComponent } from "./game-view/difference-counter/difference-counter.component";
 import { HighscoreDisplayComponent } from "./highscore-display/highscore-display.component";
 import { LoginPageComponent } from "./login/login-page/login-page.component";
 import { LoginValidatorComponent } from "./login/login-validator/login-validator.component";
