7b23de04ae9118c63cde2feaed0ec1219cc5526f
Thienng97 <duc-thien@sympatico.ca>
2019-02-13 20:45:21 -0500

    DN D: add condition for object creation

--- client/src/app/game-view/game-view-free/threejs-view/threejs-view.service.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-view.service.ts
@@ -30,7 +30,8 @@ export class ThreejsViewService {
     this.scene.add(this.ambLight);
     this.createLighting();
 
-    const cube: THREE.Mesh= new THREE.Mesh(new THREE.CubeGeometry(3,3,3), this.material);
+    const cube: THREE.Mesh = new THREE.Mesh(new THREE.CubeGeometry(3,3,3), this.material);
+    cube.rotation.x = Math.PI / 3;
     this.scene.add(cube);
 
     this.camera.lookAt(this.scene.position);
@@ -45,6 +46,48 @@ export class ThreejsViewService {
     this.scene.add(this.light2);
   }
 
+  // private initiateObject(): void {
+
+  //   const tempString: string = "sphere";
+  //   let generatedObject: THREE.Mesh;
+
+  //   switch (tempString) {
+  //     case "sphere": {
+  //       generatedObject = new THREE.Mesh(new THREE.SphereGeometry(1));
+  //       break;
+  //     }
+      
+  //     case "cube": {
+  //       generatedObject = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1));
+  //       break;
+  //     }
+
+  //     case "cone": {
+  //       generatedObject = new THREE.Mesh(new THREE.ConeGeometry(1, 1));
+  //       break;
+  //     }
+
+  //     case "cylinder": {
+  //       generatedObject = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 1));
+  //       break;
+  //     }
+
+  //     case "triangularPyramid": {
+  //       generatedObject = new THREE.Mesh(new THREE.)
+  //       break;
+  //     }
+
+  //     default: {
+  //       console.log("mauvais types");
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // private createObjectColor(): void {
+
+  // }
+
   // to remove
   public angle: number = 100;
   public radius: number = 10;
@@ -52,7 +95,6 @@ export class ThreejsViewService {
     requestAnimationFrame(this.animate.bind(this));
     this.camera.position.x = this.radius * Math.cos( this.angle );  
     this.camera.position.z = this.radius * Math.sin( this.angle );
-    this.camera.position.y = this.radius * Math.cos( this.angle );
     this.angle += 0.01;
 
     this.camera.lookAt(this.scene.position);
