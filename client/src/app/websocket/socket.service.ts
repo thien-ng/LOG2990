fbe6562bc801a372f37f4689fa0b9b5bb3bda93f
Thienng97 <duc-thien@sympatico.ca>
2019-02-14 01:14:57 -0500

    DN D: connect threejs-view.service to socket

--- client/src/app/websocket/socket.service.ts
+++ client/src/app/websocket/socket.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import * as io from "socket.io-client";
 import { IChat } from "../../../../common/communication/iChat";
+import { ISceneVariables } from "../../../../common/communication/iSceneVariables";
 import { Constants } from "../constants";
 import { ChatViewService } from "../game-view/chat-view/chat-view.service";
+import { ThreejsViewService } from "../game-view/game-view-free/threejs-view/threejs-view.service";
 
 @Injectable({
   providedIn: "root",
@@ -12,16 +14,25 @@ export class SocketService {
 
   private socket: SocketIOClient.Socket = io(Constants.WEBSOCKET_URL);
 
-  public constructor(private chatViewService: ChatViewService) {
+  public constructor(
+    private chatViewService: ChatViewService,
+    private threejsViewService: ThreejsViewService) {
+
     this.initWebsocketListener();
   }
 
   public initWebsocketListener(): void {
 
     this.socket.addEventListener(Constants.ON_CONNECT, () => {
+
       this.socket.on(Constants.ON_CHAT_MESSAGE, (data: IChat) => {
         this.chatViewService.updateConversation(data);
       });
+
+      this.socket.on(Constants.ON_GAME_FREE_DATA, (data: ISceneVariables) => {
+        this.threejsViewService.updateSceneVariable(data);
+      });
+
     });
   }
 
