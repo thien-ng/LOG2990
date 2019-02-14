f61c7d9daa5c91a8540435463b1dbf477f6731f0
Michael Sauget <michaelsauget@gmail.com>
2019-02-13 19:54:12 -0500

    MS D: Ajout des constantes définissant la première partie du header d'un BMP.

--- server/app/services/difference-checker/utilities/bmpBuilder.ts
+++ server/app/services/difference-checker/utilities/bmpBuilder.ts
@@ -1,3 +1,8 @@
 export class BMPBuilder {
 
+    private readonly HEADER_SIGNATURE: number = 0x424D; // BM in ascii
+    private readonly HEADER_RESERVED: number = 0;       // unused => 0
+    private readonly HEADER_DATAOFFSET: number = 54;
+    private readonly INFOHEADER_SIZE: number = 40;
+
 }
