a13a37119922bcd73d4b18444c13e340abe0ecea
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-14 17:50:19 -0500

    GHV D: avancement pour fonction de detection collision

--- server/app/services/scene/utilitaries/collision-validator.ts
+++ server/app/services/scene/utilitaries/collision-validator.ts
@@ -2,7 +2,23 @@ import { IAxisValues, ISceneObject } from "../../../../../common/communication/i
 
 export class CollisionValidator {
 
-    public checkForCollision(newPosition: IAxisValues, existingSceneObjects: ISceneObject[]): boolean {
+    public hasCollidingPositions(newSceneObject: ISceneObject, existingSceneObjects: ISceneObject[]): boolean {
 
+        let hasCollision: boolean = false;
+
+        existingSceneObjects.forEach((element: ISceneObject) => {
+
+            if (this.areColliding(newSceneObject.position, element.position)) {
+                // hasCollision =  true;
+                return true;
+            }
+        });
+        return false;
+
+    }
+
+    private areColliding(position1: IAxisValues, position2: IAxisValues): boolean {
+
+        return position1 === position2;
     }
 }
