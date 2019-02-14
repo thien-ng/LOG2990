7cb3e140a085b89bc9791c193356d5e15585910c
Michael Sauget <michaelsauget@gmail.com>
2019-02-14 01:57:00 -0500

    MS R: Changement du type de retour de searchDifferenceImage et ajout de la fonction de création du buffer.

--- server/app/services/difference-checker/utilities/imagesDifference.ts
+++ server/app/services/difference-checker/utilities/imagesDifference.ts
@@ -1,30 +1,44 @@
 import { injectable } from "inversify";
 import { Constants } from "../../../constants";
+import { BMPBuilder } from "./bmpBuilder";
 
 @injectable()
 export class ImagesDifference {
 
-    private readonly GREEN_SHIFT: number = 1;
-    private readonly BLUE_SHIFT: number = 2;
-    private readonly VALUE_NEXT_PIXEL: number = 3;
-    private readonly VALUE_DIFFERENCE: number = 1;
-    private readonly VALUE_EQUAL: number = 0;
-    private differenceImage: number[];
-    private readonly HEADER_SIZE: number = 54;
+    private readonly RED_SHIFT:         number = 0;
+    private readonly GREEN_SHIFT:       number = 1;
+    private readonly BLUE_SHIFT:        number = 2;
+    private readonly VALUE_NEXT_PIXEL:  number = 3;
+    private readonly VALUE_DIFFERENCE:  number = 1;
+    private readonly VALUE_EQUAL:       number = 0;
+    private readonly HEADER_SIZE:       number = 54;
 
-    public constructor() {
-        this.differenceImage = [];
-    }
+    private readonly WIDTH_OFFSET:      number = 18;
+    private readonly HEIGHT_OFFSET:     number = 22;
 
-    public searchDifferenceImage(originalBuffer: Buffer, differenceBuffer: Buffer): number[] {
+    private differenceImage: Buffer;
+
+    public constructor() { /* */ }
+
+    public searchDifferenceImage(originalBuffer: Buffer, differenceBuffer: Buffer): Buffer {
 
         if (this.buffersNotEqualSize(originalBuffer, differenceBuffer)) {
             throw new TypeError(Constants.ERROR_UNEQUAL_DIMENSIONS);
         }
 
+        this.differenceImage = this.generateEmpty24BitBuffer(originalBuffer);
         this.findDifference(originalBuffer, differenceBuffer);
 
         return this.differenceImage;
+    }
+
+    private generateEmpty24BitBuffer(buffer: Buffer): Buffer {
+
+        const width: number = buffer.readUInt32LE(this.WIDTH_OFFSET);
+        const height: number = buffer.readUInt32LE(this.HEIGHT_OFFSET);
+        const bmpBuilder: BMPBuilder = new BMPBuilder(width, height, this.VALUE_EQUAL);
+
+        return bmpBuilder.buffer;
     }
 
     private buffersNotEqualSize(originalBuffer: Buffer, modifiedBuffer: Buffer): Boolean {
