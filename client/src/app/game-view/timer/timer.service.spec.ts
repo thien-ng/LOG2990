c4e52f1d38e7d36221ca21bdc1310e20cb65de5e
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-14 16:59:14 -0500

    CT R: deplacement tests du compo vers timer service

--- client/src/app/game-view/timer/timer.service.spec.ts
+++ client/src/app/game-view/timer/timer.service.spec.ts
@@ -10,3 +10,47 @@ describe("TimerService", () => {
     expect(service).toBeTruthy();
   });
 });
+
+// describe("TimerComponent tests", () => {
+//   let timer: TimerComponent;
+
+//   beforeEach(() => {
+//     timer = new TimerComponent();
+//   });
+
+//   it("should return the right time format given 2 seconds", () => {
+//     const min: number = 2;
+//     const timeFormat: string = "00:02";
+
+//     spyOn(timer, "startTimer").and.callFake(() => {
+//       timer.timeString = timer["convertSecondsToString"](min);
+//     });
+
+//     timer.startTimer();
+//     expect(timer.timeString).toEqual(timeFormat);
+//   });
+
+//   it("should return the right time format given 720 seconds", () => {
+//     const min: number = 720;
+//     const timeFormat: string = "12:00";
+
+//     spyOn(timer, "startTimer").and.callFake(() => {
+//       timer.timeString = timer["convertSecondsToString"](min);
+//     });
+
+//     timer.startTimer();
+//     expect(timer.timeString).toEqual(timeFormat);
+//   });
+
+//   it("should return the right time format given 72 seconds", () => {
+//     const min: number = 72;
+//     const timeFormat: string = "01:12";
+
+//     spyOn(timer, "startTimer").and.callFake(() => {
+//       timer.timeString = timer["convertSecondsToString"](min);
+//     });
+
+//     timer.startTimer();
+//     expect(timer.timeString).toEqual(timeFormat);
+//   });
+// });
