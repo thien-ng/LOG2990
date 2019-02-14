890e8e9b78b784c9ab1c43ddbc34e6dc9aa839ec
Michael Sauget <michaelsauget@gmail.com>
2019-02-14 03:14:15 -0500

    MS R: Refactor la classe pour pouvoir travailler sur un Buffer BMP de 24 bits directement pour accélérer les calculs en bypassant la conversion array/Buffer.

--- server/app/services/difference-checker/utilities/differenceEnlarger.ts
+++ server/app/services/difference-checker/utilities/differenceEnlarger.ts
@@ -3,7 +3,9 @@ import { injectable } from "inversify";
 @injectable()
 export class DifferenceEnlarger {
 
-    private readonly IS_A_DIFFERENCE: number = 0;
+    private readonly IS_A_DIFFERENCE: number = 0;   // a mettre dans les constantes
+    private readonly BMP_HEADER_SIZE: number = 54;  // a mettre dans les constantes
+    private readonly PIXEL_SIZE:      number = 3;   // a mettre dans les constantes
 
     private enlargedDifferences: Buffer;
 
@@ -12,45 +14,71 @@ export class DifferenceEnlarger {
     }
 
     public circleAllDifferences(): Buffer {
-        let index: number = 0;
 
-        this.differencesFound.forEach( (value: number) => {
-            if (value === this.IS_A_DIFFERENCE) {
-                this.drawCircle(index);
+        for (let bytePos: number = this.BMP_HEADER_SIZE; bytePos < this.enlargedDifferences.length; bytePos += this.PIXEL_SIZE) {
+            if (this.differencesFound[bytePos] === this.IS_A_DIFFERENCE) {
+                const pixelIndex: number = this.convertToPixelPosition(bytePos);
+                this.drawCircleAround(pixelIndex);
             }
-            index++;
-        });
+        }
 
         return this.enlargedDifferences;
     }
 
-    private drawCircle(positionToCircle: number): void {
+    private drawCircleAround(pixelToCircle: number): void {
         // to get the square size where the draw circle should be called
-        const squareSize: number = this.radius + this.radius + 1;
-        const startIndex: number = this.getSquareStartIndex(positionToCircle);
+        const squareSizeInPixels: number = this.radius + this.radius + 1;
+        const startPixelIndex: number = this.getSquareStartPixelIndex(pixelToCircle);
 
-        for (let row: number = 0; row < squareSize; row++) {
-            for (let col: number = 0; col < squareSize; col++) {
+        for (let row: number = 0; row < squareSizeInPixels; row++) {
+            for (let col: number = 0; col < squareSizeInPixels; col++) {
 
-                const currentPosition: number = row * this.width + col + startIndex;
-                if (currentPosition >= 0 && currentPosition < this.differencesFound.length) {
-                    const currentDistanceToCenter: number = this.findDistanceBetween(positionToCircle, currentPosition);
+                const currentPixelPosition: number = row * this.width + col + startPixelIndex;
+
+                if (currentPixelPosition >= 0 && currentPixelPosition < this.numberOfPixelsInImage()) {
+
+                    const currentDistanceToCenter: number = this.findDistanceBetween(pixelToCircle, currentPixelPosition);
 
                     if (this.isInAdjustedRadius(currentDistanceToCenter)) {
-                        this.enlargedDifferences[currentPosition] = this.IS_A_DIFFERENCE;
+                        this.setPixelAsDifference(currentPixelPosition);
                     }
                 }
             }
         }
     }
 
-    private getSquareStartIndex(centerPositionIndex: number): number {
-        return centerPositionIndex - this.width * this.radius - this.radius;
+    private setPixelAsDifference(pixelPosition: number): void {
+        const firstByte: number = this.convertToBytePosition(pixelPosition);
+
+        for (let offset: number = 0; offset < this.PIXEL_SIZE; offset++) {
+            this.enlargedDifferences[firstByte + offset] = this.IS_A_DIFFERENCE;
+        }
     }
 
-    private findDistanceBetween(centerPosition: number, periphericPosition: number): number {
-        const deltaX: number = periphericPosition % this.width - centerPosition % this.width;
-        const deltaY: number = Math.floor(periphericPosition / this.width) - Math.floor(centerPosition / this.width);
+    // will only work with an image that's a width multiple of 4
+    private numberOfPixelsInImage(): number {
+        const imageSizeInBytes: number = this.differencesFound.length - this.BMP_HEADER_SIZE;
+
+        return Math.floor(imageSizeInBytes / this.PIXEL_SIZE);
+    }
+
+    // will only work with an image that's a width multiple of 4
+    private convertToPixelPosition(bytePosition: number): number {
+        return Math.floor(bytePosition / this.PIXEL_SIZE);
+    }
+
+    // will only work with an image that's a width multiple of 4
+    private convertToBytePosition(pixelPosition: number): number {
+        return pixelPosition * this.PIXEL_SIZE;
+    }
+
+    private getSquareStartPixelIndex(pixelCenterPosition: number): number {
+        return pixelCenterPosition - this.width * this.radius - this.radius;
+    }
+
+    private findDistanceBetween(centerPixelPosition: number, periphericPixelPosition: number): number {
+        const deltaX: number = periphericPixelPosition % this.width - centerPixelPosition % this.width;
+        const deltaY: number = Math.floor(periphericPixelPosition / this.width) - Math.floor(centerPixelPosition / this.width);
 
         return Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
     }
