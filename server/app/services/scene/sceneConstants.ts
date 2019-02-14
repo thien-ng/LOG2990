c80384bd37fbb78f2b45921938c491d435919b13
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-13 23:16:57 -0500

    Revert "GHV R: ajustement du scale min et max"
    This reverts commit e8b520dc0c002ac57771aebb3b79294a6200e9d9.

--- server/app/services/scene/sceneConstants.ts
+++ server/app/services/scene/sceneConstants.ts
@@ -11,6 +11,6 @@ export class SceneConstants {
     public static readonly MAX_POSITION_Y: number = 1000;
     public static readonly MAX_POSITION_Z: number = 1000;
     // Scales
-    public static readonly MIN_SCALE: number = 5;
-    public static readonly MAX_SCALE: number = 15;
+    public static readonly MIN_SCALE: number = 0.5;
+    public static readonly MAX_SCALE: number = 1.5;
 }
