3c6b7c06a6090d924c56bbd8cf332c089135d9fb
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-14 16:45:42 -0500

    CT R: suppression increment des secondes dans timer component car c'est serveur qui doit faire ca

--- client/src/app/game-view/timer/timer.component.ts
+++ client/src/app/game-view/timer/timer.component.ts
@@ -1,29 +1,17 @@
-import { AfterContentInit, Component, Inject } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { TimerService } from "./timer.service";
 @Component({
   selector: "app-timer",
   templateUrl: "./timer.component.html",
   styleUrls: ["./timer.component.css"],
 })
-export class TimerComponent implements AfterContentInit {
+export class TimerComponent {
   private readonly START_TIME: string = "00:00";
-  private readonly INTERVAL: number = 1000;
 
   public time: string;
-  private totalTime: number;
 
   public constructor(@Inject(TimerService) public timerService: TimerService) {
     this.time = this.START_TIME;
-    this.totalTime = 0;
   }
 
-  public ngAfterContentInit(): void {
-    this.startTimer();
-  }
-
-  public startTimer(): void {
-    setInterval(() => {
-                this.time = this.timerService.timeFormat(++this.totalTime);
-    },          this.INTERVAL);
-  }
 }
