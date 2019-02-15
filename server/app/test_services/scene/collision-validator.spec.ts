350911121253c641b8284a88a4187f62a4b94702
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-15 05:59:58 -0500

    GHV T: preparation des scenesObjects pour tester collision

--- server/app/test_services/scene/collision-validator.spec.ts
+++ server/app/test_services/scene/collision-validator.spec.ts
@@ -1,13 +1,54 @@
 import * as chai from "chai";
 import * as spies from "chai-spies";
 import "reflect-metadata";
-import { SceneObjectType } from "../../../../common/communication/iSceneObject";
+import { ISceneObject, SceneObjectType } from "../../../../common/communication/iSceneObject";
 import { CollisionValidator } from "../../services/scene/utilitaries/collision-validator";
 
 /* tslint:disable:no-any no-magic-numbers */
 let collisionValidator: CollisionValidator;
 
 describe("Scene builder tests", () => {
+
+    const cube1: ISceneObject = {
+        type: 1,
+        position: {
+            x: 20, y: 20, z: 20 },
+        rotation: {
+            x: 1, y: 1, z: 1,
+        },
+        scale: {
+            x: 10, y: 10, z: 10,
+        },
+        color: "#8cadbb",
+    };
+
+    const cube2: ISceneObject = {
+        type: 1,
+        position: {
+            x: 120, y: 120, z: 120 },
+        rotation: {
+            x: 1, y: 1, z: 1,
+        },
+        scale: {
+            x: 10, y: 10, z: 10,
+        },
+        color: "#8cadbb",
+    };
+
+    const cube3: ISceneObject = {
+        type: 1,
+        position: {
+            x: 220, y: 220, z: 220 },
+        rotation: {
+            x: 1, y: 1, z: 1,
+        },
+        scale: {
+            x: 10, y: 10, z: 10,
+        },
+        color: "#8cadbb",
+    };
+
+    const existingSceneObjects: ISceneObject[] = [cube1, cube2, cube3];
 
     beforeEach(() => {
 
