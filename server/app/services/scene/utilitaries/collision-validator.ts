149966e892b8ce94abba73b1860991580af2aeb2
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-14 23:39:52 -0500

    GHV D: squelette de classe pour generator boite de collision

--- server/app/services/scene/utilitaries/collision-validator.ts
+++ server/app/services/scene/utilitaries/collision-validator.ts
@@ -1,4 +1,4 @@
-import { IAxisValues, ISceneObject, SceneObjectType } from "../../../../../common/communication/iSceneObject";
+import { ISceneObject } from "../../../../../common/communication/iSceneObject";
 
 export class CollisionValidator {
 
@@ -8,7 +8,7 @@ export class CollisionValidator {
 
         existingSceneObjects.forEach((element: ISceneObject) => {
 
-            if (this.areColliding(newSceneObject.position, element.position)) {
+            if (this.checkForCollision(newSceneObject, element)) {
                 hasCollision =  true;
             }
         });
@@ -17,37 +17,9 @@ export class CollisionValidator {
 
     }
 
-    private areColliding(position1: IAxisValues, position2: IAxisValues): boolean {
+    private checkForCollision(position1: ISceneObject, position2: ISceneObject): boolean {
 
         return position1 === position2;
     }
 
-    public generateCollisionRadius(sceneObject: ISceneObject): number {
-
-        let radius: number;
-
-        switch (sceneObject.type) {
-            case SceneObjectType.Cube:
-                radius = 0;
-                break;
-
-            case SceneObjectType.Cylinder:
-                radius = 0;
-                break;
-
-            case SceneObjectType.TriangularPyramid:
-                radius = 0;
-                break;
-
-            case SceneObjectType.Cone:
-                radius = 0;
-                break;
-
-            default: // Default case is a sphere
-                radius = 0;
-                break;
-        }
-
-        return radius;
-    }
 }
