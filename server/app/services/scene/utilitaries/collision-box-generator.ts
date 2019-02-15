ca33b9ab149a25a197740510f1eb3e8fd9ca4b37
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-15 02:28:14 -0500

    GHV D: collision radius pour cone et pyramide

--- server/app/services/scene/utilitaries/collision-box-generator.ts
+++ server/app/services/scene/utilitaries/collision-box-generator.ts
@@ -33,32 +33,41 @@ export class CollisionBoxGenerator {
 
     public calculateCubeCollisionRadius(sceneObject: ISceneObject): number {
 
-        return this.pythagore3D(sceneObject.scale.x / 2, sceneObject.scale.y / 2, sceneObject.scale.z / 2);
+        return this.pythagore3D( sceneObject.scale.x / 2, sceneObject.scale.y / 2, sceneObject.scale.z / 2 );
     }
 
     public calculatePyramidCollisionRadius(sceneObject: ISceneObject): number {
 
+        // for now scale.x = radius and scale.y = heigth
+        return this.pythagore( sceneObject.scale.x, sceneObject.scale.y / 2 ); 
     }
 
     public calculateConeCollisionRadius(sceneObject: ISceneObject): number {
 
+        // for now scale.x = radius and scale.y = heigth
+        return this.pythagore( sceneObject.scale.x, sceneObject.scale.y / 2 );
     }
 
     public calculateCylinderCollisionRadius(sceneObject: ISceneObject): number {
 
-        return this.pythagore3D(sceneObject.scale.x / 2, sceneObject.scale.y / 2, sceneObject.scale.z / 2);
+        return this.pythagore3D( sceneObject.scale.x / 2, sceneObject.scale.y / 2, sceneObject.scale.z / 2 );
     }
 
     public calculateSphereCollisionRadius(sceneObject: ISceneObject): number {
+
         return sceneObject.scale.x;
     }
 
     private pythagore(x: number, y: number): number {
 
+        const xSquared: number = Math.pow(x, 2);
+        const ySquared: number = Math.pow(y, 2);
 
+        return Math.sqrt( xSquared + ySquared );
     }
 
     private pythagore3D(x: number, y: number, z: number): number {
+
         const xSquared: number = Math.pow(x, 2);
         const ySquared: number = Math.pow(y, 2);
         const zSquared: number = Math.pow(z, 2);
