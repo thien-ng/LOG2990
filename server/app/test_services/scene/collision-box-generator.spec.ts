425f94fc06919eb16805b1daea327499eb6557d7
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-15 06:16:43 -0500

    GHV T: test pour verifier bon appel de calcul pour cube

--- server/app/test_services/scene/collision-box-generator.spec.ts
+++ server/app/test_services/scene/collision-box-generator.spec.ts
@@ -22,57 +22,57 @@ describe("Scene builder tests", () => {
         color: "#8cadbb",
     };
 
-    const cylinder: ISceneObject = {
-        type: SceneObjectType.Cylinder,
-        position: {
-            x: 20, y: 20, z: 20 },
-        rotation: {
-            x: 1, y: 1, z: 1,
-        },
-        scale: {
-            x: 10, y: 10, z: 10,
-        },
-        color: "#8cadbb",
-    };
+    // const cylinder: ISceneObject = {
+    //     type: SceneObjectType.Cylinder,
+    //     position: {
+    //         x: 20, y: 20, z: 20 },
+    //     rotation: {
+    //         x: 1, y: 1, z: 1,
+    //     },
+    //     scale: {
+    //         x: 10, y: 10, z: 10,
+    //     },
+    //     color: "#8cadbb",
+    // };
 
-    const pyramid: ISceneObject = {
-        type: SceneObjectType.TriangularPyramid,
-        position: {
-            x: 20, y: 20, z: 20 },
-        rotation: {
-            x: 1, y: 1, z: 1,
-        },
-        scale: {
-            x: 10, y: 10, z: 10,
-        },
-        color: "#8cadbb",
-    };
+    // const pyramid: ISceneObject = {
+    //     type: SceneObjectType.TriangularPyramid,
+    //     position: {
+    //         x: 20, y: 20, z: 20 },
+    //     rotation: {
+    //         x: 1, y: 1, z: 1,
+    //     },
+    //     scale: {
+    //         x: 10, y: 10, z: 10,
+    //     },
+    //     color: "#8cadbb",
+    // };
 
-    const cone: ISceneObject = {
-        type: SceneObjectType.Cone,
-        position: {
-            x: 20, y: 20, z: 20 },
-        rotation: {
-            x: 1, y: 1, z: 1,
-        },
-        scale: {
-            x: 10, y: 10, z: 10,
-        },
-        color: "#8cadbb",
-    };
+    // const cone: ISceneObject = {
+    //     type: SceneObjectType.Cone,
+    //     position: {
+    //         x: 20, y: 20, z: 20 },
+    //     rotation: {
+    //         x: 1, y: 1, z: 1,
+    //     },
+    //     scale: {
+    //         x: 10, y: 10, z: 10,
+    //     },
+    //     color: "#8cadbb",
+    // };
 
-    const sphere: ISceneObject = {
-        type: SceneObjectType.Sphere,
-        position: {
-            x: 20, y: 20, z: 20 },
-        rotation: {
-            x: 1, y: 1, z: 1,
-        },
-        scale: {
-            x: 10, y: 10, z: 10,
-        },
-        color: "#8cadbb",
-    };
+    // const sphere: ISceneObject = {
+    //     type: SceneObjectType.Sphere,
+    //     position: {
+    //         x: 20, y: 20, z: 20 },
+    //     rotation: {
+    //         x: 1, y: 1, z: 1,
+    //     },
+    //     scale: {
+    //         x: 10, y: 10, z: 10,
+    //     },
+    //     color: "#8cadbb",
+    // };
 
     beforeEach(() => {
 
@@ -81,4 +81,11 @@ describe("Scene builder tests", () => {
         collisionBoxGenerator = new CollisionBoxGenerator();
     });
 
+    it("should call calculateCubeCollisionRadius for cube object", () => {
+        const spy: any = chai.spy.on(collisionBoxGenerator, "calculateCubeCollisionRadius");
+
+        collisionBoxGenerator.generateCollisionRadius(cube);
+
+        chai.expect(spy).to.have.been.called();
+    });
 });
