5dfcb704d4842a3a68949757a368eebab5bfd83d
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-15 00:25:18 -0500

    GHV D: calcul du radius pour cube

--- server/app/services/scene/utilitaries/collision-box-generator.ts
+++ server/app/services/scene/utilitaries/collision-box-generator.ts
@@ -1,60 +1,60 @@
-// import { ISceneObject, SceneObjectType } from "../../../../../common/communication/iSceneObject";
+import { ISceneObject, SceneObjectType } from "../../../../../common/communication/iSceneObject";
 
 export class CollisionBoxGenerator {
 
-    // public generateCollisionRadius(sceneObject: ISceneObject): number {
+    public generateCollisionRadius(sceneObject: ISceneObject): number {
 
-    //     let radius: number;
+        let radius: number;
 
-    //     switch (sceneObject.type) {
-    //         case SceneObjectType.Cube:
-    //             radius = 0;
-    //             break;
+        switch (sceneObject.type) {
+            case SceneObjectType.Cube:
+                radius = this.calculateCubeCollisionRadius(sceneObject);
+                break;
 
-    //         case SceneObjectType.Cylinder:
-    //             radius = 0;
-    //             break;
+            case SceneObjectType.Cylinder:
+                radius = this.calculateCylinderCollisionRadius(sceneObject);
+                break;
 
-    //         case SceneObjectType.TriangularPyramid:
-    //             radius = 0;
-    //             break;
+            case SceneObjectType.TriangularPyramid:
+                radius = this.calculatePyramidCollisionRadius(sceneObject);
+                break;
 
-    //         case SceneObjectType.Cone:
-    //             radius = 0;
-    //             break;
+            case SceneObjectType.Cone:
+                radius = this.calculateConeCollisionRadius(sceneObject);
+                break;
 
-    //         default: // Default case is a sphere
-    //             radius = 0;
-    //             break;
-    //     }
+            default: // Default case is a sphere
+                radius = this.calculateSphereCollisionRadius(sceneObject);
+                break;
+        }
 
-    //     return radius;
-    // }
+        return radius;
+    }
 
-    // public calculateCubeCollisionRadius(sceneObject: ISceneObject): number {
-    //     const halfXsquared: number = Math.pow(sceneObject.scale.x / 2, 2);
-    //     const halfYsquared: number = Math.pow(sceneObject.scale.y / 2, 2);
-    //     const halfZsquared: number = Math.pow(sceneObject.scale.z / 2, 2);
+    public calculateCubeCollisionRadius(sceneObject: ISceneObject): number {
+        const halfXsquared: number = Math.pow(sceneObject.scale.x / 2, 2);
+        const halfYsquared: number = Math.pow(sceneObject.scale.y / 2, 2);
+        const halfZsquared: number = Math.pow(sceneObject.scale.z / 2, 2);
 
-    //     const radius1Squared: number = halfXsquared + halfZsquared;
+        const radiusTempSquared: number = halfXsquared + halfZsquared;
 
-    //     const radius: number = Math.sqrt( halfYsquared + radius1Squared );
-    // }
+        return Math.sqrt( halfYsquared + radiusTempSquared );
+    }
 
-    // public calculatePyramidCollisionRadius(sceneObject: ISceneObject): number {
+    public calculatePyramidCollisionRadius(sceneObject: ISceneObject): number {
 
-    // }
+    }
 
-    // public calculateConeCollisionRadius(sceneObject: ISceneObject): number {
+    public calculateConeCollisionRadius(sceneObject: ISceneObject): number {
 
-    // }
+    }
 
-    // public calculateCylinderCollisionRadius(sceneObject: ISceneObject): number {
+    public calculateCylinderCollisionRadius(sceneObject: ISceneObject): number {
 
-    // }
+    }
 
-    // public calculateSphereCollisionRadius(sceneObject: ISceneObject): number {
+    public calculateSphereCollisionRadius(sceneObject: ISceneObject): number {
 
-    // }
+    }
 
 }
