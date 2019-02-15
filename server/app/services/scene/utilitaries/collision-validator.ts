cdffc3417d0a69c7b5a8a7d5de01b3f4d60194a8
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-14 23:03:01 -0500

    GHV D: switch case qui va appeler les bonnes fonction pr radius

--- server/app/services/scene/utilitaries/collision-validator.ts
+++ server/app/services/scene/utilitaries/collision-validator.ts
@@ -1,4 +1,4 @@
-import { IAxisValues, ISceneObject } from "../../../../../common/communication/iSceneObject";
+import { IAxisValues, ISceneObject, SceneObjectType } from "../../../../../common/communication/iSceneObject";
 
 export class CollisionValidator {
 
@@ -20,5 +20,34 @@ export class CollisionValidator {
     private areColliding(position1: IAxisValues, position2: IAxisValues): boolean {
 
         return position1 === position2;
+    }
+
+    public generateCollisionRadius(sceneObject: ISceneObject): number {
+
+        let radius: number;
+
+        switch (sceneObject.type) {
+            case SceneObjectType.Cube:
+                radius = 0;
+                break;
+
+            case SceneObjectType.Cylinder:
+                radius = 0;
+                break;
+
+            case SceneObjectType.TriangularPyramid:
+                radius = 0;
+                break;
+
+            case SceneObjectType.Cone:
+                radius = 0;
+                break;
+
+            default: // Default case is a sphere
+                radius = 0;
+                break;
+        }
+
+        return radius;
     }
 }
