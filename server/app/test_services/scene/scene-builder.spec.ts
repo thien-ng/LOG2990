0a90de78f37b521db84d1bccac8debe4556f539a
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-14 20:24:25 -0500

    GHV R: refactor des tests suite aux dernier commit

--- server/app/test_services/scene/scene-builder.spec.ts
+++ server/app/test_services/scene/scene-builder.spec.ts
@@ -1,9 +1,11 @@
 import * as chai from "chai";
 import * as spies from "chai-spies";
 import "reflect-metadata";
+import * as sinon from "sinon";
 import { SceneObjectType } from "../../../../common/communication/iSceneObject";
 import { ISceneOptions } from "../../../../common/communication/iSceneOptions";
 import { SceneBuilder } from "../../services/scene/scene-builder";
+// import { ISceneVariables } from "../../../../common/communication/iSceneVariables";
 
 /* tslint:disable:no-any */
 let sceneBuilder: SceneBuilder;
@@ -20,22 +22,22 @@ describe("Scene builder tests", () => {
 
         chai.use(spies);
 
-        sceneBuilder = new SceneBuilder(sceneOptions);
+        sceneBuilder = new SceneBuilder();
     });
 
-    it("should call the generateRandomAxisValues function when generating scene objects", () => {
+    it("should call the generateRandomAxisValues function when generating scene", () => {
         const spy: any = chai.spy.on(sceneBuilder, "generateRandomAxisValues");
-        sceneBuilder.generateSceneObjects();
+        sceneBuilder.generateScene(sceneOptions);
         chai.expect(spy).to.have.been.called();
     });
 
     it("should call the generateRandomRotationValues function when generating scene objects", () => {
         const spy: any = chai.spy.on(sceneBuilder, "generateRandomRotationValues");
-        sceneBuilder.generateSceneObjects();
+        sceneBuilder.generateScene(sceneOptions);
         chai.expect(spy).to.have.been.called();
     });
 
-    it("should change position of a new sceneObject until there is no collision", () =>) {
-        
-    });
+    // it("should change position of a new sceneObject until there is no collision", () =>) {
+
+    // });
 });
