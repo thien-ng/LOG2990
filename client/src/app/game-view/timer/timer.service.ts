f2a805da8d3922b924806003057fea474689029f
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-14 20:15:08 -0500

    CT R: deplacements constantes de timer service dans fichier constantes

--- client/src/app/game-view/timer/timer.service.ts
+++ client/src/app/game-view/timer/timer.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from "@angular/core";
-
+import { Constants } from "../../constants";
 @Injectable({
   providedIn: "root",
 })
 export class TimerService {
-  private readonly MINUTE_IN_SECONDS: number = 60;
-  private readonly TWO_DIGITS: number = 10;
 
   public timeFormat(totalTimeInSeconds: number): string {
-    const min: number = Math.floor((totalTimeInSeconds / this.MINUTE_IN_SECONDS));
-    const sec: number = totalTimeInSeconds - (min * this.MINUTE_IN_SECONDS);
+    const min: number = Math.floor((totalTimeInSeconds / Constants.MINUTE_IN_SECONDS));
+    const sec: number = totalTimeInSeconds - (min * Constants.MINUTE_IN_SECONDS);
 
-    let timeFormat: string = (min < this.TWO_DIGITS ? "0" + min.toString() : min.toString());
-    timeFormat += ":" + (sec < this.TWO_DIGITS ? "0" + sec.toString() : sec.toString());
+    let timeFormat: string = (min < Constants.TWO_DIGITS ? "0" + min.toString() : min.toString());
+    timeFormat += ":" + (sec < Constants.TWO_DIGITS ? "0" + sec.toString() : sec.toString());
 
     return timeFormat;
   }
