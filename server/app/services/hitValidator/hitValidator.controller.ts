ebbd99c3e204b5a553784679f98d56b9298b1b7e
Michael Sauget <michaelsauget@gmail.com>
2019-02-16 18:38:21 -0500

    MS R: Refactor la manière dont les arguments de la requête HTTP sont récupérés.

--- server/app/services/hitValidator/hitValidator.controller.ts
+++ server/app/services/hitValidator/hitValidator.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
 import { HitValidatorService } from "./hitValidator.service";
+import { IHitToValidate } from "./interfaces";
 
 import Types from "../../types";
 
@@ -13,17 +14,16 @@ export class HitValidatorController {
 
         const router: Router = Router();
 
-        router.post("/", (req: Request, res: Response, next: NextFunction) => {
+        router.post("/", async (req: Request, res: Response, next: NextFunction) => {
 
-            const color: Promise<number> = this.hitValidatorService.confirmHit(
-                Number(req.body.inputX),
-                Number(req.body.inputY),
-                String(req.body.imageUrl),
-            );
+            const hitToValidate: IHitToValidate = {
+                posX: req.body.inputX,
+                posY: req.body.inputY,
+                imageUrl: String(req.body.imageUrl),
+                colorToIgnore: req.body.colorToIgnore,
+            };
 
-            res.json({
-                "Color": color,
-            });
+            res.json(await this.hitValidatorService.confirmHit(hitToValidate));
         });
 
         return router;
