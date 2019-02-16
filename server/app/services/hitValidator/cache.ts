d0bfe4286390ad5316413881d50847d23262c340
Michael Sauget <michaelsauget@gmail.com>
2019-02-15 22:53:19 -0500

    MS D: Ajout de la classe Cache.

---
+++ server/app/services/hitValidator/cache.ts
@@ -0,0 +1,21 @@
+export interface ICacheElement {
+    imageUrl:   string;
+    buffer:     Buffer;
+}
+export class Cache {
+
+    private storage: ICacheElement[];
+    private insertionIndex: number;
+
+    public constructor(private cacheSize: number) {
+        this.storage = [];
+        this.insertionIndex = 0;
+    }
+
+    public insert(entry: ICacheElement): void {
+        if (!this.contains(entry.imageUrl)) {
+            this.storage[this.insertionIndex] = entry;
+            this.updateInsertionIndex();
+        }
+    }
+}
