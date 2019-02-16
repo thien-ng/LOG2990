9443496ddd910b9e2b47463ed4d56fe48b4ec9e8
Michael Sauget <michaelsauget@gmail.com>
2019-02-15 23:39:04 -0500

    MS D: Ajout de la fonction permettant de trouver la couleur pointée par des coordonnées x, y dans une image.

--- server/app/services/hitValidator/hitValidator.service.ts
+++ server/app/services/hitValidator/hitValidator.service.ts
@@ -15,4 +15,21 @@ export class HitValidatorService {
         this.cache = new Cache(this.CACHE_SIZE);
         // this.cache.insert( { imageUrl: "url de l'image", buffer: Buffer.from("FUCKING SHIT")});
     }
+
+    private findColorAtPoint(posX: number, posY: number, buffer: Buffer): number {
+
+        // console.log();
+        const trueX: number = posX;
+        const imageWidth: number = 640;
+        const imageHeight: number = 640;
+        const trueY: number = imageHeight - posY;
+        const headerSize: number = 54;
+        const pixelSize: number = 3;
+        const absolutePosition: number = trueX * pixelSize + trueY * imageWidth * pixelSize + headerSize;
+        // console.log("absolutePosition " + absolutePosition );
+        // console.log("buffer lenght " + buffer.length );
+
+        return buffer[absolutePosition];
+    }
+
 }
