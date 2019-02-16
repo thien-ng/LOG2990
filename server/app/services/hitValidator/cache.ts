c3d57af20ec74987ee0dbe6f98fe7946ffff9ea9
Michael Sauget <michaelsauget@gmail.com>
2019-02-16 02:58:44 -0500

    MS B: Correction de la fonction calculant l'obsolescence maximum.

--- server/app/services/hitValidator/cache.ts
+++ server/app/services/hitValidator/cache.ts
@@ -1,39 +1,76 @@
-export interface ICacheElement {
-    imageUrl:   string;
-    buffer:     Buffer;
-}
+import { ICacheElement, IImageToCache } from "./interfaces";
+
 export class Cache {
 
     private storage: ICacheElement[];
-    private insertionIndex: number;
+    private readonly ON_NOTFOUND_ERROR: string =
+    "The element wanted has not been found in the cache. File: Cache.ts. Line: 44. \nError message: ";
 
     public constructor(private cacheSize: number) {
         this.storage = [];
-        this.insertionIndex = 0;
     }
 
-    public insert(entry: ICacheElement): void {
+    public insert(entry: IImageToCache): void {
         if (!this.contains(entry.imageUrl)) {
-            this.storage[this.insertionIndex] = entry;
-            this.updateInsertionIndex();
+            const elementToInsert: ICacheElement = {
+                                                    imageToCache: entry,
+                                                    obsolescenceDegree: 0,
+                                                    };
+            this.storage[this.leastRecentlyUsedIndex()] = elementToInsert;
+            this.updateLRUPolitic(elementToInsert);
         }
     }
 
     public contains(imageUrl: string): boolean {
         return this.storage.some((cachedElement: ICacheElement): boolean => {
-            return cachedElement.imageUrl === imageUrl;
+            return cachedElement.imageToCache.imageUrl === imageUrl;
         });
     }
 
-    public get(imageUrl: string): Buffer | undefined {
-        const query: ICacheElement[] = this.storage.filter((element: ICacheElement) => {
-            return element.imageUrl === imageUrl;
-        });
+    public get(imageUrl: string): Buffer {
 
-        return query.length > 0 ? query[0].buffer : undefined;
+        try {
+            const foundElement: ICacheElement = this.storage.filter((element: ICacheElement) => {
+                return element.imageToCache.imageUrl === imageUrl;
+            })[0];
+            this.updateLRUPolitic(foundElement);
+
+            return foundElement.imageToCache.buffer;
+        } catch (error) {
+            throw new TypeError(this.ON_NOTFOUND_ERROR + error.message);
+        }
     }
 
-    private updateInsertionIndex(): void {
-        this.insertionIndex = ++this.insertionIndex % this.cacheSize;
+    private updateLRUPolitic(elementRecentlyAccessed: ICacheElement): void {
+        this.storage.forEach((elementCached: ICacheElement) => {
+            if (elementCached !== elementRecentlyAccessed) {
+                elementCached.obsolescenceDegree++;
+            }
+        });
+    }
+
+    private leastRecentlyUsedIndex(): number {
+        if (this.storage.length < this.cacheSize) {
+            return this.storage.length;
+        }
+
+        return this.findMaxObsolescenceIndex();
+    }
+
+    private findMaxObsolescenceIndex(): number {
+
+        const allObsolescences: number[] = this.storage.map((element: ICacheElement) => element.obsolescenceDegree);
+
+        let maxFound:   number = 0;
+        let indexOfMax: number = 0;
+
+        allObsolescences.forEach((obsol: number, index: number) => {
+            if (obsol > maxFound) {
+                maxFound = obsol;
+                indexOfMax = index;
+            }
+        });
+
+        return indexOfMax;
     }
 }
