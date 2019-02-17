947a3bdd8349ee9ef01e2fad947b14fcc2f3deb5
Michael Sauget <michaelsauget@gmail.com>
2019-02-17 05:42:38 -0500

    MS D: Ajout des imports et des constantes.

--- server/app/services/game/arena/arena.ts
+++ server/app/services/game/arena/arena.ts
@@ -1,1 +1,9 @@
-export class Arena {}
+import { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { User } from "../../../../../common/communication/iUser";
+import { IArenaInfos, IHitConfirmation, IHitToValidate } from "./interfaces";
+import { Player } from "./player";
+// import { Timer } from "./timer";
+
+const FF: number = 255;
+const WHITE: number[] = [FF, FF, FF];
+const URL_HIT_VALIDATOR: string = "http://localhost:3000/api/hitvalidator";
