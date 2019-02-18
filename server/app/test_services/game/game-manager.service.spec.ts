c450c843f5c19b2ab83e6a6ae9658ac3c36fbb93
jeremy <jeremyyoyo@hotmail.fr>
2019-02-17 21:06:50 -0500

    JB T: tester game manager

--- server/app/test_services/game/game-manager.service.spec.ts
+++ server/app/test_services/game/game-manager.service.spec.ts
@@ -1,8 +1,34 @@
-import { expect } from "chai";
 import "reflect-metadata";
+
+import { expect } from "chai";
+import { GameMode } from "../../../../common/communication/iCard";
+import { GameType, IGameRequest } from "../../../../common/communication/iGameRequest";
+import { Message } from "../../../../common/communication/message";
 import { GameManagerService } from "../../services/game/game-manager.service";
 
 let gameManagerService: GameManagerService;
+
+const request2D: IGameRequest = {
+    username: "Frank",
+    gameId: 100,
+    type: GameType.singlePlayer,
+    mode: GameMode.simple,
+}
+
+const request3D: IGameRequest = {
+    username: "Franky",
+    gameId: 105,
+    type: GameType.singlePlayer,
+    mode: GameMode.free,
+}
+
+const invalidRequest: IGameRequest = {
+    username: "Frankette",
+    gameId: 103,
+    type: GameType.singlePlayer,
+    mode: GameMode.invalid,
+}
+
 beforeEach(() => {
     gameManagerService = new GameManagerService();
 });
@@ -23,6 +49,21 @@ describe("GameManagerService tests", () => {
         gameManagerService.unsubscribeSocketID("dylan");
         const result: string = gameManagerService.userList[0];
         expect(result).to.be.equal("michelGagnon");
+    });
+
+    it("Should return a success message when creating a 2D arena", () => {
+        const answer: Message = gameManagerService.analyseRequest(request2D);
+        expect(answer.title).deep.equal("onSuccess");
+    });
+
+    it("Should return a success message when creating a 3D arena", () => {
+        const answer: Message = gameManagerService.analyseRequest(request3D);
+        expect(answer.title).deep.equal("onSuccess");
+    });
+
+    it("Should return an error message when loading an invalid game", () => {
+        const answer: Message = gameManagerService.analyseRequest(invalidRequest);
+        expect(answer.title).deep.equal("onError");
     });
 
 });
