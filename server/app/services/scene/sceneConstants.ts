d390c2760b24e221733d2bfea066851905046b8b
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-18 20:22:47 -0500

    GHV B: pu de collision

--- server/app/services/scene/sceneConstants.ts
+++ server/app/services/scene/sceneConstants.ts
@@ -8,9 +8,10 @@ export class SceneConstants {
     public static readonly MIN_COLOR_GRADIENT: number = 0;
     public static readonly MAX_COLOR_GRADIENT: number = 255;
     // Positions
-    public static readonly MAX_POSITION_X: number = 100;
-    public static readonly MAX_POSITION_Y: number = 100;
-    public static readonly MAX_POSITION_Z: number = 100;
+    public static readonly MAX_POSITION_X: number = 200;
+    public static readonly MAX_POSITION_Y: number = 200;
+    public static readonly MAX_POSITION_Z: number = 200;
+    public static readonly DISTANCE_OFFSET: number = 10;
     // Scales
     public static readonly MIN_SCALE: number = 2;
     public static readonly MAX_SCALE: number = 8;
