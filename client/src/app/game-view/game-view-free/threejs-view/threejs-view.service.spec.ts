eb0f34703f278fdc429d795bc28b63a9ed148e78
Thienng97 <duc-thien@sympatico.ca>
2019-02-19 16:24:40 -0500

    DN T: uncomment  unit testing

--- client/src/app/game-view/game-view-free/threejs-view/threejs-view.service.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-view.service.spec.ts
@@ -1,39 +1,39 @@
 import { inject, TestBed } from "@angular/core/testing";
-// import * as THREE from "three";
-// import { SceneObjectType } from "../../../../../../common/communication/iSceneObject";
-// import { ISceneVariables } from "../../../../../../common/communication/iSceneVariables";
+import * as THREE from "three";
+import { SceneObjectType } from "../../../../../../common/communication/iSceneObject";
+import { ISceneVariables } from "../../../../../../common/communication/iSceneVariables";
 import { ThreejsViewService } from "./threejs-view.service";
+import { mock } from "ts-mockito";
 
-// IL FAUT PARLE AU CHARGE POUR CETTE CRISSE DE MERDE
+let sceneVariables: ISceneVariables = {
+  theme: 1,
+  gameName: "gameName",
+  sceneObjectsQuantity: 1,
+  sceneObjects: [
+    {
+      id: 1,
+      type: SceneObjectType.Cone,
+      position: {x: 1, y: 1, z: 1},
+      rotation: {x: 1, y: 1, z: 1},
+      color: "#FFFFFF",
+      scale: {x: 1, y: 1, z: 1},
+    },
+  ],
+  sceneBackgroundColor: "#FFFFFF",
+}
+let renderer: THREE.WebGLRenderer = mock(THREE.WebGLRenderer);
+let scene: THREE.Scene = mock(THREE.Scene);
 
-// let scene: THREE.Scene;
-// let sceneVariables: ISceneVariables = {
-//   gameName: "gameName",
-//   sceneObjectsQuantity: 1,
-//   sceneObjects: [
-//     {
-//       id: 1,
-//       type: SceneObjectType.Cone,
-//       position: {x: 1, y: 1, z: 1},
-//       rotation: {x: 1, y: 1, z: 1},
-//       color: "#FFFFFF",
-//       scale: {x: 1, y: 1, z: 1},
-//     },
-//   ],
-//   sceneBackgroundColor: "#FFFFFF",
-// }
-
-describe("ThreejsViewService Tests", () => {
+fdescribe("ThreejsViewService Tests", () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [ThreejsViewService],
   }));
 
   it("should create scene", inject([ThreejsViewService], (threejsViewService: ThreejsViewService) => {
     // spyOn<any>(threejsViewService, "generateSceneObjects");
-    // threejsViewService.createScene(scene, sceneVariables);
+    threejsViewService.createScene(scene, sceneVariables, renderer);
+    threejsViewService.animate();
     // expect(threejsViewService).toBeTruthy();
-    // threejsViewService.animate();
-    // expect(threejsViewService["generateSceneObjects"]).toHaveBeenCalled()
   }));
 
 });
