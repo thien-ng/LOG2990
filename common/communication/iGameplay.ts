004d93542ce0b8e4a6359bd57d0bc13a18dff090
jeremy <jeremyyoyo@hotmail.fr>
2019-02-19 21:55:12 -0500

    JB R: nouvelles interfaces

--- common/communication/iGameplay.ts
+++ common/communication/iGameplay.ts
@@ -2,3 +2,26 @@ export interface ICanvasPosition {
     positionX: number;
     positionY: number;
 }
+
+export interface IClickMessage {
+    position: ICanvasPosition;
+    arenaID: number;
+    username: string;
+}
+
+export interface IPosition2D {
+    x: number;
+    y: number;
+}
+
+export interface IOriginalImageSegment {
+    startPosition: IPosition2D;
+    width:      number;
+    height:     number;
+    image:      number[];
+}
+
+export interface IPlayerInputReponse {
+    status:         string;
+    response:       IOriginalImageSegment | string;
+}
