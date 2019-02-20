862509caa6fef153b9d0cd2042f3accfeacd1928
jeremy <jeremyyoyo@hotmail.fr>
2019-02-19 21:48:14 -0500

    JB D: eveluser le playerinputresponse

--- client/src/app/game-view/chat-view/chat-view.service.ts
+++ client/src/app/game-view/chat-view/chat-view.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { IChat } from "../../../../../common/communication/iChat";
+import { IPlayerInputReponse } from "../../../../../common/communication/iGameplay";
+import { Constants } from "../../constants";
 
 @Injectable({
   providedIn: "root",
@@ -12,8 +14,16 @@ export class ChatViewService {
     this.conversation = [];
   }
 
-  public updateConversation(data: IChat): void {
-    this.conversation.unshift(data);
+  public updateConversation(data: IPlayerInputReponse): void {
+    console.log(data);
+
+    const body: string = (data.status === Constants.ON_ERROR_MESSAGE) ? "Wrong Hit" : "Good Hit Muther Fucker";
+    const message: IChat = {
+      username: "SERVEUR",
+      message:  body,
+      time: "time",
+    }
+    this.conversation.unshift(message);
   }
 
   public getConversation(): IChat[] {
