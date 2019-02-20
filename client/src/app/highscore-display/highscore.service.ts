67c7d60dc4bb4889573e9c7fdb3a8d815e145114
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-02-20 01:35:32 -0500

    CT D: remplacement initialisation isTimeValid par un compteur de timeValid

--- client/src/app/highscore-display/highscore.service.ts
+++ client/src/app/highscore-display/highscore.service.ts
@@ -43,23 +43,26 @@ export class HighscoreService {
   }
 
   private validateTimes(times: string[]): boolean {
-    let isTimesValid: boolean = true;
+    let totalSubTimes: number = 0;
+    let totalTimesValid: number = 0;
 
     times.forEach((timeElement) => {
       let highscoreSplit: string[] = ["", ""];
 
       if (this.validateSelector(timeElement, this.SELECTOR)) {
         highscoreSplit = timeElement.split(this.SELECTOR);
+
+        highscoreSplit.forEach((splitElement) => {
+          totalSubTimes++;
+          if (Number(splitElement) > this.MAX_TIME || Number(splitElement) > this.MIN_TIME) {
+            totalTimesValid++;
+          }
+        });
       }
-
-      highscoreSplit.forEach((splitElement) => {
-        if (Number(splitElement) > this.MAX_TIME || Number(splitElement) < this.MIN_TIME) {
-          isTimesValid = false;
-        }
-      });
     });
+    console.log("subtimes: " + totalSubTimes);
+    console.log("timevalide: " + totalTimesValid);
 
-    return isTimesValid;
-
+    return totalTimesValid === totalSubTimes ? true : false;
   }
 }
