10dc24e786ac81918ef64b8aa45e23dcfe4790f7
Thienng97 <duc-thien@sympatico.ca>
2019-02-21 00:06:05 -0500

    JB D: timer service emit quand le time change

--- client/src/app/game-view/timer/timer.service.ts
+++ client/src/app/game-view/timer/timer.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from "@angular/core";
 import { Constants } from "../../constants";
+import { BehaviorSubject, Observable } from "rxjs";
+
 @Injectable({
   providedIn: "root",
 })
 export class TimerService {
 
-  public timeFormat(totalTimeInSeconds: number): string {
+  private readonly START_TIME: string = "00:00";
+
+  private timeUpdated: BehaviorSubject<string>;
+  public timeUpdatedObservable: Observable<string>;
+
+  public constructor() {
+    this.timeUpdated = new BehaviorSubject<string>(this.START_TIME);
+    this.timeUpdatedObservable = this.timeUpdated.asObservable();
+  }
+
+  public get timer(): Observable<string> {
+    return this.timeUpdatedObservable;
+  }
+
+  public timeFormat(totalTimeInSeconds: number): void {
     const min: number = Math.floor((totalTimeInSeconds / Constants.MINUTE_IN_SECONDS));
     const sec: number = totalTimeInSeconds - (min * Constants.MINUTE_IN_SECONDS);
 
     let timeFormat: string = (min < Constants.TWO_DIGITS ? "0" + min.toString() : min.toString());
     timeFormat += ":" + (sec < Constants.TWO_DIGITS ? "0" + sec.toString() : sec.toString());
 
-    return timeFormat;
+    this.timeUpdated.next(timeFormat);
   }
 }
