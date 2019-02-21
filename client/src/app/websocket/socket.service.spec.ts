fb24c18dcfd81315b546372f8b1d344928b6bdcd
Thienng97 <duc-thien@sympatico.ca>
2019-02-20 20:11:35 -0500

    DN R: refactor test

--- client/src/app/websocket/socket.service.spec.ts
+++ client/src/app/websocket/socket.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from "@angular/core/testing";
 import { Observable } from "rxjs";
 import "rxjs/add/observable/of";
 import { mock } from "ts-mockito";
+import { GameViewSimpleService } from "../game-view/game-view-simple/game-view-simple.service";
 import { ChatViewService } from "../game-view/chat-view/chat-view.service";
 import { SocketService } from "./socket.service";
 
@@ -18,7 +19,7 @@ describe("SocketService tests", () => {
   let socketService: SocketService;
 
   beforeEach(() => {
-    socketService = new SocketService(mock(ChatViewService));
+    socketService = new SocketService(mock(ChatViewService), mock(GameViewSimpleService));
   });
 
   it("should call socket.emit() when calling sendMsg()", () => {
