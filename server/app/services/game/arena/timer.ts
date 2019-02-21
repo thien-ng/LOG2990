9589d1fdf7dbb6e5db2b20a96c67af4ae7ac7e7b
Jeremy <jeremyyoyo@hotmail.fr>
2019-02-21 16:28:39 -0500

    JB D: initialisation des variables pour classe timer

--- server/app/services/game/arena/timer.ts
+++ server/app/services/game/arena/timer.ts
@@ -1,6 +1,12 @@
+import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+import { Constants } from "../../../constants";
+
 export class Timer {
 
-    // private secondsSinceStart: number;
+    private secondsSinceStart: number;
+    private timerUpdated: Subject<number>;
+    private timer: NodeJS.Timeout;
 
     public constructor() {
         // this.secondsSinceStart = 0;
