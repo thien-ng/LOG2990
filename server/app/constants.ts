d86d9ab7a9a31b0f989ea673b178339f24a16e45
jeremy <jeremyyoyo@hotmail.fr>
2019-02-21 03:00:55 -0500

    JB D: ajouter constante

--- server/app/constants.ts
+++ server/app/constants.ts
@@ -15,6 +15,7 @@ export class Constants {
   public static readonly CLICK_EVENT:               string = "onClick";
   public static readonly ON_ARENA_RESPONSE:         string = "onArenaResponse";
   public static readonly ON_TIMER_UPDATE:           string = "onTimerUpdate";
+  public static readonly ON_POINT_ADDED:            string = "onPointAdded";
 
   // Constants for card-manager.service.ts
   public static readonly SCENE_SNAPSHOT:            string = "_snapshot.jpeg";
