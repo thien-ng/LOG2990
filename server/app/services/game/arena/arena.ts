c43a8fa62da70b6f4803569261b07052f5f5a233
Michael Sauget <michaelsauget@gmail.com>
2019-02-21 02:38:05 -0500

    MS D: Ajout de la fonction vérifiant si la partie est terminée. Retourne vrai seulement si un des joueurs a atteint le quota exigé ou si toutes les différences ont été trouvées.

--- server/app/services/game/arena/arena.ts
+++ server/app/services/game/arena/arena.ts
@@ -167,6 +167,17 @@ export class Arena {
             player.addPoints(1);
         }
     }
+
+    private gameIsFinished(): boolean {
+
+        const playerHasReachPointsNeeded: boolean = this.players.some((player: Player) => {
+            return player.points >= this.pointsNeededToWin;
+        });
+        const differenceAreAllFound: boolean = this.differencesFound.length >= this.originalPixelClusters.size;
+
+        return playerHasReachPointsNeeded || differenceAreAllFound;
+    }
+
     private buildPlayerInputResponse(status: string, response: IOriginalPixelCluster): IPlayerInputResponse {
         return {
             status: status,
