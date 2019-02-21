114169971512a8eccc2f8ce940e309b1b13bd301
Jeremy <jeremyyoyo@hotmail.fr>
2019-02-21 03:29:23 -0500

    JB D: relier les observables au component

--- client/src/app/game-view/difference-counter/difference-counter.service.ts
+++ client/src/app/game-view/difference-counter/difference-counter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
 import { Constants } from "../../constants";
 @Injectable({
   providedIn: "root",
@@ -6,6 +7,21 @@ import { Constants } from "../../constants";
 export class DifferenceCounterService {
 
   private maxError: number;
+  private counterUpdated: BehaviorSubject<number>;
+  private counterUpdatedObservable: Observable<number>;
+
+  public constructor() {
+    this.counterUpdated = new BehaviorSubject<number>(0);
+    this.counterUpdatedObservable = this.counterUpdated.asObservable();
+  }
+
+  public get counter(): Observable<number> {
+    return this.counterUpdatedObservable;
+  }
+
+  public updateCounter(newValue: number): void {
+    this.counterUpdated.next(newValue);
+  }
 
   public convertErrorToPercent(errorFoundCounter: number): number {
     return errorFoundCounter * Constants.PERCENT / this.maxError;
