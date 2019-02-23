2a56b26267957f81654b09a945e635ecbd984191
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-23 18:58:43 -0500

    GHV T: test pour  le retour de onCanvasClick

--- client/src/app/game-view/game-view-simple/game-view-simple.service.spec.ts
+++ client/src/app/game-view/game-view-simple/game-view-simple.service.spec.ts
@@ -1,15 +1,33 @@
 
-import { TestBed } from "@angular/core/testing";
+import { inject, TestBed } from "@angular/core/testing";
+import { IClickMessage, IPosition2D } from "../../../../../common/communication/iGameplay";
 import { GameViewSimpleService } from "./game-view-simple.service";
 
 // tslint:disable:no-any
 
 describe("GameViewService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [GameViewSimpleService],
+  }));
 
-  it("should be created", () => {
-    const service: GameViewSimpleService = TestBed.get(GameViewSimpleService);
-    expect(service).toBeTruthy();
-  });
+  it("should return an IClickMessage", inject([GameViewSimpleService], (gameViewService: GameViewSimpleService) => {
+    const position: IPosition2D = {
+      x: 10,
+      y: 15,
+    };
+
+    const arenaId: number = 1;
+    const username: string = "myname";
+
+    const clickMessage: IClickMessage = {
+      position: {x: 10, y: 15},
+      arenaID: 1,
+      username: "myname",
+    };
+
+    const generatedClickMessage: IClickMessage = gameViewService.onCanvasClick(position, arenaId, username);
+
+    expect(generatedClickMessage).toEqual(clickMessage);
+  }));
 
 });
