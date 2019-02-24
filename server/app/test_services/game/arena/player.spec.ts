dd178423564ef4ed3a0893574192daf530e31842
Michael Sauget <michaelsauget@gmail.com>
2019-02-23 21:23:26 -0500

    MS T: Ajout des variables utilisées pour faire les tests sur le player.

---
+++ server/app/test_services/game/arena/player.spec.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+
+import { expect } from "chai";
+import { IUser } from "../../../../../common/communication/iUser";
+import { Player } from "../../../services/game/arena/player";
+
+// tslint:disable:no-magic-numbers
+
+const user: IUser = {
+    username: "Ettore Merlo",
+    socketID: "666",
+};
+let player: Player;
