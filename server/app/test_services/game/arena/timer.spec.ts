a25d05da8b36960da0a2266d5612f7ae7b59d52f
Michael Sauget <michaelsauget@gmail.com>
2019-02-24 15:15:12 -0500

    MS T: Ajout des imports et varaibles pour tester le timer.

---
+++ server/app/test_services/game/arena/timer.spec.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+
+import * as chai from "chai";
+import * as spies from "chai-spies";
+import { Timer } from "../../../services/game/arena/timer";
+
+// tslint:disable:no-magic-numbers no-any
+
+let timer: Timer;
+
