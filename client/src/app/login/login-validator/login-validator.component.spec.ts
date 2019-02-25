f6be148007f14cc8659196f58f292ad6b33d8b85
Syn92 <Syn92@users.noreply.github.com>
2019-02-24 23:33:56 -0500

    AG R: obligatory catch

--- client/src/app/login/login-validator/login-validator.component.spec.ts
+++ client/src/app/login/login-validator/login-validator.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { Constants } from "src/app/constants";
 import { TestingImportsModule } from "../../testing-imports/testing-imports.module";
 import { LoginValidatorComponent } from "./login-validator.component";
 
@@ -17,7 +18,7 @@ describe("LoginValidatorComponent", () => {
         RouterTestingModule,
       ],
     })
-    .compileComponents().catch();
+    .compileComponents().catch(() => Constants.OBLIGATORY_CATCH);
   }));
 
   beforeEach(() => {
