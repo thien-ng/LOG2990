c02443ea34814280735cc1a378542e4964170992
Jeremy <jeremyyoyo@hotmail.fr>
2019-02-24 19:26:32 -0500

    JB T: ajouter un test de robustesse pour cluster counter

--- server/app/services/difference-checker/tests/clusterCounter.spec.ts
+++ server/app/services/difference-checker/tests/clusterCounter.spec.ts
@@ -94,6 +94,26 @@ describe("Cluster Counter tests", () => {
         done();
     });
 
+    it("should count clusters in a vertical array", (done: Function) => {
+
+        const HEIGHT:   number = 8;
+        const WIDTH:    number = 1;
+
+        const newBuilder: BMPBuilder = new BMPBuilder(WIDTH, HEIGHT, WHITE);
+        newBuilder.setColorAtPos(BLACK, BLACK, BLACK, 0, 1);
+        newBuilder.setColorAtPos(BLACK, BLACK, BLACK, 0, 2);
+        newBuilder.setColorAtPos(BLACK, BLACK, BLACK, 0, 4);
+        newBuilder.setColorAtPos(BLACK, BLACK, BLACK, 0, 5);
+
+        const bufferWIthDiff: Buffer = Buffer.from(newBuilder.buffer);
+
+        const clusterCounter: ClusterCounter = new ClusterCounter(bufferWIthDiff, WIDTH);
+        const numberOfDiffFound: number = clusterCounter.countAllClusters();
+
+        expect(numberOfDiffFound).to.equal(2);
+        done();
+    });
+
     it("should count clusters in an single cell array with a difference", (done: Function) => {
         const width:    number = 1;
         const height:   number = 1;
@@ -110,52 +130,28 @@ describe("Cluster Counter tests", () => {
         done();
     });
 
-    // it("should count clusters in an single cell array without a difference", (done: Function) => {
-    //     const diffList: number[] = [ 0 ];
-    //     const WIDTH: number = 1;
+    it("should count no clusters in an empty given array", (done: Function) => {
+        const diffList: Buffer = Buffer.alloc(0);
+        const WIDTH: number = 0;
 
-    //     const clusterCounter: ClusterCounter = new ClusterCounter(diffList, WIDTH);
-    //     const count: number = clusterCounter.countAllClusters();
-    //     const expectedAnswer: number = 0;
-    //     expect(count).to.equal(expectedAnswer);
-    //     done();
-    // });
+        const clusterCounter: ClusterCounter = new ClusterCounter(diffList, WIDTH);
+        const count: number = clusterCounter.countAllClusters();
 
-    // it("should count no clusters in an empty given array", (done: Function) => {
-    //     const diffList: number[] = [ ];
-    //     const WIDTH: number = 0;
+        expect(count).to.equal(0);
+        done();
+    });
 
-    //     const clusterCounter: ClusterCounter = new ClusterCounter(diffList, WIDTH);
-    //     const count: number = clusterCounter.countAllClusters();
-    //     const expectedAnswer: number = 0;
-    //     expect(count).to.equal(expectedAnswer);
-    //     done();
-    // });
+    it("should handle a given width bigger than the given array length ", (done: Function) => {
+        const newBuilder: BMPBuilder = new BMPBuilder(1, 1, WHITE);
+        newBuilder.setColorAtPos(BLACK, BLACK, BLACK, 0, 0);
+        const WIDTH: number = 10;
 
-    // it("should handle a given width bigger than the given array length ", (done: Function) => {
-    //     const diffList: number[] = [ 0, 0,
-    //                                  1, 0,
-    //                                 ];
-    //     const WIDTH: number = 10;
+        const bufferWIthDiff: Buffer = Buffer.from(newBuilder.buffer);
 
-    //     const clusterCounter: ClusterCounter = new ClusterCounter(diffList, WIDTH);
-    //     const count: number = clusterCounter.countAllClusters();
-    //     const expectedAnswer: number = 1;
-    //     expect(count).to.equal(expectedAnswer);
-    //     done();
-    // });
+        const clusterCounter: ClusterCounter = new ClusterCounter(bufferWIthDiff, WIDTH);
+        const count: number = clusterCounter.countAllClusters();
 
-    // it("should handle a given array with errors in data (something else than 0's and 1's", (done: Function) => {
-    //     const diffList: number[] = [ 0, 0, 6,
-    //                                  1, 0, 0,
-    //                                  0, 0, 0,
-    //                                 ];
-    //     const WIDTH: number = 3;
-
-    //     const clusterCounter: ClusterCounter = new ClusterCounter(diffList, WIDTH);
-    //     const count: number = clusterCounter.countAllClusters();
-    //     const expectedAnswer: number = 1;
-    //     expect(count).to.equal(expectedAnswer);
-    //     done();
-    // });
+        expect(count).to.equal(1);
+        done();
+    });
 });
