6e1acdd515033227de6ef1b58e5c298d80092b89
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-02-24 23:30:09 -0500

    GHV A: correction lint

--- client/src/app/create-free-game/create-free-game.component.ts
+++ client/src/app/create-free-game/create-free-game.component.ts
@@ -21,11 +21,11 @@ import { Constants } from "../constants";
 })
 export class CreateFreeGameComponent {
 
-  public isButtonEnabled: boolean = true;
-  public sliderValue: number = 100;
-  public addChecked: boolean = false;
-  public delChecked: boolean = false;
-  public colorChecked: boolean = false;
+  public isButtonEnabled: boolean;
+  public sliderValue: number;
+  public addChecked: boolean;
+  public delChecked: boolean;
+  public colorChecked: boolean;
   public readonly INVALID_NAME: string = "Nom invalide";
   public readonly MAX_VALUE: number = 200;
   public readonly MIN_VALUE: number = 10;
@@ -58,6 +58,12 @@ export class CreateFreeGameComponent {
     private httpClient: HttpClient,
     private snackBar: MatSnackBar,
   ) {
+
+    this.isButtonEnabled = true;
+    this.sliderValue = Constants.DEFAULT_SLIDER_VALUE;
+    this.addChecked = false;
+    this.delChecked = false;
+    this.colorChecked = false;
 
     const controls: FormControl[] = this.modifTypes.map(() => new FormControl(false));
     this.formControl = this.formBuilder.group({
