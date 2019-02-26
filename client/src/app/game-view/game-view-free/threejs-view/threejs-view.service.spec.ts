5ea31d8345fe826a87954a60b66aa29b3c3daed8
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 21:13:03 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #310

--- client/src/app/game-view/game-view-free/threejs-view/threejs-view.service.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-view.service.spec.ts
@@ -8,23 +8,23 @@ import { ThreejsViewService } from "./threejs-view.service";
 // tslint:disable:no-any
 
 const sceneVariables: ISceneVariables = {
-  theme: 1,
-  gameName: "gameName",
-  sceneObjectsQuantity: 1,
+  theme:                  1,
+  gameName:               "gameName",
+  sceneObjectsQuantity:   1,
   sceneObjects: [
     {
-      id: 1,
-      type: SceneObjectType.Cone,
-      position: {x: 1, y: 1, z: 1},
-      rotation: {x: 1, y: 1, z: 1},
-      color: "#FFFFFF",
-      scale: {x: 1, y: 1, z: 1},
+      id:         1,
+      type:       SceneObjectType.Cone,
+      position:   { x: 1, y: 1, z: 1 },
+      rotation:   { x: 1, y: 1, z: 1 },
+      color:      "#FFFFFF",
+      scale:      { x: 1, y: 1, z: 1 },
     },
   ],
   sceneBackgroundColor: "#FFFFFF",
 };
 const renderer: THREE.WebGLRenderer = mock(THREE.WebGLRenderer);
-const scene: THREE.Scene = mock(THREE.Scene);
+const scene:    THREE.Scene         = mock(THREE.Scene);
 
 describe("ThreejsViewService Tests", () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -37,17 +37,16 @@ describe("ThreejsViewService Tests", () => {
     expect(spy).toHaveBeenCalled();
   }));
 
-  it("should add lighting in scene", inject([ThreejsViewService], (threejsViewService: ThreejsViewService) => {
+  it("should add lighting in scene",    inject([ThreejsViewService], (threejsViewService: ThreejsViewService) => {
     const spy: any = spyOn<any>(threejsViewService, "createLighting");
     threejsViewService.createScene(scene, sceneVariables, renderer);
     expect(spy).toHaveBeenCalled();
   }));
 
-  it("should render scene", inject([ThreejsViewService], (threejsViewService: ThreejsViewService) => {
+  it("should render scene",             inject([ThreejsViewService], (threejsViewService: ThreejsViewService) => {
     const spy: any = spyOn<any>(threejsViewService, "renderObject");
     threejsViewService.createScene(scene, sceneVariables, renderer);
     threejsViewService.animate();
     expect(spy).toHaveBeenCalled();
   }));
-
 });
