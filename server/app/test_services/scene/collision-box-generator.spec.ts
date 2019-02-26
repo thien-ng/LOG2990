81d7007839093c4def3ed559115b1a35d7fbef7b
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 19:21:14 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #221

--- server/app/test_services/scene/collision-box-generator.spec.ts
+++ server/app/test_services/scene/collision-box-generator.spec.ts
@@ -25,12 +25,13 @@ describe("Collision box generator tests", () => {
     };
 
     const cylinder: ISceneObject = {
-        id: 2,
-        type: SceneObjectType.Cylinder,
+        id:     2,
+        type:   SceneObjectType.Cylinder,
         position: {
-            x: 20, y: 20, z: 20 },
+            x: 20, y: 20, z: 20,
+        },
         rotation: {
-            x: 1, y: 1, z: 1,
+            x: 1,  y: 1,  z: 1,
         },
         scale: {
             x: 10, y: 10, z: 10,
