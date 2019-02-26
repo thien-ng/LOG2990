40eb856ea1038ae3426df7c74fd2294df0eb248f
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 19:46:10 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #245

--- common/communication/iSceneVariables.ts
+++ common/communication/iSceneVariables.ts
@@ -2,14 +2,14 @@ import { ISceneObject } from "./iSceneObject";
 import { SceneType } from "./iSceneOptions";
 
 export interface ISceneVariablesMessage {
-    originalScene: ISceneVariables;
-    modifiedScene: ISceneVariables;
+    originalScene:          ISceneVariables;
+    modifiedScene:          ISceneVariables;
 }
 
 export interface ISceneVariables {
-    theme: SceneType;
-    gameName: string;
-    sceneObjectsQuantity: number;
-    sceneObjects: ISceneObject[];
-    sceneBackgroundColor: string;
+    theme:                  SceneType;
+    gameName:               string;
+    sceneObjectsQuantity:   number;
+    sceneObjects:           ISceneObject[];
+    sceneBackgroundColor:   string;
 }
