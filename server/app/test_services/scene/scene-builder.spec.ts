f782fc2babfe4cbef6e87068547bfca051ab80b9
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 19:18:48 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #206

--- server/app/test_services/scene/scene-builder.spec.ts
+++ server/app/test_services/scene/scene-builder.spec.ts
@@ -54,15 +54,12 @@ describe("Scene builder tests", () => {
     });
 
     it("should generate colors in hex format", () => {
-
         const color: string = sceneBuilder.generateRandomColor();
         chai.expect(color.slice(0, 1)).equal("#");
     });
 
     it("should generate colors as string of length 8", () => {
-
         const color: string = sceneBuilder.generateRandomColor();
         chai.expect(color.length).equal(7);
     });
-
 });
