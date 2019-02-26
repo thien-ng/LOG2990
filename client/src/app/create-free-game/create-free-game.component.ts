a1b6689d92864c55ced0b7b843fd543dda9bddaf
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 21:08:45 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #286

--- client/src/app/create-free-game/create-free-game.component.ts
+++ client/src/app/create-free-game/create-free-game.component.ts
@@ -15,55 +15,56 @@ import { FormMessage } from "../../../../common/communication/message";
 import { Constants } from "../constants";
 
 @Component({
-  selector: "app-create-free-game",
-  templateUrl: "./create-free-game.component.html",
-  styleUrls: ["./create-free-game.component.css"],
+  selector:     "app-create-free-game",
+  templateUrl:  "./create-free-game.component.html",
+  styleUrls:    ["./create-free-game.component.css"],
 })
 export class CreateFreeGameComponent {
 
-  public isButtonEnabled: boolean;
-  public sliderValue: number;
-  public addChecked: boolean;
-  public delChecked: boolean;
-  public colorChecked: boolean;
-  public readonly INVALID_NAME: string = "Nom invalide";
-  public readonly MAX_VALUE: number = 200;
-  public readonly MIN_VALUE: number = 10;
-  public readonly SUBMIT: string = "Soumettre";
-  public readonly CANCEL: string = "Annuler";
-  public readonly TITLE: string = "Créer un jeu de point de vue libre";
-  public readonly PLACE_HOLDER_NAME: string = "Nom du jeu";
-  public readonly PLACE_HOLDER_TYPE: string = "Type d'objet";
-  public readonly GEOMETRIC_OPTION: string = "Formes géométriques";
-  public readonly THEMATIC_OPTION: string = "Thématique";
-  public readonly EDIT_TYPE_ADD: string = "Ajout";
-  public readonly EDIT_TYPE_DELETE: string = "Suppression";
-  public readonly EDIT_TYPE_COLOR: string = "Changement de couleur";
-  public readonly ATLEASTONE_CHECKED: string = "Au moins une option doit être cochée";
-  public readonly NEEDED_SNAPSHOT: boolean = true;
+  public readonly INVALID_NAME:         string = "Nom invalide";
+  public readonly MAX_VALUE:            number = 200;
+  public readonly MIN_VALUE:            number = 10;
+  public readonly SUBMIT:               string = "Soumettre";
+  public readonly CANCEL:               string = "Annuler";
+  public readonly TITLE:                string = "Créer un jeu de point de vue libre";
+  public readonly PLACE_HOLDER_NAME:    string = "Nom du jeu";
+  public readonly PLACE_HOLDER_TYPE:    string = "Type d'objet";
+  public readonly GEOMETRIC_OPTION:     string = "Formes géométriques";
+  public readonly THEMATIC_OPTION:      string = "Thématique";
+  public readonly EDIT_TYPE_ADD:        string = "Ajout";
+  public readonly EDIT_TYPE_DELETE:     string = "Suppression";
+  public readonly EDIT_TYPE_COLOR:      string = "Changement de couleur";
+  public readonly ATLEASTONE_CHECKED:   string = "Au moins une option doit être cochée";
+  public readonly NEEDED_SNAPSHOT:      boolean = true;
+
+  public isButtonEnabled:               boolean;
+  public sliderValue:                   number;
+  public addChecked:                    boolean;
+  public delChecked:                    boolean;
+  public colorChecked:                  boolean;
 
   public readonly modifTypes: {name: string}[] = [
-      { name: this.EDIT_TYPE_ADD },
-      { name: this.EDIT_TYPE_DELETE},
-      { name: this.EDIT_TYPE_COLOR},
+      { name:   this.EDIT_TYPE_ADD    },
+      { name:   this.EDIT_TYPE_DELETE },
+      { name:   this.EDIT_TYPE_COLOR  },
     ];
 
-  public formControl: FormGroup;
-  public isSceneGenerated: boolean;
-  public iSceneVariablesMessage: ISceneVariablesMessage;
+  public formControl:             FormGroup;
+  public isSceneGenerated:        boolean;
+  public iSceneVariablesMessage:  ISceneVariablesMessage;
 
   public constructor(
-    public dialogRef: MatDialogRef<CreateFreeGameComponent>,
-    private formBuilder: FormBuilder,
-    private httpClient: HttpClient,
-    private snackBar: MatSnackBar,
+    public  dialogRef:    MatDialogRef<CreateFreeGameComponent>,
+    private formBuilder:  FormBuilder,
+    private httpClient:   HttpClient,
+    private snackBar:     MatSnackBar,
   ) {
 
-    this.isButtonEnabled = true;
-    this.sliderValue = Constants.DEFAULT_SLIDER_VALUE;
-    this.addChecked = false;
-    this.delChecked = false;
-    this.colorChecked = false;
+    this.isButtonEnabled  = true;
+    this.sliderValue      = Constants.DEFAULT_SLIDER_VALUE;
+    this.addChecked       = false;
+    this.delChecked       = false;
+    this.colorChecked     = false;
 
     const controls: FormControl[] = this.modifTypes.map(() => new FormControl(false));
     this.formControl = this.formBuilder.group({
@@ -98,9 +99,9 @@ export class CreateFreeGameComponent {
   }
 
   public hasFormControlErrors(): boolean {
-    const hasErrorForm: Boolean = this.formControl.controls.nameControl.errors == null;
-    const checkboxChecked: Boolean = this.formControl.controls.modifTypes.valid;
-    const selectedOne: Boolean = this.formControl.controls.selectControl.valid;
+    const hasErrorForm:     Boolean = this.formControl.controls.nameControl.errors == null;
+    const checkboxChecked:  Boolean = this.formControl.controls.modifTypes.valid;
+    const selectedOne:      Boolean = this.formControl.controls.selectControl.valid;
 
     return !(hasErrorForm && checkboxChecked && selectedOne && this.isButtonEnabled);
   }
@@ -125,10 +126,10 @@ export class CreateFreeGameComponent {
 
   private createFormMessage(formData: NgForm): FormMessage {
     return {
-      gameName: formData.value.nameControl,
-      checkedTypes: formData.value.modifTypes,
-      theme: formData.value.selectControl,
-      quantityChange: this.sliderValue,
+      gameName:         formData.value.nameControl,
+      checkedTypes:     formData.value.modifTypes,
+      theme:            formData.value.selectControl,
+      quantityChange:   this.sliderValue,
     } as FormMessage;
   }
 
@@ -141,7 +142,7 @@ export class CreateFreeGameComponent {
         this.openSnackBar(response, Constants.SNACK_ACTION);
       } else {
         this.iSceneVariablesMessage = response;
-        this.isSceneGenerated = true;
+        this.isSceneGenerated       = true;
       }
 
     });
