8ded536c32f9573b1cd5889643f07cb7cd86a419
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 19:44:24 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #234

--- common/communication/highscore.ts
+++ common/communication/highscore.ts
@@ -1,13 +1,13 @@
 export interface Highscore {
-    id: number,
-    timesSingle: [number, number, number],
-    timesMulti: [number, number, number],
+    id:             number,
+    timesSingle:    [number, number, number],
+    timesMulti:     [number, number, number],
   }
 
 export interface HighscoreMessage {
-    id: number,
-    timesSingle: [string, string, string],
-    timesMulti: [string, string, string],
+    id:             number,
+    timesSingle:    [string, string, string],
+    timesMulti:     [string, string, string],
 }
 
 export enum Mode{
