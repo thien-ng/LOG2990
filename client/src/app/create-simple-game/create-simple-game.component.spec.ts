623f4cc2d87b128473bb426a8e4117a5786c4ffb
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 21:08:55 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #287

--- client/src/app/create-simple-game/create-simple-game.component.spec.ts
+++ client/src/app/create-simple-game/create-simple-game.component.spec.ts
@@ -6,18 +6,18 @@ import { TestingImportsModule } from "../testing-imports/testing-imports.module"
 import { CreateSimpleGameComponent } from "./create-simple-game.component";
 
 describe("CreateSimpleGameComponent", () => {
-  let component: CreateSimpleGameComponent;
-  let fixture: ComponentFixture<CreateSimpleGameComponent>;
+  let component:  CreateSimpleGameComponent;
+  let fixture:    ComponentFixture<CreateSimpleGameComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateSimpleGameComponent ],
-      imports: [TestingImportsModule],
-      providers: [{
-        provide: MatDialogRef,
+      declarations:   [ CreateSimpleGameComponent ],
+      imports:        [TestingImportsModule],
+      providers:      [{
+        provide:  MatDialogRef,
         useValue: {},
       },
-                  {
+                       {
         provide: MAT_DIALOG_DATA,
       }],
     })
@@ -26,7 +26,7 @@ describe("CreateSimpleGameComponent", () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CreateSimpleGameComponent);
+    fixture   = TestBed.createComponent(CreateSimpleGameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
