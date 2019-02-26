3fb24394cf2905af38f5d5d8847793c4bc306220
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 19:44:46 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #236

--- common/communication/iCard.ts
+++ common/communication/iCard.ts
@@ -14,19 +14,19 @@ export interface ICard {
   }
 
 export const DefaultCard2D: ICard = {
-  gameID: 1,
-  gamemode: GameMode.simple,
-  title: "Stewie deathray",
-  subtitle: "Default Image",
-  avatarImageUrl: "http://localhost:3000/image/default.gif",
-  gameImageUrl: "http://localhost:3000/image/default.gif",
+  gameID:             1,
+  gamemode:           GameMode.simple,
+  title:              "Stewie deathray",
+  subtitle:           "Default Image",
+  avatarImageUrl:     "http://localhost:3000/image/default.gif",
+  gameImageUrl:       "http://localhost:3000/image/default.gif",
 }
 
 export const DefaultCard3D: ICard = {
-  gameID: 1,
-  gamemode: GameMode.free,
-  title: "Scène par défaut",
-  subtitle: "Scène par défaut",
-  avatarImageUrl: "http://localhost:3000/image/1_snapshot.jpeg",
-  gameImageUrl: "http://localhost:3000/image/1_snapshot.jpeg",
+  gameID:             1,
+  gamemode:           GameMode.free,
+  title:              "Scène par défaut",
+  subtitle:           "Scène par défaut",
+  avatarImageUrl:     "http://localhost:3000/image/1_snapshot.jpeg",
+  gameImageUrl:       "http://localhost:3000/image/1_snapshot.jpeg",
 }
