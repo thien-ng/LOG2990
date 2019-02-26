13451b4da9cc46a2e7e02052912a173a08e474bf
Michael Sauget <michaelsauget@gmail.com>
2019-02-25 19:20:31 -0500

    MS R: Refactor pour alignement et padding. Un parmi de multiples commit. commit #215

--- server/app/test_services/card-operations.service.spec.ts
+++ server/app/test_services/card-operations.service.spec.ts
@@ -29,12 +29,12 @@ describe("Card-operations tests", () => {
     };
 
     const c2: ICard = {
-        gameID: 2,
-        title: "Default 3D",
-        subtitle: "default 3D",
-        avatarImageUrl: FAKE_PATH + "/moutain.jpg",
-        gameImageUrl: FAKE_PATH + "/moutain.jpg",
-        gamemode: GameMode.free,
+        gameID:             2,
+        title:              "Default 3D",
+        subtitle:           "default 3D",
+        avatarImageUrl:     FAKE_PATH + "/moutain.jpg",
+        gameImageUrl:       FAKE_PATH + "/moutain.jpg",
+        gamemode:           GameMode.free,
     };
 
     const c3: ICard = {
