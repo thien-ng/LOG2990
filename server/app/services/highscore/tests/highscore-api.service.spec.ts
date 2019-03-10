50915fa04dfa0cff3bc7e6df7d5d8e4d03803d4c
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-09 19:39:24 -0500

    JB T: ajout dun test pour le microservice highcore

--- server/app/services/highscore/tests/highscore-api.service.spec.ts
+++ server/app/services/highscore/tests/highscore-api.service.spec.ts
@@ -29,5 +29,11 @@ describe("Highscore micro service tests", () => {
         const newHighscore: Highscore = highscoreService.checkScore({username: "cpu", time: 1}, mockHighscore, Mode.Singleplayer);
         expect(newHighscore.timesSingle[0].time).to.be.equal(1);
     });
+
+    it("Should update the times when the new value is smaller than any of the previous highscores (multiplayer)", () => {
+        const newHighscore: Highscore = highscoreService.checkScore({username: "cpu", time: 1}, mockHighscore, Mode.Multiplayer);
+        expect(newHighscore.timesMulti[0].time).to.be.equal(1);
+    });
+
    });
 });
