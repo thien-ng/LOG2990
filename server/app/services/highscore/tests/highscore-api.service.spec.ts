86a1196de820de8ee80168e08a609ec52c8a85ea
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-09 19:38:48 -0500

    JB T: initialisation des variables

--- server/app/services/highscore/tests/highscore-api.service.spec.ts
+++ server/app/services/highscore/tests/highscore-api.service.spec.ts
@@ -12,5 +12,12 @@ let mockHighscore: Highscore;
 describe("Highscore micro service tests", () => {
 
     beforeEach(() => {
+        mockHighscore = {
+            id:             1,
+            timesSingle:    [{username: "cpu", time: 2}, {username: "cpu", time: 4}, {username: "cpu", time: 6}],
+            timesMulti:     [{username: "cpu", time: 2}, {username: "cpu", time: 4}, {username: "cpu", time: 6}],
+        };
+        highscoreService = new HighscoreApiService();
+   });
    });
 });
