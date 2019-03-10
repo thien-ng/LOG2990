31acbb99b265abd53a72cb9a43b59cda8219595b
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-10 17:39:48 -0400

    JB D: ajout d'interface au microservice

--- server/app/services/highscore/utilities/interfaces.ts
+++ server/app/services/highscore/utilities/interfaces.ts
@@ -3,6 +3,12 @@ export interface Time {
     time: number;
 }
 
+export interface Highscore {
+    id:             number;
+    timesSingle:    [Time, Time, Time];
+    timesMulti:     [Time, Time, Time];
+}
+
 export enum Position {
     position1 = 0,
     position2 = 1,
