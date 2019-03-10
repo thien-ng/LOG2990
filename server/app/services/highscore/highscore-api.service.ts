127cb4ac9ec3b8de25d899add023e1fe2e354960
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-09 19:35:13 -0500

    JB R: ajouter une une fonction pour du code reutilise

--- server/app/services/highscore/highscore-api.service.ts
+++ server/app/services/highscore/highscore-api.service.ts
@@ -1,4 +1,5 @@
 import { injectable } from "inversify";
+import { Highscore, Mode } from "../../../../common/communication/highscore";
 import { Time } from "./utilities/interfaces";
 
 const REMOVE_NOTHING: number = 0;
@@ -9,8 +10,23 @@ export class HighscoreApiService {
         if (newValue.time < 0) {
             return times;
         }
-        let hasBeenReplaced: Boolean = false;
 
+        switch (mode) {
+            case Mode.Singleplayer:
+                times.timesSingle = this.sortTimes(times.timesSingle, newValue);
+                break;
+            case Mode.Multiplayer:
+                times.timesMulti = this.sortTimes(times.timesMulti, newValue);
+                break;
+            default:
+                break;
+        }
+
+        return times;
+    }
+
+    private sortTimes(times: [Time, Time, Time], newValue: Time): [Time, Time, Time] {
+        let hasBeenReplaced: Boolean = false;
         times.forEach((element: Time) => {
             if (element.time > newValue.time && !hasBeenReplaced) {
                 times.splice(times.indexOf(element), REMOVE_NOTHING, newValue);
