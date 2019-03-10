960466985562e55ddd049278f2303cc5305d058e
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-10 17:44:06 -0400

    JB R: resituer / modifier fonction public

--- server/app/services/highscore/tests/highscore-api.service.spec.ts
+++ server/app/services/highscore/tests/highscore-api.service.spec.ts
@@ -45,7 +45,14 @@ describe("Highscore micro service tests", () => {
 
     it("Should update the times when the new value is smaller than any of the previous highscores (multiplayer)", () => {
         const newHighscore: Highscore = highscoreService.checkScore({username: "cpu", time: 1}, mockHighscore, Mode.Multiplayer);
-        expect(newHighscore.timesMulti[0].time).to.be.equal(1);
+            newValue: {username: "cpu", time: 1},
+            mode: Mode.Multiplayer,
+            times: mockHighscore,
+        };
+        const newHighscore: HighscoreValidationResponse = highscoreService.checkScoreRoutine(param);
+        expect(newHighscore.highscore.timesMulti[0].time).to.be.equal(1);
+    });
+
     });
 
     it("Should fail quietly if the mode is undefined", () => {
