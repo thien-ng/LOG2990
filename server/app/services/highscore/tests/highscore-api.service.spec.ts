d38d4c5ca451d3929281418c7e8a5d461b963cc9
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-09 19:38:28 -0500

    JB T: creation du describe / before each pour les tests du micro service

--- server/app/services/highscore/tests/highscore-api.service.spec.ts
+++ server/app/services/highscore/tests/highscore-api.service.spec.ts
@@ -1,3 +1,16 @@
 import { expect } from "chai";
 import { Highscore, Mode } from "../../../../../common/communication/highscore";
 import { HighscoreApiService } from "../highscore-api.service";
+
+// tslint:disable:no-magic-numbers no-any
+
+const UNDEFINED: number = 4;
+
+let highscoreService: HighscoreApiService;
+let mockHighscore: Highscore;
+
+describe("Highscore micro service tests", () => {
+
+    beforeEach(() => {
+   });
+});
