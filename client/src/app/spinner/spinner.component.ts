4989755c8ddf6cd9569527aaf5f41f494bbe9836
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-10 00:23:04 -0500

    GHV D: implementation du spinner

--- client/src/app/spinner/spinner.component.ts
+++ client/src/app/spinner/spinner.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-spinner",
   templateUrl: "./spinner.component.html",
-  styleUrls: ["./spinner.component.css"],
+  styleUrls: ["./spinner.component.scss"],
 })
 export class SpinnerComponent implements OnInit {
 
@@ -12,7 +12,7 @@ export class SpinnerComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    //
+    console.log("init du spinner");
   }
 
 }
