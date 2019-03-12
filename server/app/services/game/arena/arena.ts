602c5d256b305e4b3d16d7e45a42bc9aa79429bc
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-12 04:56:36 -0400

    GHV-MS D: imports pour Arena abstraite

--- server/app/services/game/arena/arena.ts
+++ server/app/services/game/arena/arena.ts
@@ -1,6 +1,16 @@
+import { AxiosInstance, AxiosResponse } from "axios";
+import { inject } from "inversify";
 import { GameMode } from "../../../../../common/communication/iCard";
+import { IArenaResponse } from "../../../../../common/communication/iGameplay";
+import { IUser } from "../../../../../common/communication/iUser";
+import Types from "../../../types";
+import { GameManagerService } from "../game-manager.service";
+import { IArenaInfos, IHitConfirmation } from "./interfaces";
+import { Player } from "./player";
+// import { Referee } from "./referee";
+import { Timer } from "./timer";
 
-export class Arena<DIFF_T> {
+const axios: AxiosInstance = require("axios");
 
     public readonly ARENA_TYPE: GameMode = GameMode.simple;
     public DEFAULT_DIFF_TO_UPDATE: DIFF_T;
