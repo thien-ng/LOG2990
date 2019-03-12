7134da06d1c1a43ea771d3ea38715604571c4da2
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-12 02:37:00 -0400

    GHV-MS R: definition du game mode de l'arena (valeur temporaire!)

--- server/app/services/game/arena/arena.ts
+++ server/app/services/game/arena/arena.ts
@@ -1,6 +1,7 @@
 import { GameMode } from "../../../../../common/communication/iCard";
 
 
+    public readonly ARENA_TYPE: GameMode = GameMode.simple;
 
     public sendMessage(playerSocketId: string, event: string, message: number): void {
         this.gameManagerService.sendMessage(playerSocketId, event, message);
