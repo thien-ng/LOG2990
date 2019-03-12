2bf1df2d5b6d80b329fd429ddcd28f06bddd556f
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-12 02:37:49 -0400

    GHV-MS R: templatage de Arena + attribut de DIFF_TO_UPDATE selon template

--- server/app/services/game/arena/arena.ts
+++ server/app/services/game/arena/arena.ts
@@ -1,7 +1,9 @@
 import { GameMode } from "../../../../../common/communication/iCard";
 
+export class Arena<DIFF_T> {
 
     public readonly ARENA_TYPE: GameMode = GameMode.simple;
+    public DEFAULT_DIFF_TO_UPDATE: DIFF_T;
 
     public sendMessage(playerSocketId: string, event: string, message: number): void {
         this.gameManagerService.sendMessage(playerSocketId, event, message);
