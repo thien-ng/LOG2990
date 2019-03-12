3c61dbbb9b263363c74a52eabd4b0b66e05151c1
Michael Sauget <michaelsauget@gmail.com>
2019-03-11 21:31:56 -0400

    GHV-MS: Ajout des attributs et du constructeur

--- server/app/services/hitValidator/hitValidator3D.service.ts
+++ server/app/services/hitValidator/hitValidator3D.service.ts
@@ -5,4 +5,15 @@ import { IHitConfirmation, IHitToValidate, IImageToCache } from "./interfaces";
 
 @injectable()
 export class HitValidatorService3D {
+
+    private readonly ERROR_ON_HTTPGET:      string =
+    "Didn't succeed to get scene buffer from URL given.";
+    private readonly CACHE_SIZE:            number = 5;
+
+    private cache: Cache;
+
+    public constructor() {
+        this.cache = new Cache(this.CACHE_SIZE);
+    }
+
 }
