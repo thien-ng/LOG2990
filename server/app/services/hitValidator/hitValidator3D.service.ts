8a375e84e42df6a5dcb0af050f1bd689fb35aff5
Michael Sauget <michaelsauget@gmail.com>
2019-03-11 21:31:16 -0400

    GHV-MS: Ajout de la classe hitValidator3D

---
+++ server/app/services/hitValidator/hitValidator3D.service.ts
@@ -0,0 +1,8 @@
+import { AxiosInstance, AxiosResponse } from "axios";
+import { injectable } from "inversify";
+import { Cache } from "./cache";
+import { IHitConfirmation, IHitToValidate, IImageToCache } from "./interfaces";
+
+@injectable()
+export class HitValidatorService3D {
+}
