52ad0fcd27630f22233030cb887e6da25eb7b803
Michael Sauget <michaelsauget@gmail.com>
2019-03-11 21:32:13 -0400

    GHV-MS: Ajout de la fonctino confirmHit (a implementer)

--- server/app/services/hitValidator/hitValidator3D.service.ts
+++ server/app/services/hitValidator/hitValidator3D.service.ts
@@ -16,4 +16,23 @@ export class HitValidatorService3D {
         this.cache = new Cache(this.CACHE_SIZE);
     }
 
+    public async confirmHit(hitToValidate: IHitToValidate): Promise<IHitConfirmation> {
+
+        let buffer: Buffer;
+
+        if (this.isStoredInCache(hitToValidate.differenceDataURL)) {
+            buffer = this.cache.get(hitToValidate.differenceDataURL);
+        } else {
+            buffer = await this.getImageFromUrl(hitToValidate.differenceDataURL);
+            this.insertElementInCache(hitToValidate.differenceDataURL, buffer);
+        }
+        // const colorFound: number = this.findColorAtPoint(hitToValidate.position, buffer);
+
+        // _TODO: A REFAIRE AU COMPLET
+        return {
+            isAHit:             this.isValidHit(hitToValidate, 1),
+            differenceIndex:    3,
+        };
+    }
+
 }
