18d029435e98debed219b51d21823d091fdd8942
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-12 05:02:46 -0400

    GHV-MS D: imports pour Arena2D

--- server/app/services/game/arena/arena2d.ts
+++ server/app/services/game/arena/arena2d.ts
@@ -1,4 +1,14 @@
+import { inject } from "inversify";
+import { GameMode } from "../../../../../common/communication/iCard";
+import { IArenaResponse, IOriginalPixelCluster, IPosition2D } from "../../../../../common/communication/iGameplay";
+import { IUser } from "../../../../../common/communication/iUser";
+import { Constants } from "../../../constants";
+import Types from "../../../types";
+import { GameManagerService } from "../game-manager.service";
 import { Arena } from "./arena";
+import { DifferencesExtractor } from "./differencesExtractor";
+import { IArenaInfos, IHitConfirmation, IPlayerInput } from "./interfaces";
+import { Referee } from "./referee";
 
 export class Arena2D extends Arena {
     // dfg
