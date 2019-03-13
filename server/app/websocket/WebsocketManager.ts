a02fbbe44307a3ed23c83af0edd2f80e5f75f6c2
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-13 19:51:05 -0400

    GHV-MS R: Changement temporaire de la variable 3D a any

--- server/app/websocket/WebsocketManager.ts
+++ server/app/websocket/WebsocketManager.ts
@@ -58,7 +58,7 @@ export class WebsocketManager {
             if (typeof user !== "string") {
                 const playerInput: IPlayerInput<IPosition2D | number> = this.buildPlayerInput(data, user);
                 this.gameManagerService.onPlayerInput(playerInput)
-                .then((response: IArenaResponse<IOriginalPixelCluster | Obj3DDifferences>) => {    // _TODO: type de RES_T pour scene 3d
+                .then((response: IArenaResponse<IOriginalPixelCluster | any>) => {    // _TODO: type de RES_T pour scene 3d
                     socket.emit(CCommon.ON_ARENA_RESPONSE, response);
                     this.chatManagerService.sendPositionValidationMessage(data.username, userList, response, this.io);
                 }).catch((error: Error) => {
