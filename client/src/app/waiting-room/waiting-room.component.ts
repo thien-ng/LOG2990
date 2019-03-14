44fb2ab18d176abd9a7b6c8b15483ddadc18ef4c
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-14 14:36:32 -0400

    JB D: imports du component waiting room

--- client/src/app/waiting-room/waiting-room.component.ts
+++ client/src/app/waiting-room/waiting-room.component.ts
@@ -1,1 +1,8 @@
 import { HttpClient } from "@angular/common/http";
+import { Component, Input } from "@angular/core";
+import { MatSnackBar } from "@angular/material";
+import { Router } from "@angular/router";
+import { Message } from "../../../../common/communication/message";
+import { CCommon } from "../../../../common/constantes/cCommon";
+import { Constants } from "../constants";
+
