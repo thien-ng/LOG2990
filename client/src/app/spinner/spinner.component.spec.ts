fba77da4ba23def251ddcac222d236b3d2afe5ed
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-14 14:07:53 -0400

    GHV A: correction lint

--- client/src/app/spinner/spinner.component.spec.ts
+++ client/src/app/spinner/spinner.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SpinnerComponent } from './spinner.component';
+import { SpinnerComponent } from "./spinner.component";
 
-describe('SpinnerComponent', () => {
+describe("SpinnerComponent", () => {
   let component: SpinnerComponent;
   let fixture: ComponentFixture<SpinnerComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SpinnerComponent ]
+      declarations: [ SpinnerComponent ],
     })
-    .compileComponents();
+    .compileComponents().catch();
   }));
 
   beforeEach(() => {
@@ -19,7 +19,7 @@ describe('SpinnerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
