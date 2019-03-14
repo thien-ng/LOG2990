3b404c9982f4723a02a5c96ab7451f3004e3d405
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-14 13:48:03 -0400

    GHV R: retrait du constructeur et ngOnInit vides

--- client/src/app/spinner/spinner.component.ts
+++ client/src/app/spinner/spinner.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-spinner",
   templateUrl: "./spinner.component.html",
   styleUrls: ["./spinner.component.scss"],
 })
-export class SpinnerComponent implements OnInit {
-
-  public constructor() {
-    //
-  }
-
-  public ngOnInit(): void {
-    console.log("init du spinner");
-  }
+export class SpinnerComponent {
 
 }
