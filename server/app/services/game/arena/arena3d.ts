110fd76fecf1e0266372f2638a9af406d1be6614
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-13 20:28:36 -0400

    GHV-MS D: Ajout de la méthode sendMessage

--- server/app/services/game/arena/arena3d.ts
+++ server/app/services/game/arena/arena3d.ts
@@ -5,4 +5,9 @@ import { IHitConfirmation } from "./interfaces";
 
 // tslint:disable:no-any
 export class Arena3D extends Arena<any, any, any, any> {
+
+    public sendMessage(playerSocketId: string, event: string, message: number): void {
+        this.gameManagerService.sendMessage(playerSocketId, event, message);
+    }
+
 }
