ec45d7cf7e521326b9c4268f6f95de9d484a98b2
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-14 16:50:01 -0400

    GHV A: groupement des imports selon CR

--- server/app/services/game/arena/referee.ts
+++ server/app/services/game/arena/referee.ts
@@ -1,14 +1,12 @@
 import { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { CCommon } from "../../../../../common/constantes/cCommon";
-import { Constants } from "../../../constants";
-
-import { Arena } from "./arena";
-import { Player } from "./player";
-import { Timer } from "./timer";
-
 import { IArenaResponse } from "../../../../../common/communication/iGameplay";
 import { IUser } from "../../../../../common/communication/iUser";
+import { CCommon } from "../../../../../common/constantes/cCommon";
+import { Constants } from "../../../constants";
+import { Arena } from "./arena";
 import { IHitConfirmation, IHitToValidate } from "./interfaces";
+import { Player } from "./player";
+import { Timer } from "./timer";
 
 const axios: AxiosInstance = require("axios");
 
