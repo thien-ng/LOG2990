1b815dda2ab01ae9b38877ac92f1e33e1d44db4f
Jeremy <jeremyyoyo@hotmail.fr>
2019-03-14 14:34:30 -0400

    JB D: ajout des tests du waiting room

---
+++ client/src/app/waiting-room/waiting-room.component.spec.ts
@@ -0,0 +1,9 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { Constants } from "../constants";
+import { TestingImportsModule } from "../testing-imports/testing-imports.module";
+import { WaitingRoomComponent } from "./waiting-room.component";
+
+describe("WaitingRoomComponent", () => {
+
+});
