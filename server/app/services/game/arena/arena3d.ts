4c092adc0db023acc58aa95c7ef56a498e1892c6
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-13 20:27:33 -0400

    GHV-MS D: Ajout des imports dans arena3d

--- server/app/services/game/arena/arena3d.ts
+++ server/app/services/game/arena/arena3d.ts
@@ -1,4 +1,7 @@
+import { IArenaResponse } from "../../../../../common/communication/iGameplay";
+import { IUser } from "../../../../../common/communication/iUser";
 import { Arena } from "./arena";
+import { IHitConfirmation } from "./interfaces";
 
 export class Arena3D extends Arena {
     // dfg
