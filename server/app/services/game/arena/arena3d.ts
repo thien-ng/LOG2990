83c6354e985ac4294acf905546218add67e737c9
Michael Sauget <michaelsauget@gmail.com>
2019-03-14 23:31:10 -0400

    GHV-MS D: Ajout de la fonction findObjectToUpdate

--- server/app/services/game/arena/arena3d.ts
+++ server/app/services/game/arena/arena3d.ts
@@ -22,5 +22,30 @@ export class Arena3D extends Arena<any, any, any, any> {
     }
     public async prepareArenaForGameplay(): Promise<void> {
         throw new Error("Method not implemented.");
+    private findObjectToUpdate(modification: IModification, sceneVariableMessage: ISceneVariablesMessage): ISceneObjectUpdate {
+
+        const originalSceneObjects: ISceneObject[] = sceneVariableMessage.originalScene.sceneObjects;
+        const modifiedSceneObjects: ISceneObject[] = sceneVariableMessage.modifiedScene.sceneObjects;
+
+        let sceneObjectUpdate: ISceneObjectUpdate;
+
+        switch (modification.type) {
+            case ModificationType.added:
+                sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.CHANGE_COLOR, modifiedSceneObjects[modification.id]);
+                break;
+            case ModificationType.removed:
+                sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.CHANGE_COLOR, originalSceneObjects[modification.id]);
+                break;
+            case ModificationType.changedColor:
+                sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.CHANGE_COLOR, originalSceneObjects[modification.id]);
+                break;
+            default:
+                sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.CHANGE_COLOR);
+                break;
+        }
+
+        return sceneObjectUpdate;
+    }
+
     }
 }
