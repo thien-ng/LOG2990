2db1554e728275528414bc5c07c8168a772b8af5
Michael Sauget <michaelsauget@gmail.com>
2019-03-15 02:25:42 -0400

    MS R: Renomme le ISceneVariablesMessage à ISceneData

--- server/app/controllers/scene-manager.controller.ts
+++ server/app/controllers/scene-manager.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
-import { ISceneVariablesMessage } from "../../../common/communication/iSceneVariables";
+import { ISceneData } from "../../../common/communication/iSceneVariables";
 import { SceneManager } from "../services/scene/scene-manager.service";
 import Types from "../types";
 
@@ -13,7 +13,7 @@ export class SceneManagerController {
         const router: Router = Router();
 
         router.post("/generator", async (req: Request, res: Response, next: NextFunction) => {
-            const resultGenerator: ISceneVariablesMessage | string = this.sceneManager.createScene(req.body);
+            const resultGenerator: ISceneData | string = this.sceneManager.createScene(req.body);
             res.json(resultGenerator);
         });
 
