1274fa74901fd425ce98115b37aa59c972e483d4
Michael Sauget <michaelsauget@gmail.com>
2019-03-14 23:41:46 -0400

    GHV-MS D: Constructeur et attributs de hitValidator3D

--- server/app/services/hitValidator/hitValidator3D.service.ts
+++ server/app/services/hitValidator/hitValidator3D.service.ts
@@ -7,14 +7,14 @@ import { IDataToCache, IHitConfirmation, IHitToValidate } from "./interfaces";
 @injectable()
 export class HitValidatorService3D {
 
-    // private readonly ERROR_ON_HTTPGET:      string =
-    // "Didn't succeed to get scene buffer from URL given.";
-    // private readonly CACHE_SIZE:            number = 5;
+    private readonly ERROR_ON_HTTPGET:      string =
+    "Didn't succeed to get scene buffer from URL given.";
+    private readonly CACHE_SIZE:            number = 5;
 
-    // private cache: Cache;
+    private cache: Cache<ISceneVariablesMessage>;
 
     public constructor() {
-        // this.cache = new Cache(this.CACHE_SIZE);
+        this.cache = new Cache<ISceneVariablesMessage>(this.CACHE_SIZE);
     }
 
     // public async confirmHit(hitToValidate: IHitToValidate<>): Promise<IHitConfirmation> {
