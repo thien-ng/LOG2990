7357226b0593e2f8683e78be41c3dd56e488a32e
Syn92 <Syn92@users.noreply.github.com>
2019-03-15 08:43:31 -0400

    AG D: player penalty

--- server/app/services/game/arena/player.ts
+++ server/app/services/game/arena/player.ts
@@ -2,16 +2,26 @@ import { IUser } from "../../../../../common/communication/iUser";
 
 export class Player {
 
-    private _points: number;
+    private _points:        number;
+    private isInPenalty:    boolean;
 
     public constructor(private user: IUser) {
         this._points = 0;
+        this.isInPenalty = false;
     }
 
     public addPoints(pointsEarned: number): void {
         if (pointsEarned > 0) {
             this._points += pointsEarned;
         }
+    }
+
+    public setPenaltyState(state: boolean): void {
+        this.isInPenalty = state;
+    }
+
+    public get penaltyState(): boolean {
+        return this.isInPenalty;
     }
 
     public get userSocketId(): string {
