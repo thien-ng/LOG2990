1891dc0b1e2b986dd21b1b6d63e19389f1669e82
Michael Sauget <michaelsauget@gmail.com>
2019-03-14 23:47:01 -0400

    GHV-MS D: Ajout des imports et des attributs

--- server/app/services/hitValidator/hitValidator3D.service.ts
+++ server/app/services/hitValidator/hitValidator3D.service.ts
@@ -1,15 +1,14 @@
 import { AxiosInstance, AxiosResponse } from "axios";
 import { injectable } from "inversify";
-import { ISceneVariablesMessage } from "../../../../common/communication/iSceneVariables";
+import { IModification, ISceneVariablesMessage } from "../../../../common/communication/iSceneVariables";
 import { Cache } from "./cache";
 import { IDataToCache, IHitConfirmation, IHitToValidate } from "./interfaces";
 
 @injectable()
 export class HitValidatorService3D {
 
-    private readonly ERROR_ON_HTTPGET:      string =
-    "Didn't succeed to get scene buffer from URL given.";
-    private readonly CACHE_SIZE:            number = 5;
+    private readonly ERROR_ON_HTTPGET:  string = "Didn't succeed to get scene buffer from URL given.";
+    private readonly CACHE_SIZE:        number = 5;
 
     private cache: Cache<ISceneVariablesMessage>;
 
