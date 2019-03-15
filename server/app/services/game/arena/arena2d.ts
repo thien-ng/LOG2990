64791314e3c3863bf6bea526430f0afd93d4daa0
Michael Sauget <michaelsauget@gmail.com>
2019-03-14 23:15:59 -0400

    GHV-MS D: Enlève les imports inutiles

--- server/app/services/game/arena/arena2d.ts
+++ server/app/services/game/arena/arena2d.ts
@@ -2,7 +2,6 @@ import { inject } from "inversify";
 import { GameMode } from "../../../../../common/communication/iCard";
 import { IArenaResponse, IOriginalPixelCluster, IPosition2D } from "../../../../../common/communication/iGameplay";
 import { IUser } from "../../../../../common/communication/iUser";
-import { Constants } from "../../../constants";
 import Types from "../../../types";
 import { GameManagerService } from "../game-manager.service";
 import { Arena } from "./arena";
