d687338320064e1f30349b057befd4e4c2fa44e2
Michael Sauget <michaelsauget@gmail.com>
2019-03-14 23:19:25 -0400

    GHV-MS D: Templatage de l'interface ICacheElement

--- server/app/services/hitValidator/interfaces.ts
+++ server/app/services/hitValidator/interfaces.ts
@@ -3,8 +3,8 @@ export interface IImageToCache {
     buffer:                 Buffer;
 }
 
-export interface ICacheElement {
-    imageToCache:           IImageToCache;
+export interface ICacheElement<T> {
+    dataToCache:           IDataToCache<T>;
     obsolescenceDegree:     number;
 }
 
