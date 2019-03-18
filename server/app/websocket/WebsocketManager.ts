941fe53d3d2737a61374865f242c01dc8060b370
Thienng97 <duc-thien@sympatico.ca>
2019-03-18 17:04:56 -0400

    DN D: add emit to send arena response

--- server/app/websocket/WebsocketManager.ts
+++ server/app/websocket/WebsocketManager.ts
@@ -65,6 +65,8 @@ export class WebsocketManager {
                 this.gameManagerService.onPlayerInput(playerInput)
                 // tslint:disable-next-line:no-any _TODO
                 .then((response: IArenaResponse<IOriginalPixelCluster | any>) => {    // _TODO: type de RES_T pour scene 3d
+
+                    socket.emit(CCommon.ON_ARENA_RESPONSE, response);
                     if (response.status !== Constants.ON_PENALTY) {
                         this.chatManagerService.sendPositionValidationMessage(data.username, userList, response, this.io);
                     }
