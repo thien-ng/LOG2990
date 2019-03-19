9320a8ada060544994c85ddc68dd837a6fcdfc0f
jeremy <jeremyyoyo@hotmail.fr>
2019-03-18 21:51:45 -0400

    JB R: regler change request

--- client/src/app/card/confirmation-dialog/confirmation-dialog.component.ts
+++ client/src/app/card/confirmation-dialog/confirmation-dialog.component.ts
@@ -2,23 +2,23 @@ import { Component, Inject} from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 
 @Component({
-  selector: "app-confirmation-dialog",
-  templateUrl: "./confirmation-dialog.component.html",
-  styleUrls: ["./confirmation-dialog.component.css"],
+  selector:     "app-confirmation-dialog",
+  templateUrl:  "./confirmation-dialog.component.html",
+  styleUrls:    ["./confirmation-dialog.component.css"],
 })
 export class ConfirmationDialogComponent {
 
-  public TITLE:           string = "Avertissement";
+  public readonly YES_BUTTON_TEXT: string = "Oui";
+  public readonly NO_BUTTON_TEXT:  string = "Non";
+  public readonly TITLE:           string = "Avertissement";
+  public readonly CONFIRMATION:    string = "Voulez vous vraiment supprimer le jeu";
+
   public cardTitle:       string;
-  public MESSAGE:         string;
-  public YES_BUTTON_TEXT: string = "Oui";
-  public NO_BUTTON_TEXT:  string = "Non";
 
   public constructor(
     public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: string) {
       this.cardTitle = data;
-      this.MESSAGE = "Voulez vous vraiment supprimer le jeu";
   }
 
   public decline(): void {
