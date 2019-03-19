564c7e9f909719671dde955d87452cee9c60cf01
Thienng97 <duc-thien@sympatico.ca>
2019-03-18 21:23:34 -0400

    DN R: lint refactoring

--- server/app/services/game/arena/arena3d.ts
+++ server/app/services/game/arena/arena3d.ts
@@ -64,12 +64,12 @@ export class Arena3D extends Arena<IPlayerInput<number>, IArenaResponse<ISceneOb
     private async extractModifiedSceneObjects(): Promise<void> {
         const sceneData:        Buffer      = await this.getDifferenceDataFromURL(this.arenaInfos.dataUrl.sceneData);
         const sceneDataObject:  ISceneData  = JSON.parse(sceneData.toString()) as ISceneData;
-        
+
         sceneDataObject.modifications.forEach((modification: IModification) => {
             const sceneObjectUpdate: ISceneObjectUpdate = this.findObjectToUpdate(modification, sceneDataObject);
             this.originalElements.set(modification.id, sceneObjectUpdate);
         });
-        
+
     }
 
     private findObjectToUpdate(modification: IModification, sceneVariableMessage: ISceneData): ISceneObjectUpdate {
@@ -81,7 +81,8 @@ export class Arena3D extends Arena<IPlayerInput<number>, IArenaResponse<ISceneOb
 
         switch (modification.type) {
             case ModificationType.added:
-                sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.DELETE, this.findObjectById(modification.id, modifiedSceneObjects));
+                sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.DELETE,
+                                                                this.findObjectById(modification.id, modifiedSceneObjects));
                 break;
             case ModificationType.removed:
                 sceneObjectUpdate = this.buildSceneObjectUpdate(ActionType.ADD, originalSceneObjects[modification.id]);
@@ -97,9 +98,9 @@ export class Arena3D extends Arena<IPlayerInput<number>, IArenaResponse<ISceneOb
         return sceneObjectUpdate;
     }
 
-    private findObjectById(id :number, objectList: ISceneObject[]): ISceneObject | undefined {
+    private findObjectById(id: number, objectList: ISceneObject[]): ISceneObject | undefined {
 
-        let foundObject: ISceneObject | undefined = undefined;
+        let foundObject: ISceneObject | undefined;
 
         objectList.forEach((object: ISceneObject) => {
             if (id === object.id) {
--- server/app/services/hitValidator/hitValidator.controller.ts
+++ server/app/services/hitValidator/hitValidator.controller.ts
@@ -14,7 +14,7 @@ export class HitValidatorController {
 
     public constructor(
         @inject(Types.HitValidatorService2D) private hitValidatorService2D: HitValidatorService2D,
-        @inject(Types.HitValidatorService3D) private hitValidatorService3D: HitValidatorService3D
+        @inject(Types.HitValidatorService3D) private hitValidatorService3D: HitValidatorService3D,
         ) {}
 
     public get router(): Router {
