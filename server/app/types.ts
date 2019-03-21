85c87d2eb763c1e0c868c2abfdc8757fd6105660
jeremy <jeremyyoyo@hotmail.fr>
2019-03-21 17:52:25 -0400

    JB R: regler change request

--- server/app/types.ts
+++ server/app/types.ts
@@ -21,7 +21,6 @@ export default  {
         HitValidatorService3D:          Symbol("HitValidatorService3D"),
         HitValidatorController:         Symbol("HitValidatorController"),
 
-        GameManager:                    Symbol("GameManager"), // _TODO
         GameManagerController:          Symbol("GameManagerController"),
         GameManagerService:             Symbol("GameManagerService"),
 
