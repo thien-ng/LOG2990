b16396b972769be192d5ac306eb7f8099f08d49e
Syn92 <Syn92@users.noreply.github.com>
2019-03-21 22:29:02 -0400

    AG T: test freeView service

--- client/src/app/game-view/game-view-free/game-view-free.service.spec.ts
+++ client/src/app/game-view/game-view-free/game-view-free.service.spec.ts
@@ -1,12 +1,36 @@
-import { TestBed } from "@angular/core/testing";
+import { inject, TestBed } from "@angular/core/testing";
 
+import { ElementRef } from "@angular/core";
+import { GameMode } from "../../../../../common/communication/iCard";
+import { ActionType, IArenaResponse, ISceneObjectUpdate } from "../../../../../common/communication/iGameplay";
+import { ISceneObject } from "../../../../../common/communication/iSceneObject";
+import { CCommon } from "../../../../../common/constantes/cCommon";
 import { GameViewFreeService } from "./game-view-free.service";
 
-describe("GameViewFreeService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+// tslint:disable:no-any no-magic-numbers no-empty
+const sceneObject: ISceneObject = {
+  id:         1,
+  type:       1,
+  position:   {x: 1, y: 2, z: 3},
+  rotation:   {x: 1, y: 2, z: 3},
+  color:      "#FFFFFF",
+  scale:      {x: 1, y: 2, z: 3},
+  hidden:     true,
+};
+const expectedResponse: IArenaResponse<ISceneObjectUpdate> = {
+  status:             "onSuccess",
+  response: {
+      actionToApply:  ActionType.ADD,
+      sceneObject:    sceneObject,
+  },
+  arenaType:          GameMode.free,
+};
 
-  it("should be created", () => {
-    const service: GameViewFreeService = TestBed.get(GameViewFreeService);
-    expect(service).toBeTruthy();
+describe("GameViewFreeService", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    providers: [GameViewFreeService],
   });
+});
+
 });
