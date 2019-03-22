3a1a15c7cdde81e64dcb270990fd3808d91473c8
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-22 03:31:26 -0400

    GHV-MS D: generate cube de geo dans theme gen

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-themeGenerator.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-themeGenerator.ts
@@ -42,6 +42,21 @@ export class ThreejsThemeGenerator {
     this.addObjectToScene(generatedObject, object3D.position, object3D.rotation);
   }
 
+  private generateCube(object3D: ISceneObject): void {
+
+    const generatedColor:   THREE.MeshBasicMaterial = this.createObjectColor(object3D);
+    const cubeGeometry:     THREE.Geometry          = new THREE.BoxGeometry(
+      object3D.scale.x,
+      object3D.scale.z,
+      object3D.scale.y,
+    );
+
+    const generatedObject:  THREE.Mesh = new THREE.Mesh(cubeGeometry, generatedColor);
+
+    this.addColorToMap(object3D.id, object3D.color);
+    this.addObjectIdToMap(object3D.id, generatedObject.id);
+    this.addObjectToScene(generatedObject, object3D.position, object3D.rotation);
+  }
   }
 
   private addObjectIdToMap(objectId: number, generatedObjectId: number): void {
