54a207088a9ee5831ffe1ffdca6c1ee3274d185b
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-22 03:24:40 -0400

    GHV-MS D: squelette classe pour theme generator

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-themeGenerator.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-themeGenerator.ts
@@ -1,3 +1,6 @@
 import * as THREE from "three";
 import { IAxisValues, ISceneObject, SceneObjectType} from "../../../../../../../common/communication/iSceneObject";
 
+export class ThreejsThemeGenerator {
+
+}
