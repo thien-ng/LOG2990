1f6e74ac3722aa6bf11e580c072f7e78435bc6e6
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-22 03:17:04 -0400

    GHV-MS D: implementation de geo pour changeColor dans theme service

--- client/src/app/game-view/game-view-free/threejs-view/threejs-ThemeView.service.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-ThemeView.service.ts
@@ -104,6 +104,28 @@ export class ThreejsThemeViewService {
   }
 
   public changeObjectsColor(cheatColorActivated: boolean, isLastChange: boolean, modifiedList?: number[]): void {
+
+    if (!modifiedList) {
+      return;
+    }
+
+    modifiedList.forEach((differenceId: number) => {
+
+      const meshObject:      THREE.Mesh | undefined = this.recoverObjectFromScene(differenceId);
+      const objectColor:     string     | undefined = this.originalColorById.get(differenceId);
+      let opacityNeeded:     number                 = (cheatColorActivated) ? 0 : 1;
+
+      if (isLastChange) {
+
+        const originalOpacity: number = this.opacityById.get(differenceId) as number;
+
+        opacityNeeded = originalOpacity;
+      }
+
+      if (meshObject !== undefined) {
+        meshObject.material = new THREE.MeshPhongMaterial({color: objectColor, opacity: opacityNeeded, transparent: true});
+      }
+    });
   }
 
   public setupFront(orientation: number): void {
