1bc2119fba43d7257ac53d8bd49ea077cee733aa
Syn92 <Syn92@users.noreply.github.com>
2019-03-21 22:23:41 -0400

    AG R: relocalisation des routines d'erreurs

--- client/src/app/game-view/game-view-simple/game-view-simple.service.ts
+++ client/src/app/game-view/game-view-simple/game-view-simple.service.ts
@@ -1,5 +1,4 @@
 import { ElementRef, Injectable } from "@angular/core";
-import { Constants } from "src/app/constants";
 import {
   IArenaResponse,
   IClickMessage,
@@ -15,13 +14,9 @@ import { CCommon } from "../../../../../common/constantes/cCommon";
 
 export class GameViewSimpleService {
 
-  private canvasOriginal:        CanvasRenderingContext2D;
   private canvasModified:        CanvasRenderingContext2D;
-  private successSound:          ElementRef;
-  private failSound:             ElementRef;
-  private textCanvasOriginal:    HTMLDivElement;
-  private textCanvasModified:    HTMLDivElement;
-  private position:              IPosition2D;
+  private successSound:          HTMLAudioElement;
+  private failSound:             HTMLAudioElement;
 
   public onArenaResponse(data: IArenaResponse<IOriginalPixelCluster>): void {
     if (data.status === CCommon.ON_SUCCESS) {
@@ -35,61 +30,26 @@ export class GameViewSimpleService {
     }
   }
 
-  public wrongClickRoutine(): void {
-    this.playFailSound();
-    this.disableClickRoutine();
-  }
-
-  public enableClickRoutine(): void {
-    document.body.style.cursor              = "auto";
-    this.canvasModified.canvas.style.cursor = "auto";
-    this.canvasOriginal.canvas.style.cursor = "auto";
-    this.textCanvasOriginal.textContent     = null;
-    this.textCanvasModified.textContent     = null;
-  }
-
-  private disableClickRoutine(): void {
-    document.body.style.cursor              = "not-allowed";
-    this.canvasModified.canvas.style.cursor = "not-allowed";
-    this.canvasOriginal.canvas.style.cursor = "not-allowed";
-    const positionTop: number               = this.position.y - Constants.CENTERY;
-    const positionRight: number             = this.position.x - Constants.CENTERX;
-
-    this.textCanvasOriginal.style.top   = positionTop     + "px";
-    this.textCanvasOriginal.style.left  = positionRight   + "px";
-    this.textCanvasModified.style.top   = positionTop     + "px";
-    this.textCanvasModified.style.left  = positionRight   + "px";
-    this.textCanvasOriginal.textContent = Constants.ERROR_MESSAGE;
-    this.textCanvasModified.textContent = Constants.ERROR_MESSAGE;
-  }
-
-  private playFailSound(): void {
-    this.failSound.nativeElement.currentTime = 0;
-    this.failSound.nativeElement.play();
+  public playFailSound(): void {
+    this.failSound.currentTime = 0;
+    this.failSound.play();
   }
 
   private playSuccessSound(): void {
-    this.successSound.nativeElement.currentTime = 0;
-    this.successSound.nativeElement.play();
+    this.successSound.currentTime = 0;
+    this.successSound.play();
   }
 
-  public setCanvas(modified: CanvasRenderingContext2D, original: CanvasRenderingContext2D): void {
+  public setCanvas(modified: CanvasRenderingContext2D): void {
     this.canvasModified = modified;
-    this.canvasOriginal = original;
-  }
-
-  public setText(text1: ElementRef, text2: ElementRef): void {
-    this.textCanvasOriginal = text1.nativeElement;
-    this.textCanvasModified = text2.nativeElement;
   }
 
   public setSounds(success: ElementRef, fail: ElementRef): void {
-    this.successSound = success;
-    this.failSound    = fail;
+    this.successSound = success.nativeElement;
+    this.failSound    = fail.nativeElement;
   }
 
   public onCanvasClick(pos: IPosition2D, id: number, username: string): IClickMessage<IPosition2D> {
-    this.position = pos;
 
     return {
       value:        pos,
