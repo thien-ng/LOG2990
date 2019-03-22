cb1d693ccf8316b435c6c7d822103524c7015e81
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-03-22 03:26:46 -0400

    GHV-MS D: fc pour set position pour theme gen

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-themeGenerator.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-themeGenerator.ts
@@ -3,6 +3,12 @@ import { IAxisValues, ISceneObject, SceneObjectType} from "../../../../../../../
 
 export class ThreejsThemeGenerator {
 
+  private setObjectPosition(object3D: THREE.Mesh, position: IAxisValues): void {
+    object3D.position.x = position.x;
+    object3D.position.y = position.y;
+    object3D.position.z = position.z;
+  }
+
   private setObjectRotation(object3D: THREE.Mesh, orientation: IAxisValues): void {
     object3D.rotation.x = orientation.x;
     object3D.rotation.y = orientation.y;
