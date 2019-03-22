8085fcb109580ad9c2d2390e7ac030d0732c323d
Syn92 <Syn92@users.noreply.github.com>
2019-03-21 22:23:35 -0400

    AG R: relocalisation des routines d'erreurs

--- client/src/app/game-view/game-view-free/game-view-free.service.ts
+++ client/src/app/game-view/game-view-free/game-view-free.service.ts
@@ -1,6 +1,5 @@
 import { ElementRef, Injectable } from "@angular/core";
 import { Observable, Subject } from "rxjs";
-import { Constants } from "src/app/constants";
 import { GameConnectionService } from "src/app/game-connection.service";
 import { IArenaResponse, IPosition2D, ISceneObjectUpdate } from "../../../../../common/communication/iGameplay";
 import { CCommon } from "../../../../../common/constantes/cCommon";
@@ -11,10 +10,8 @@ import { CCommon } from "../../../../../common/constantes/cCommon";
 export class GameViewFreeService {
 
   private rightClickActive:      Subject<boolean>;
-  private successSound:          ElementRef;
-  private failSound:             ElementRef;
-  private textCanvasOriginal:    HTMLDivElement;
-  private textCanvasModified:    HTMLDivElement;
+  private successSound:          HTMLAudioElement;
+  private failSound:             HTMLAudioElement;
   public  position:              IPosition2D;
 
   public constructor (private gameConnectionService: GameConnectionService) {
@@ -41,53 +38,24 @@ export class GameViewFreeService {
     }
   }
 
-  public setPosition(mouseEvent: MouseEvent): void {
-    this.position.x = mouseEvent.offsetX;
-    this.position.y = mouseEvent.offsetY;
+  public setPosition(posX: number, posY: number): void {
+    this.position.x = posX;
+    this.position.y = posY;
   }
 
-  public wrongClickRoutine(): void {
-    this.playFailSound();
-    this.disableClickRoutine();
-  }
-
-  public enableClickRoutine(): void {
-    document.body.style.cursor          = "auto";
-    this.textCanvasOriginal.textContent = null;
-    this.textCanvasModified.textContent = null;
-  }
-
-  private disableClickRoutine(): void {
-      document.body.style.cursor  = "not-allowed";
-      const positionTop:   number = this.position.y - Constants.CENTERY;
-      const positionRight: number = this.position.x - Constants.CENTERX;
-
-      this.textCanvasOriginal.style.top   = positionTop   + "px";
-      this.textCanvasOriginal.style.left  = positionRight + "px";
-      this.textCanvasModified.style.top   = positionTop   + "px";
-      this.textCanvasModified.style.left  = positionRight + "px";
-      this.textCanvasOriginal.textContent = Constants.ERROR_MESSAGE;
-      this.textCanvasModified.textContent = Constants.ERROR_MESSAGE;
-  }
-
-  private playFailSound(): void {
-    this.failSound.nativeElement.currentTime = 0;
-    this.failSound.nativeElement.play();
+  public playFailSound(): void {
+    this.failSound.currentTime = 0;
+    this.failSound.play();
   }
 
   private playSuccessSound(): void {
-    this.successSound.nativeElement.currentTime = 0;
-    this.successSound.nativeElement.play();
+    this.successSound.currentTime = 0;
+    this.successSound.play();
   }
 
   public setSounds(success: ElementRef, fail: ElementRef): void {
-    this.successSound = success;
-    this.failSound    = fail;
-  }
-
-  public setText(text1: ElementRef, text2: ElementRef): void {
-    this.textCanvasOriginal = text1.nativeElement;
-    this.textCanvasModified = text2.nativeElement;
+    this.successSound = success.nativeElement;
+    this.failSound    = fail.nativeElement;
   }
 
 }
