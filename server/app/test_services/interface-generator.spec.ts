31ba382fc5a27402011d9d2d617539fd8e55b022
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-03-23 13:50:26 -0400

    CT T: ajout test pour le buildMessage()

--- server/app/test_services/interface-generator.spec.ts
+++ server/app/test_services/interface-generator.spec.ts
@@ -1,3 +1,22 @@
 import { expect } from "chai";
 import { Message } from "../../../common/communication/message";
 import { InterfaceBuilder } from "../../app/services/interface-generator";
+
+describe("interface-generator tests", () => {
+    let interfaceBuilder: InterfaceBuilder;
+
+    beforeEach( () => {
+        interfaceBuilder = new InterfaceBuilder();
+    });
+
+    it("should build a Message when calling buildMessage()", () => {
+        const expectedMessage: Message = {
+            title:  "MessageBonjour",
+            body:   "allo",
+        };
+
+        const resultMessage: Message = interfaceBuilder.buildMessage("MessageBonjour", "allo");
+        expect(resultMessage).to.not.equal(expectedMessage);
+
+    });
+});
