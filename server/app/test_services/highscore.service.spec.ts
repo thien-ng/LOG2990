dfb572be2584756587e72bdb44c4fac30e2df704
Thienng97 <duc-thien@sympatico.ca>
2019-03-23 09:45:33 -0400

    DN T: add test for highscoreService

--- server/app/test_services/highscore.service.spec.ts
+++ server/app/test_services/highscore.service.spec.ts
@@ -1,4 +1,6 @@
 import { expect } from "chai";
+import * as SocketIO from "socket.io";
+import { mock } from "ts-mockito";
 import { Highscore, HighscoreMessage, Mode } from "../../../common/communication/highscore";
 import { CCommon } from "../../../common/constantes/cCommon";
 import { Constants } from "../constants";
@@ -49,10 +51,17 @@ describe("HighscoreService tests", () => {
         ];
         highscoreService = new HighscoreService();
         highscoreService["highscores"] = mockHighscore;
+        highscoreService["socketServer"] = mock(SocketIO);
     });
 
     afterEach(() => {
         mockAxios.restore();
+    });
+
+    it("Should set socket server", () => {
+        const mockSocket: any = mock(SocketIO);
+        highscoreService.setServer(mockSocket);
+        expect(highscoreService["socketServer"]).to.equal(mockSocket);
     });
 
     it("Should return the right highscore", () => {
