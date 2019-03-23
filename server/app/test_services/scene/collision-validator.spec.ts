3b2d293b7f5e3e9fc6499a41d029da3e3d4a2d17
Cromer Tristan <cromer.tristan2.0@hotmail.com>
2019-03-23 19:30:20 -0400

    CT T: test non collision triangularPyramid

--- server/app/test_services/scene/collision-validator.spec.ts
+++ server/app/test_services/scene/collision-validator.spec.ts
@@ -383,5 +383,27 @@ describe("Collision validator tests", () => {
     describe("tests collision with triangularPyramid", () => {
         const existingSceneObjects: ISceneObject[] = [triangularPyramid1, triangularPyramid2];
 
+        it("should return false if there is no collision between new and already existing scene objects", () => {
+            const newTriangularPyramid: ISceneObject = {
+                id:     1,
+                type:   4,
+                position: {
+                    x: 360, y: 360, z: 360,
+                },
+                rotation: {
+                    x: 1,   y: 1,   z: 1,
+                },
+                scale: {
+                    x: 10,  y: 10,  z: 10,
+                },
+                color:  "#8cadbb",
+                hidden: false,
+            };
+
+            const isColliding: boolean = collisionValidator.hasCollidingPositions(newTriangularPyramid, existingSceneObjects);
+
+            chai.expect(isColliding).equal(false);
+        });
+
     });
 });
