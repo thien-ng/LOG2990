bfaea3a68fb2215328c15708a6a69603bae2dc39
Syn92 <Syn92@users.noreply.github.com>
2019-03-24 23:04:33 -0400

    AG A: change request

--- client/src/app/game-view/game-view-free/threejs-view/threejs-Themeview.service.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-Themeview.service.spec.ts
@@ -269,9 +269,9 @@ describe("ThreejsThemeViewService Tests", () => {
 
     threejsThemeViewService["threejsGenerator"] = mock(ThreejsThemeGenerator);
     threejsThemeViewService["threejsThemeRaycast"] = mock(ThreejsRaycast);
-    spyOn<any>(threejsThemeViewService, "createLighting").and.callFake(() => {return; });
+    spyOn<any>(threejsThemeViewService, "createLighting").and.callFake(()       => {return; });
     spyOn<any>(threejsThemeViewService, "generateSceneObjects").and.callFake(() => { return; });
-    spyOn<any>(threejsThemeViewService, "getModelObjects").and.callFake(() => {Promise.resolve(); });
+    spyOn<any>(threejsThemeViewService, "getModelObjects").and.callFake(()      => {Promise.resolve(); });
 
     const changeSpy: any = spyOn<any>(threejsThemeViewService["threejsGenerator"], "changeObjectColor");
 
