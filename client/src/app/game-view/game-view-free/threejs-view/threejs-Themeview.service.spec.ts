32bfd96a729d95d50406aa356099bd0a7f1f805c
Syn92 <Syn92@users.noreply.github.com>
2019-03-24 23:04:57 -0400

    AG A: change request

--- client/src/app/game-view/game-view-free/threejs-view/threejs-Themeview.service.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-Themeview.service.spec.ts
@@ -377,11 +377,11 @@ describe("ThreejsThemeViewService Tests", () => {
   it("should call function change color object from threejsGenerator (check if not call initiateObject)",
      inject([ThreejsThemeViewService], (threejsThemeViewService: ThreejsThemeViewService) => {
 
-    threejsThemeViewService["threejsGenerator"] = mock(ThreejsThemeGenerator);
-    threejsThemeViewService["threejsThemeRaycast"] = mock(ThreejsRaycast);
-    spyOn<any>(threejsThemeViewService, "createLighting").and.callFake(() => {return; });
+    threejsThemeViewService["threejsGenerator"]     = mock(ThreejsThemeGenerator);
+    threejsThemeViewService["threejsThemeRaycast"]  = mock(ThreejsRaycast);
+    spyOn<any>(threejsThemeViewService, "createLighting").and.callFake(()       => {return; });
     spyOn<any>(threejsThemeViewService, "generateSceneObjects").and.callFake(() => { return; });
-    spyOn<any>(threejsThemeViewService, "getModelObjects").and.callFake(() => {Promise.resolve(); });
+    spyOn<any>(threejsThemeViewService, "getModelObjects").and.callFake(()      => {Promise.resolve(); });
 
     const initSpy:   any = spyOn<any>(threejsThemeViewService["threejsGenerator"], "initiateObject");
 
