f6802e59f03834f4e5844fa47b3e0a7a5f2578b6
jeremy <jeremyyoyo@hotmail.fr>
2019-04-02 16:11:50 -0400

    JB T: regler les tests du end game dialog component

--- client/src/app/game-view/endGameDialog/end-game-dialog/end-game-dialog.component.spec.ts
+++ client/src/app/game-view/endGameDialog/end-game-dialog/end-game-dialog.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { TestingImportsModule } from "src/app/testing-imports/testing-imports.module";
+import { EndGameDialogService } from "../end-game-dialog.service";
 import { EndGameDialogComponent } from "./end-game-dialog.component";
 
 // tslint:disable: no-floating-promises
@@ -10,6 +13,11 @@ describe("EndGameDialogComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ EndGameDialogComponent ],
+      imports:      [TestingImportsModule],
+      providers:    [
+        EndGameDialogService,
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} } ],
     })
     .compileComponents();
   }));
