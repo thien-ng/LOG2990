08b760a8da3a54a0dbed819ae75f130134b75695
Syn92 <Syn92@users.noreply.github.com>
2019-04-03 16:01:38 -0400

    AG D: son de countdown

--- client/src/app/waiting-room/waiting-room.component.ts
+++ client/src/app/waiting-room/waiting-room.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, ViewChild } from "@angular/core";
 import { MatSnackBar } from "@angular/material";
 import { Router } from "@angular/router";
 import { CardDeleted } from "../../../../common/communication/iCard";
@@ -23,6 +23,9 @@ export class WaitingRoomComponent {
   public readonly CANCEL_BUTTON_TEXT: string = "Retourner à la liste de jeu";
   public readonly LOBBY_MESSAGE:      string = "En attente d'un autre joueur";
   public readonly VSIMAGE:            string = CClient.PATH_TO_IMAGES + "/versus.png";
+  public readonly COUNTDOWN_SOUND:    string  = CCommon.BASE_URL  + CCommon.BASE_SERVER_PORT + "/audio/countdown_01.mp3";
+
+  @ViewChild("countdownSound",  {read: ElementRef})  public countdownSound:    ElementRef;
 
   public counter:           string;
   public username:          string | null;
@@ -59,6 +62,7 @@ export class WaitingRoomComponent {
       this.counter = (message === 0) ? GO_MESSAGE : message.toString();
       if (message === COUNTDOWN_START ) {
         this.isCounterStarted = true;
+        this.countdownSound.nativeElement.play();
       }
     });
   }
