4f5941a1f9c155dd5b9390b07804fb58db5a4cb1
Syn92 <35808535+Syn92@users.noreply.github.com>
2019-04-02 23:09:11 -0400

    AG D: changement pour un post

--- client/src/app/main-nav/picture-changer-dialog/picture-changer-dialog.component.ts
+++ client/src/app/main-nav/picture-changer-dialog/picture-changer-dialog.component.ts
@@ -2,16 +2,21 @@ import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { MatDialogRef } from "@angular/material/dialog";
 import { CClient } from "src/app/CClient";
+import { Message } from "../../../../../common/communication/message";
+import { CCommon } from "../../../../../common/constantes/cCommon";
+
+const DELAY_CLICK: number = 1000;
 
 @Component({
   selector: "app-picture-changer-dialog",
   templateUrl: "./picture-changer-dialog.component.html",
   styleUrls: ["./picture-changer-dialog.component.css"],
 })
+
 export class PictureChangerDialogComponent {
   public profilePic: string;
   public username:   string | null;
-  public isDisable:    boolean;
+  public isDisable:  boolean;
 
   public constructor(
     private httpClient: HttpClient,
@@ -23,13 +28,22 @@ export class PictureChangerDialogComponent {
      }
 
   public changeImage(): void {
-      this.httpClient.get(CClient.PATH_TO_NEW_PICTURE + "/" + this.username).subscribe(() => {
-      window.setTimeout(() => {
-        this.profilePic = CClient.PATH_TO_PROFILE_IMAGES + this.username + ".bmp" + "?" + new Date().getTime();
-      }, 1000);
+    if (this.username === null) {
+      return;
+    }
+    const message: Message = {
+      title: "username",
+      body: this.username,
+    };
+    this.httpClient.post<Message>(CClient.PATH_TO_NEW_PICTURE, message).subscribe((message: Message) => {
+      if (message.title !== CCommon.ON_ERROR) {
+        window.setTimeout(() => {
+          this.profilePic = CClient.PATH_TO_PROFILE_IMAGES + this.username + ".bmp" + "?" + new Date().getTime();
+        },                DELAY_CLICK);
+      }
       });
-      this.isDisable = true;
-      window.setTimeout(() => {this.isDisable = false; }, 1500);
+    this.isDisable = true;
+    window.setTimeout(() => {this.isDisable = false; }, DELAY_CLICK);
     }
 
   public updateImage(): void {
