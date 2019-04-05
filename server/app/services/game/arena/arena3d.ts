e8acdd802493760b3dac902d896178b8397a7ffe
Thienng97 <duc-thien@sympatico.ca>
2019-04-05 01:17:49 -0400

    DN R: break line for lint

--- server/app/services/game/arena/arena3d.ts
+++ server/app/services/game/arena/arena3d.ts
@@ -60,7 +60,6 @@ export class Arena3D extends Arena<
     public async prepareArenaForGameplay(): Promise<void> {
         await this.extractModifiedSceneObjects();
         this.referee = new Referee<number, ISceneObjectUpdate<ISceneObject | IMesh>>(
-            this, this.players, this.originalElements as Map<number, ISceneObjectUpdate<ISceneObject | IMesh>>, this.timer, this.arenaInfos.dataUrl.sceneData);
     }
 
     private async extractModifiedSceneObjects(): Promise<void> {
