585db4fe2ed5fc50f362a2ef8250469b74499770
Michael Sauget <michaelsauget@gmail.com>
2019-04-05 03:13:37 -0400

    MS R: Refonte du movement-collision

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.ts
@@ -9,139 +9,82 @@ export class ThreejsMovement {
 
     private velocity:   THREE.Vector3;
     private direction:  THREE.Vector3;
-    private pointingAt: THREE.Vector3;
-    private mouseIsClicked: boolean;
-    // private front:      THREE.Vector3;
-    // private orthogonal: THREE.Vector3;
-    private objettttt:  THREE.Mesh;
+    private forward:    THREE.Vector3;
+    private orthogonal: THREE.Vector3;
+    private boule: THREE.Mesh;
 
+    public constructor(private camera: THREE.PerspectiveCamera, private scene: THREE.Scene) {
         this.velocity   = new THREE.Vector3(0, 0, 0);
         this.direction  = new THREE.Vector3(0, 0, 0);
-        this.mouseIsClicked = false;
-        // this.front      = new THREE.Vector3(0, 0, 0);
-        // this.orthogonal = new THREE.Vector3(0, 0, 0);
-        this.pointingAt = new THREE.Vector3(0, 0, 0);
+        this.forward    = new THREE.Vector3(0, 0, 0);
+        this.orthogonal = new THREE.Vector3(0, 0, 0);
 
         const geometry: THREE.SphereGeometry = new THREE.SphereGeometry( 0.1 );
         const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-        this.objettttt = new THREE.Mesh( geometry, material );
-        scene.add( this.objettttt );
+        this.boule = new THREE.Mesh( geometry, material );
+        scene.add( this.boule );
     }
 
     public rotateCamera(position: IPosition2D): void {
-    public setMouseClickStatus(isClicked: boolean): void {
-        this.mouseIsClicked = isClicked;
+        const yAxis: THREE.Vector3 = new THREE.Vector3(0, 1, 0);
+        const xAxis: THREE.Vector3 = new THREE.Vector3(1, 0, 0);
+        this.camera.rotateOnAxis(xAxis, -position.y * this.CAMERA_ROTATION_SPEED);
+        this.camera.rotateOnWorldAxis(yAxis, -position.x * this.CAMERA_ROTATION_SPEED);
     }
 
     public movementCamera(moveForward: boolean, moveBackward: boolean, moveLeft: boolean, moveRight: boolean): void {
 
         if ( moveLeft ) {
-        //   this.moveToSide(1);
+          this.moveToSide(1);
 
         } else if ( moveRight ) {
-        //   this.moveToSide(-1);
+          this.moveToSide(-1);
         }
 
-        // this.direction = this.addVectors();
-        // this.addVectors(this.front, this.orthogonal, this.direction);
-        // this.direction.normalize();
+        this.direction = this.addVectors(this.forward, this.orthogonal);
+        this.direction.normalize();
 
         if ( moveForward || moveBackward || moveLeft || moveRight) {
 
-            this.direction.z = Number(moveForward) - Number(moveBackward);
-            this.direction.x = Number(moveRight)     - Number(moveLeft);
-            console.log("Direction z " + this.direction.z);
-            console.log("Direction x " + this.direction.x);
-            console.log("");
-            this.updatePointAt();
-            this.bougeMaBoule();
-            // this.updatePointAt();
-            // this.bougeMaBoule();
-            this.setCameraVelocity();
-            // this.moveCameraTowards(this.pointingAt);
-            this.translateCamera();
+            this.direction.z = Number(moveBackward) - Number(moveForward);
+            this.direction.x = Number(moveRight)    - Number(moveLeft);
+            this.setCameratVelocity();
+            if (!this.objectIsBlockingDirection(this.direction.z)) {
+                this.translateCamera();
+                this.bougeMaBoule();
+            }
         } else {
             this.multiplyVector(this.velocity, 0);
         }
-        // if (!this.detectCollisionInDirection(this.direction)) {
-            // this.translateCamera();
-        // }
+
     }
 
-    // private getAimedDirection(commandDirection: THREE.Vector3): THREE.Vector3 {
-    //     const front: THREE.Vector3 = this.getFront();
-    //     const left: THREE.Vector3 = this.getLeft();
+    private moveToSide(orientation: number): void {
+        const frontvector:  THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+        const yAxis:        THREE.Vector3 = new THREE.Vector3(0, orientation, 0);
+        this.camera.getWorldDirection(frontvector);
+        this.orthogonal = this.crossProduct(frontvector, yAxis);
+    }
 
-    //     // front.x *= this.direction.z;
-    //     // front.y *= this.direction.z;
-    //     front.z *= this.direction.z;
-
-    //     left.x *= this.direction.x;
-    //     // left.y *= this.direction.x;
-    //     // left.z *= this.direction.x;
-
-    //     this.pointingAt = this.addVectors(front, left);
-    //     // this.addVectors(front, left, this.pointingAt);
-
-    //     this.pointingAt.normalize();
-    // }
-
-    // private moveToSide(orientation: number): void {
-    //     const frontvector:  THREE.Vector3 = new THREE.Vector3(0, 0, 0);
-    //     const yAxis:        THREE.Vector3 = new THREE.Vector3(0, orientation, 0);
-    //     this.camera.getWorldDirection(frontvector);
-    //     frontvector.z = frontvector.z * -1;
-    //     this.orthogonal = this.crossProduct(frontvector, yAxis);
-    // }
-
-    private setCameraVelocity(): void {
+    private setCameratVelocity(): void {
         this.direction.normalize();
-        // this.printVector("direction ", this.direction);
         this.velocity.x = this.direction.x * this.CAMERA_MOVEMENT_SPEED;
         this.velocity.y = this.direction.y * this.CAMERA_MOVEMENT_SPEED;
-        this.velocity.z = -(this.direction.z * this.CAMERA_MOVEMENT_SPEED);
+        this.velocity.z = this.direction.z * this.CAMERA_MOVEMENT_SPEED;
     }
 
-    private updatePointAt(): void {
-        const front: THREE.Vector3 = this.getFront();
-        const left: THREE.Vector3 = this.getLeft();
+    private objectIsBlockingDirection(frontDirection: number): boolean {
+        const raycaster:      THREE.Raycaster = new THREE.Raycaster();
+        const worldDirection: THREE.Vector3   = new THREE.Vector3();
 
-        // front.x *= this.direction.z;
-        // front.y *= this.direction.z;
-        front.z *= this.direction.z;
+        this.camera.getWorldDirection(worldDirection);
 
-        left.x *= this.direction.x;
-        // left.y *= this.direction.x;
-        // left.z *= this.direction.x;
+        const ray: THREE.Vector3 = new THREE.Vector3(worldDirection.x, worldDirection.y, worldDirection.z * - frontDirection);
+        raycaster.set(this.camera.position, ray);
 
-        this.pointingAt = this.addVectors(front, left);
-        // this.addVectors(front, left, this.pointingAt);
+        const objectsIntersected: THREE.Intersection[] = raycaster.intersectObjects(this.scene.children, true);
 
-        this.pointingAt.normalize();
-    }
-
-    private getFront(): THREE.Vector3 {
-        const front: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
-        this.camera.getWorldDirection(front);
-        // this.printVector("front ", front);
-        // front = this.multiplyVector(front, -1);
-
-        // console.log("front: ", front);
-        // this.printVector("front ", front);
-
-        return front.normalize();
-    }
-
-
-    private getLeft(): THREE.Vector3 {
-        const yAxis: THREE.Vector3 = new THREE.Vector3(0, -1, 0);
-        const left: THREE.Vector3 = this.crossProduct(this.getFront(), yAxis);
-
-        // left.x = left.x * -1;
-        // left.y = left.y * -1;
-        // left.z = left.z * -1;
-
-        return left.normalize();
+        return objectsIntersected.length > 0 && objectsIntersected[0].distance < this.CAMERA_COLLISION_RADIUS;
     }
 
     private bougeMaBoule(): void {
