85c6ee1cc38ae39f7987fd43493633dbb22e1d45
Thienng97 <duc-thien@sympatico.ca>
2019-04-05 01:15:52 -0400

    DN R: change import poisiton for lint

--- client/src/app/game-view/game-view-free/threejs-view/threejs-view.component.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-view.component.ts
@@ -6,6 +6,7 @@ import { MatSnackBar } from "@angular/material";
 import { GameConnectionService } from "src/app/game-connection.service";
 import * as THREE from "three";
 import { IClickMessage, IPosition2D, ISceneObjectUpdate, ICheat, ActionType } from "../../../../../../common/communication/iGameplay";
+import { ActionType, ICheat, IClickMessage, IPosition2D, ISceneObjectUpdate } from "../../../../../../common/communication/iGameplay";
 import { ISceneMessage } from "../../../../../../common/communication/iSceneMessage";
 import { IMesh, ISceneObject } from "../../../../../../common/communication/iSceneObject";
 import { SceneType } from "../../../../../../common/communication/iSceneOptions";
