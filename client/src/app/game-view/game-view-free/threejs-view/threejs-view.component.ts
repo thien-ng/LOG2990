a0cea9e65000371445454987ceeb2cd1df81221d
Thienng97 <duc-thien@sympatico.ca>
2019-04-05 00:21:59 -0400

    DN D: add subscribe cheat path

--- client/src/app/game-view/game-view-free/threejs-view/threejs-view.component.ts
+++ client/src/app/game-view/game-view-free/threejs-view/threejs-view.component.ts
@@ -157,6 +157,7 @@ export class TheejsViewComponent implements AfterContentInit, OnChanges, OnDestr
 
   private cheatRoutine (): void {
     this.httpClient.get(CClient.GET_OBJECTS_ID_PATH + this.CHEAT_URL + this.arenaID).subscribe((modifications: number[]) => {
+    this.httpClient.get(CClient.GET_OBJECTS_ID_PATH + this.CHEAT_URL + this.arenaID).subscribe((modifications: ICheat[]) => {
       if (this.isFirstGet) {
         this.previousModifications = modifications;
         this.isFirstGet            = false;
