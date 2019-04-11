b8e842b8594bf5cc3522c061238a8bb6ba446e77
Thienng97 <duc-thien@sympatico.ca>
2019-04-10 21:48:25 -0400

    DN T: remove spy on removed attribut

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.spec.ts
@@ -61,7 +61,6 @@ describe("Threejs-movement", () => {
     it("should move camera forward (check value)", () => {
         const cameraMocked: any = threejsMovement["camera"];
 
-        threejsMovement["front"]        = new THREE.Vector3(0, 0, 1);
         threejsMovement["orthogonal"]   = new THREE.Vector3(0, 0, 1);
         threejsMovement["direction"]    = new THREE.Vector3(1, 2, 3);
         threejsMovement["velocity"]     = new THREE.Vector3(3, 3, 3);
