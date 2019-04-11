732c78732060fa08f2d8370018c24bea5a1f25ee
Thienng97 <duc-thien@sympatico.ca>
2019-04-10 21:52:01 -0400

    DN R: remove spy on removed private method

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.spec.ts
@@ -100,7 +100,6 @@ describe("Threejs-movement", () => {
     });
 
     it("should move camera backward (check call)", () => {
-        const spy: any = spyOn<any>(threejsMovement, "setCameratVelocity");
 
         threejsMovement["direction"]    = new THREE.Vector3(1, 2, 3);
         threejsMovement["velocity"]     = new THREE.Vector3(3, 3, 3);
