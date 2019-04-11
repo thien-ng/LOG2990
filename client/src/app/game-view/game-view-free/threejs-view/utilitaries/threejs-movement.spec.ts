e791d0802c4f2b2afb8e784b84087d56424c6dce
Thienng97 <duc-thien@sympatico.ca>
2019-04-10 21:53:27 -0400

    DN T: add spy on private method setCameraVelocity

--- client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.spec.ts
+++ client/src/app/game-view/game-view-free/threejs-view/utilitaries/threejs-movement.spec.ts
@@ -152,6 +152,7 @@ describe("Threejs-movement", () => {
     });
 
     it("should move camera right (check call)", () => {
+        const spy: any = spyOn<any>(threejsMovement, "setCameraVelocity");
 
         threejsMovement["direction"]    = new THREE.Vector3(1, 2, 3);
         threejsMovement["velocity"]     = new THREE.Vector3(3, 3, 3);
