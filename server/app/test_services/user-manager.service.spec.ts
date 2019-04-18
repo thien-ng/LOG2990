77b1f595cebce8ed7229d36f641435150a114f9f
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2019-04-18 05:23:22 -0400

    DN D: add test for user manager service when succes message to updateProfile picture

--- server/app/test_services/user-manager.service.spec.ts
+++ server/app/test_services/user-manager.service.spec.ts
@@ -61,6 +61,7 @@ describe("UserManagerService test", () => {
             username: "fuckoff",
             color:      {R: 1, G: 1, B: 1},
         };
+
     it ("should return False if name input is not unique", async () => {
         const user: IUser = {
             username:       "patate",
