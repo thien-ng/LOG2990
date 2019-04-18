ed804a7e003a3a6d9bf38ae28287b82b1fae1c50
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2019-04-18 02:25:10 -0400

    GHV A: alignement

--- client/src/app/create-free-game/create-free-game.component.ts
+++ client/src/app/create-free-game/create-free-game.component.ts
@@ -28,7 +28,6 @@ export class CreateFreeGameComponent {
                                                   + " et "
                                                   + CCommon.MAX_GAME_LENGTH
                                                   + " caractères" ;
-  public readonly EDIT_TYPE_COLOR:      string = "Changement de couleur";
   public readonly ATLEASTONE_CHECKED:   string = "Au moins une option doit être cochée";
   public readonly SUBMIT:               string  = "Soumettre";
   public readonly CANCEL:               string  = "Annuler";
@@ -39,6 +38,7 @@ export class CreateFreeGameComponent {
   public readonly THEMATIC_OPTION:      string  = "Thématique";
   public readonly EDIT_TYPE_ADD:        string  = "Ajout";
   public readonly EDIT_TYPE_DELETE:     string  = "Suppression";
+  public readonly EDIT_TYPE_COLOR:      string  = "Changement de couleur";
   public readonly NEEDED_SNAPSHOT:      boolean = true;
   public readonly MAX_OBJECT_COUNT:     number  = 200;
   public readonly MIN_OBJECT_COUNT:     number  = 10;
