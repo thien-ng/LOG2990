1124c031474d8795245cdc8177a9e4b67f647ff0
Cromer Tristan <cromer.tristan30@hotmail.com>
2019-04-18 22:40:12 -0400

    CT R: ajout close dialog dans le ngOnDestroy

--- client/src/app/game-view/game-view-simple/game-view-simple.component.ts
+++ client/src/app/game-view/game-view-simple/game-view-simple.component.ts
@@ -101,6 +101,7 @@ export class GameViewSimpleComponent implements OnInit, AfterContentInit, OnDest
     this.subscription.forEach((sub: Subscription) => {
       sub.unsubscribe();
     });
+    this.endGameDialogService.closeDialog();
   }
 
   private initEventSubscription(): void {
